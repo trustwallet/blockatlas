ee93d92ee050aef269b20e31e3409e91d7c1f087
Richard Patel <me@terorie.dev>
2019-03-17 16:45:09 +0100

    platform/nimiq: Fix model

--- platform/nimiq/api.go
+++ platform/nimiq/api.go
@@ -5,6 +5,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"net/http"
+	"strconv"
+	"trustwallet.com/blockatlas/models"
 	"trustwallet.com/blockatlas/platform/nimiq/source"
 	"trustwallet.com/blockatlas/util"
 )
@@ -18,9 +20,21 @@ func Setup(router gin.IRouter) {
 }
 
 func getTransactions(c *gin.Context) {
-	txs, err := client.GetTxsOfAddress(c.Param("address"))
+	s, err := client.GetTxsOfAddress(c.Param("address"))
 	if apiError(c, err) {
 		return
+	}
+
+	txs := make([]models.BasicTx, len(s))
+	for i, srcTx := range s {
+		txs[i] = models.BasicTx{
+			Kind: models.TxBasic,
+			Id:    srcTx.Hash,
+			From:  srcTx.FromAddress,
+			To:    srcTx.ToAddress,
+			Value: strconv.FormatUint(srcTx.Value, 10),
+			Fee:   strconv.FormatUint(srcTx.Fee, 10),
+		}
 	}
 	c.JSON(http.StatusOK, txs)
 }
