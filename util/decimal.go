0d44a3d26f4081d8c9da865374a4faeab9ceebf4
Richard Patel <me@terorie.dev>
2019-03-17 02:41:25 +0100

    util: Add DecimalExp function

--- util/decimal.go
+++ util/decimal.go
@@ -6,8 +6,11 @@ import (
 	"unicode"
 )
 
-func DecimalToSatoshis(decStr string) (string, error) {
-	out := strings.Replace(decStr, ".", "", 1)
+// DecimalToSatoshis straight up yeets the comma out of the number
+// "12.345" => "12345"
+// "0.0230" => "230"
+func DecimalToSatoshis(dec string) (string, error) {
+	out := strings.Replace(dec, ".", "", 1)
 	out = strings.TrimLeft(out, "0")
 	for _, c := range out {
 		if !unicode.IsNumber(c) {
@@ -15,4 +18,42 @@ func DecimalToSatoshis(decStr string) (string, error) {
 		}
 	}
 	return out, nil
+}
+
+// DecimalExp calculates dec * 10^exp in decimal string representation
+func DecimalExp(dec string, exp int) string {
+	// 0 * n = 0
+	if dec == "0" {
+		return "0"
+	}
+	// Get comma position
+	i := strings.IndexRune(dec, '.')
+	if i == -1 {
+		// Virtual comma at the end of the string
+		i = len(dec)
+	} else {
+		// Remove comma from underlying number
+		dec = strings.Replace(dec, ".", "", 1)
+	}
+	// Shift comma by exponent
+	i += exp
+	// Remove leading zeros
+	origSize := len(dec)
+	dec = strings.TrimLeft(dec, "0")
+	i -= origSize - len(dec)
+	// Fix bounds
+	if i <= 0 {
+		zeros := ""
+		for ; i < 0; i++ {
+			zeros += "0"
+		}
+		return "0." + zeros + dec
+	} else if i >= len(dec) {
+		for i > len(dec) {
+			dec += "0"
+		}
+		return dec
+	}
+	// No bound fix needed
+	return dec[:i] + "." + dec[i:]
 }
--- util/decimal_test.go
+++ util/decimal_test.go
@@ -18,3 +18,43 @@ func TestDecimalToSatoshis(t *testing.T) {
 	assertSatEquals("13602", "136.02")
 	assertSatEquals("1500000", "0.01500000")
 }
+
+func TestDecimalExp(t *testing.T) {
+	assertEquals := func(inputDec string, inputExp int, expected string) {
+		actual := DecimalExp(inputDec, inputExp)
+		if expected != actual {
+			t.Errorf("expected: %s * (10^%d) = %s, got %s",
+				inputDec, inputExp, expected, actual)
+		}
+	}
+
+	// No-Op
+	assertEquals("0", 300, "0")
+	assertEquals("123", 0, "123")
+	assertEquals("0.456", 0, "0.456")
+	assertEquals("123.456", 0, "123.456")
+
+	// In-Bounds, comma
+	assertEquals("12.34", -1, "1.234")
+	assertEquals("12.34",  1, "123.4")
+
+	// 1 past bounds, comma
+	assertEquals("12.34", -2, "0.1234")
+	assertEquals("12.34",  2, "1234")
+
+	// n past bounds, comma
+	assertEquals("12.34", -4, "0.001234")
+	assertEquals("12.34",  4, "123400")
+
+	// Integer
+	assertEquals("1234", -1, "123.4")
+	assertEquals("1234",  1, "12340")
+
+	// Denormalized
+	assertEquals("0.1234", -1, "0.01234")
+	assertEquals("0.1234",  1, "1.234")
+
+	// Tiny
+	assertEquals("0.001234", -1, "0.0001234")
+	assertEquals("0.001234",  1, "0.01234")
+}
