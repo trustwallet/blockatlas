1e822d6bb17b845ac6573390c3c7389db7788519
Richard Patel <me@terorie.dev>
2019-03-19 23:24:28 +0100

    platform/stellar: Rewrite streaming

--- platform/stellar/api.go
+++ platform/stellar/api.go
@@ -1,93 +1,47 @@
 package stellar
 
 import (
-	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stellar/go/clients/horizon"
-	"github.com/stellar/go/xdr"
 	"net/http"
 	"strconv"
-	"sync"
-	"time"
 	"trustwallet.com/blockatlas/models"
+	"trustwallet.com/blockatlas/platform/stellar/source"
 	"trustwallet.com/blockatlas/util"
 )
-
-var client = horizon.Client{
-	HTTP: http.DefaultClient,
-}
 
 func Setup(router gin.IRouter) {
 	router.Use(util.RequireConfig("stellar.api"))
 	router.Use(func(c *gin.Context) {
-		client.URL = viper.GetString("stellar.api")
+		source.Client.URL = viper.GetString("stellar.api")
 		c.Next()
 	})
 	router.GET("/:address", getTransactions)
 }
 
 func getTransactions(c *gin.Context) {
-	acc, err := client.LoadAccount(c.Param("address"))
+	s, err := source.GetTxsOfAddress(c.Param("address"))
 	if apiError(c, err) {
 		return
 	}
 
-	ctxt, _ := context.WithTimeout(context.Background(), time.Second)
-
-	var txMut sync.Mutex
-	var txs []models.LegacyTx
-
-	err = client.StreamTransactions(ctxt, acc.ID, nil, func(tx horizon.Transaction) {
-		txMut.Lock()
-		defer txMut.Unlock()
-
-		if tx.ResultXdr == "" {
-			return
+	txs := make([]models.LegacyTx, 0)
+	for _, srcTx := range s {
+		legacy := models.LegacyTx{
+			Id:          srcTx.Tx.Hash,
+			BlockNumber: uint64(srcTx.Tx.Ledger),
+			Timestamp:   strconv.FormatInt(srcTx.Tx.LedgerCloseTime.Unix(), 10),
+			From:        srcTx.Tx.Account,
+			To:          srcTx.Payment.Destination.Address(),
+			Value:       strconv.FormatInt(int64(srcTx.Payment.Amount), 10),
+			GasPrice:    strconv.FormatInt(int64(srcTx.Tx.FeePaid), 10),
+			Coin:        148,
 		}
-		if !tx.Successful {
-			return
-		}
-
-		var envelope xdr.TransactionEnvelope
-		err = xdr.SafeUnmarshalBase64(tx.EnvelopeXdr, &envelope)
-		if err != nil {
-			return
-		}
-
-		for _, op := range envelope.Tx.Operations {
-			payment := op.Body.PaymentOp
-			if payment == nil {
-				continue
-			}
-			if payment.Asset.Type != xdr.AssetTypeAssetTypeNative {
-				continue
-			}
-			legacy := models.LegacyTx{
-				Id:          tx.Hash,
-				BlockNumber: uint64(tx.Ledger),
-				Timestamp:   strconv.FormatInt(tx.LedgerCloseTime.Unix(), 10),
-				From:        tx.Account,
-				To:          payment.Destination.Address(),
-				Value:       strconv.FormatInt(int64(payment.Amount), 10),
-				GasPrice:    strconv.FormatInt(int64(tx.FeePaid), 10),
-				Coin:        148,
-			}
-			legacy.Init()
-			txs = append(txs, legacy)
-		}
-	})
-	if err != nil {
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
+		legacy.Init()
 	}
 
-	// Wait for transaction stream to finish
-	<-ctxt.Done()
-
-	txMut.Lock()
-	defer txMut.Unlock()
 	c.JSON(http.StatusOK, models.Response {
 		Total: len(txs),
 		Docs:  txs,
---
+++ platform/stellar/source/client.go
@@ -0,0 +1,92 @@
+package source
+
+import (
+	"context"
+	"github.com/stellar/go/clients/horizon"
+	"github.com/stellar/go/xdr"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var Client = horizon.Client{
+	HTTP: &http.Client{
+		Timeout: 2 * time.Second,
+	},
+}
+
+type Tx struct {
+	Tx       horizon.Transaction
+	Envelope xdr.TransactionEnvelope
+	Payment  *xdr.PaymentOp
+}
+
+type txStream struct {
+	sync.Mutex
+	Txs []Tx
+	Err error
+}
+
+func GetTxsOfAddress(address string) ([]Tx, error) {
+	var ctxt context.Context
+	var cancel context.CancelFunc
+	ctxt = context.Background()
+	ctxt, _ = context.WithTimeout(context.Background(), time.Second)
+	ctxt, cancel = context.WithCancel(ctxt)
+
+	var stream txStream
+	go streamTransactions(ctxt, cancel, address, &stream)
+
+	// Wait for transaction stream to finish
+	<-ctxt.Done()
+
+	stream.Lock()
+	defer stream.Unlock()
+	if stream.Err != nil {
+		return nil, stream.Err
+	}
+
+	return stream.Txs, nil
+}
+
+func streamTransactions(ctxt context.Context, cancel context.CancelFunc, address string, stream *txStream) {
+	defer cancel()
+
+	err := Client.StreamTransactions(ctxt, address, nil, func(tx horizon.Transaction) {
+		if tx.ResultXdr == "" {
+			return
+		}
+		if !tx.Successful {
+			return
+		}
+
+		var envelope xdr.TransactionEnvelope
+		err := xdr.SafeUnmarshalBase64(tx.EnvelopeXdr, &envelope)
+		if err != nil {
+			return
+		}
+
+		stream.Lock()
+		defer stream.Unlock()
+
+		for _, op := range envelope.Tx.Operations {
+			payment := op.Body.PaymentOp
+			if payment == nil {
+				continue
+			}
+			if payment.Asset.Type != xdr.AssetTypeAssetTypeNative {
+				continue
+			}
+			stream.Txs = append(stream.Txs, Tx{
+				Tx:       tx,
+				Envelope: envelope,
+				Payment:  payment,
+			})
+		}
+	})
+	if err != nil {
+		stream.Lock()
+		stream.Err = err
+		stream.Unlock()
+	}
+}
