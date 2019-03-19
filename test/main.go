188b07b780b10f411c2c626d3c6188959a6a3780
Richard Patel <me@terorie.dev>
2019-03-19 10:04:00 +0100

    Add basic integration test

---
+++ test/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fastjson"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var failedFlag = 0
+
+var addresses = map[string]string {
+	"binance": "tbnb12hlquylu78cjylk5zshxpdj6hf3t0tahwjt3ex",
+	"nimiq":   "NQ86 2H8F YGU5 RM77 QSN9 LYLH C56A CYYR 0MLA",
+	"ripple":  "rMQ98K56yXJbDGv49ZSmW51sLn94Xe1mu1",
+	"stellar": "GDKIJJIKXLOM2NRMPNQZUUYK24ZPVFC6426GZAEP3KUK6KEJLACCWNMX",
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		logrus.Fatal("Usage: ./test <base_url>")
+	}
+	b := os.Args[1]
+
+	for ns, test := range addresses {
+		runTest(ns, test, b)
+	}
+
+	os.Exit(failedFlag)
+}
+
+func log(endpoint string) *logrus.Entry {
+	return logrus.WithField("platform", endpoint)
+}
+
+func runTest(endpoint string, address string, baseUrl string) {
+	start := time.Now()
+
+	defer func() {
+		if r := recover(); r != nil {
+			log(endpoint).
+				WithField("error", r).
+				Error("Endpoint failed")
+			failedFlag = 1
+		}
+
+		log(endpoint).WithField("time", time.Since(start)).Info("Endpoint tested")
+	}()
+
+	log(endpoint).Info("Testing endpoint")
+	test(endpoint, address, baseUrl)
+	log(endpoint).Info("Endpoint works")
+}
+
+func test(endpoint string, address string, baseUrl string) {
+	res, err := http.Get(fmt.Sprintf("%s/v1/%s/%s", baseUrl, endpoint, address))
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		panic("Status " + res.Status)
+	}
+
+	if !strings.HasPrefix(res.Header.Get("Content-Type"), "application/json") {
+		panic("Unexpected Content-Type " + res.Header.Get("Content-Type"))
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := fastjson.ValidateBytes(bytes); err != nil {
+		panic(err)
+	}
+}
