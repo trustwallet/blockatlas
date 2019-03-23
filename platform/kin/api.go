19c77ddf1fd2a53dcabee60fd2835383946f6518
Richard Patel <me@terorie.dev>
2019-03-23 17:22:20 +0100

    Kin support, Stellar improvements

--- platform/kin/api.go
+++ platform/kin/api.go
@@ -3,7 +3,7 @@ package kin
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
-	"github.com/stellar/go/clients/horizon"
+	"github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/platform/stellar"
 	"github.com/trustwallet/blockatlas/platform/stellar/source"
 	"github.com/trustwallet/blockatlas/util"
@@ -12,20 +12,18 @@ import (
 )
 
 var kinClient = source.Client{
-	Client: horizon.Client{
-		HTTP: &http.Client{
-			Timeout: 2 * time.Second,
-		},
+	HTTP: &http.Client{
+		Timeout: 2 * time.Second,
 	},
 }
 
 func Setup(router gin.IRouter) {
 	router.Use(util.RequireConfig("kin.api"))
 	router.Use(func(c *gin.Context) {
-		kinClient.URL = viper.GetString("kin.api")
+		kinClient.API = viper.GetString("kin.api")
 		c.Next()
 	})
 	router.GET("/:address", func(c *gin.Context) {
-		stellar.GetTransactions(c, &kinClient)
+		stellar.GetTransactions(c, &coin.KIN, &kinClient)
 	})
 }
