8d4b4a1abfa03144c14a6a6dab38f2ba52ded3a1
Mykola <kolya182@gmail.com>
2019-04-16 11:38:23 -0700

    Temporary increase returned limit of XRP transactions

--- platform/ripple/client.go
+++ platform/ripple/client.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/sirupsen/logrus"
-	"github.com/trustwallet/blockatlas/models"
+	// "github.com/trustwallet/blockatlas/models"
 	"net/http"
 	"net/url"
 )
@@ -18,7 +18,7 @@ func (c *Client) GetTxsOfAddress(address string) ([]Tx, error) {
 	uri := fmt.Sprintf("%s/accounts/%s/transactions?type=Payment&result=tesSUCCESS&limit=%d",
 		c.RpcUrl,
 		url.PathEscape(address),
-		models.TxPerPage)
+		100)
 	httpRes, err := c.HttpClient.Get(uri)
 	if err != nil {
 		logrus.WithError(err).Error("Ripple: Failed to get transactions")
