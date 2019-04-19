42ca2d6be8eea9fab8eafaf17d5a7c84da99e184
Mykola <kolya182@gmail.com>
2019-04-19 15:29:14 -0700

    paltformvechain: client logic

---
+++ platform/vechain/client.go
@@ -0,0 +1,85 @@
+package vechain
+
+import(
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+	"strings"
+)
+
+type Client struct {
+	HttpClient *http.Client
+	RPCURI     string
+}
+
+func (c *Client) GetAddressTransactions(address string) ([]Tx, error) {
+	url := fmt.Sprintf("%s/logs/transfer", c.RPCURI)
+
+	addressEscaped, _ := json.Marshal(address)
+
+	payload := fmt.Sprintf(`{
+		"range": {
+			"unit": "block",
+			"from": 0,
+			"to": 9000000
+		},
+		"options": {
+			"offset": 0,
+			"limit": 25
+		},
+		"criteriaSet": [
+			{
+				"sender": %[1]s
+			},
+			{
+				"recipient": %[1]s
+			}
+		],
+		"order": "desc"
+	}`, string(addressEscaped))
+
+	resp, err := c.HttpClient.Post(url, "application/json", strings.NewReader(payload))
+	if err != nil {
+		logrus.WithError(err).Error("VeChain: Failed HTTP get transactions")
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, errBody := ioutil.ReadAll(resp.Body)
+	var transactions []Tx
+	if errBody != nil {
+		logrus.WithError(err).Error("VeChain: Error decode transaction response body")
+		return nil, err
+	}
+
+	errUnm := json.Unmarshal(body, &transactions)
+	if errUnm != nil {
+		logrus.WithError(err).Error("VeChain: Error Unmarshal transaction response body")
+		return nil, err
+	}
+
+	return transactions, nil
+}
+
+func (c *Client) GetTransactionReceipt(cn chan<- TxReceipt, id string) {
+	defer wg.Done()
+
+	url := fmt.Sprintf("%s/transactions/%s", c.RPCURI, id)
+
+	resp, err := c.HttpClient.Get(url)
+	if err != nil {
+		logrus.WithError(err).Error("VeChain: Failed HTTP get transaction receipt")
+	}
+	defer resp.Body.Close()
+
+	var receipt TxReceipt
+	err = json.NewDecoder(resp.Body).Decode(&receipt)
+
+	if err != nil {
+		logrus.WithError(err).Error("VeChain: Error decode transaction receipt response body")
+	} else {
+		cn <- receipt
+	}
+}
