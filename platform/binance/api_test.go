e626e612440cfd92a534ee3254d955a9e76a174f
Mykola <kolya182@gmail.com>
2019-04-21 15:29:12 -0700

    bypas wrong decimal parsing

--- platform/binance/api_test.go
+++ platform/binance/api_test.go
@@ -32,7 +32,7 @@ var transferDst = models.Tx{
 	Coin: coin.BNB,
 	From: "tbnb1fhr04azuhcj0dulm7ka40y0cqjlafwae9k9gk2",
 	To: "tbnb1sylyjw032eajr9cyllp26n04300qzzre38qyv5",
-	Fee: "125000",
+	Fee: "1250000000000000",
 	Date: 1555049867,
 	Block: 7761368,
 	Status: models.StatusCompleted,
