68017850c49c251bac33a249b6075bb2f1d8c9d2
Mykola <3277207+kolya182@users.noreply.github.com>
2019-04-20 17:19:11 -0700

    Revert "fix: test"
    This reverts commit 2da889e24fe667e23569f328b4cfb883b3cce047.

--- platform/nimiq/api_test.go
+++ platform/nimiq/api_test.go
@@ -20,7 +20,7 @@ const basicSrc = `
 	"fromAddress": "NQ69 9A4A MB83 HXDQ 4J46 BH5R 4JFF QMA9 C3GN",
 	"to": "ad25610feb43d75307763d3f010822a757027429",
 	"toAddress": "NQ15 MLJN 23YB 8FBM 61TN 7LYG 2212 LVBG 4V19",
-	"value": 0.01,
+	"value": 10000000000000,
 	"fee": 138,
 	"data": null,
 	"flags": 0
@@ -36,7 +36,7 @@ var basicDst = models.Tx{
 	Date:  1538924505,
 	Block: 252575,
 	Meta: models.Transfer{
-		Value: "1000000",
+		Value: "10000000000000",
 	},
 }
 
