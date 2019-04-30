0a858c8e16d917d5cfa06394a016c39ab6b6ba19
Mykola <3277207+kolya182@users.noreply.github.com>
2019-04-30 12:47:09 -0700

    Table driven test approuch

--- platform/vechain/api_test.go
+++ platform/vechain/api_test.go
@@ -10,214 +10,140 @@ import (
 
 const transferTrx = `
 {
-	"sender": "0x7b41d6451eaa012c551b6ed3af9f871a00602e88",
-	"recipient": "0xa4adafaef9ec07bc4dc6de146934c7119341ee25",
-	"amount": "0x3b5dd5955f12cbf0000",
+	"sender": "0xb6b6c3ad63192cadd9064432242f3a52329302f3",
+	"recipient": "0xb853d6a965fbc047aaa9f04d774d53861d7ed653",
+	"amount": "0xaf6751326c8d4a80000",
 	"meta": {
-		"blockID": "0x0026a6f4a7673e9268e1ee5cfdec899d751b93b611ffff5ec4fa16492b56c55a",
-		"blockNumber": 2533108,
-		"blockTimestamp": 1555688350,
-		"txID": "0xe282bc330b660759ac4060e33f8d74b71a59dd8dea041d71046b32d2a84e8fdd"
+		"blockID": "0x0003cae121113a2e3c7bf510629ceab7ead48b6cf2cf1a18747e7fe058b3e190",
+		"blockNumber": 248545,
+		"blockTimestamp": 1532802660,
+		"txID": "0xd989ab9e91e9af849666d507758f75ea2d4dcbe0a658284d8196ac527dbee181",
+		"txOrigin": "0xb6b6c3ad63192cadd9064432242f3a52329302f3"
 	}
 }
 `
 
-const tokenTrx = `
+const transferReceipt = `
 {
-	"sender": "0xb69ded9f0da15d240ee6803dacd7fcf68744e8ff",
-	"recipient": "0x70e0a09fa23019f83b42f018891611935c1459d0",
-	"amount": "0x4405277d0cb1630000",
-	"meta": {
-		"blockID": "0x002712dcaa19f9837e85579dfa28ad4ea54ebe2cc8cdf945ef6eb4d9b72aa0f5",
-		"blockNumber": 2560732,
-		"blockTimestamp": 1555964860,
-		"txID": "0x52ae85810f8568c0dc13bc8afd6f144d7a8e2bb949b76a5d14ab529c2438f32f",
-		"txOrigin": "0x70e0a09fa23019f83b42f018891611935c1459d0"
-	}
-}
-`
-
-
-const clause = `
-{
-	"to": "0xa4adafaef9ec07bc4dc6de146934c7119341ee25",
-	"value": "0x3b5dd5955f12cbf0000",
-	"data": "0x"
-}
-`
-
-const transactionId = `
-{
-    "id": "0xe282bc330b660759ac4060e33f8d74b71a59dd8dea041d71046b32d2a84e8fdd",
-    "clauses": [
-        {
-            "to": "0xa4adafaef9ec07bc4dc6de146934c7119341ee25",
-            "value": "0x3b5dd5955f12cbf0000",
-            "data": "0x"
-        }
-    ],
-    "gasPriceCoef": 0,
-    "gas": 21000,
-    "nonce": "0xfd15e25fbb381811",
+    "gasUsed": 21000,
+    "gasPayer": "0xb6b6c3ad63192cadd9064432242f3a52329302f3",
+    "paid": "0x1236efcbcbb340000",
+    "reward": "0x576e189f04f60000",
+    "reverted": false,
     "meta": {
-        "blockID": "0x0026a6f4a7673e9268e1ee5cfdec899d751b93b611ffff5ec4fa16492b56c55a",
-        "blockNumber": 2533108,
-        "blockTimestamp": 1555688350
-    }
-}
-`
-
-const transactionReceipt = `
-{
-    "paid": "0x1966cc5d60c170000",
-    "meta": {
-        "blockID": "0x002712dcaa19f9837e85579dfa28ad4ea54ebe2cc8cdf945ef6eb4d9b72aa0f5",
-        "blockNumber": 2560732,
-        "blockTimestamp": 1555964860,
-        "txID": "0x52ae85810f8568c0dc13bc8afd6f144d7a8e2bb949b76a5d14ab529c2438f32f"
+        "blockID": "0x0003cae121113a2e3c7bf510629ceab7ead48b6cf2cf1a18747e7fe058b3e190",
+        "blockNumber": 248545,
+        "blockTimestamp": 1532802660,
+        "txID": "0xd989ab9e91e9af849666d507758f75ea2d4dcbe0a658284d8196ac527dbee181",
+        "txOrigin": "0xb6b6c3ad63192cadd9064432242f3a52329302f3"
     },
-    "outputs": [
-        {
-            "transfers": [
-                {
-                    "sender": "0xb69ded9f0da15d240ee6803dacd7fcf68744e8ff",
-                    "recipient": "0x70e0a09fa23019f83b42f018891611935c1459d0",
-                    "amount": "0x4405277d0cb1630000"
-                }
-            ]
-        }
-    ]
+    "outputs": []
 }
 `
 
-const output = `
+const transferOutput = `
 {
+	"contractAddress": null,
+	"events": [],
 	"transfers": [
 		{
-			"sender": "0xb69ded9f0da15d240ee6803dacd7fcf68744e8ff",
-			"recipient": "0x70e0a09fa23019f83b42f018891611935c1459d0",
-			"amount": "0x4405277d0cb1630000"
+			"sender": "0xb6b6c3ad63192cadd9064432242f3a52329302f3",
+			"recipient": "0xb853d6a965fbc047aaa9f04d774d53861d7ed653",
+			"amount": "0xaf6751326c8d4a80000"
 		}
 	]
 }
 `
 
-var finalTx = models.Tx{
-	ID:    "0xe282bc330b660759ac4060e33f8d74b71a59dd8dea041d71046b32d2a84e8fdd",
+var expectedTransferTrx = models.Tx{
+	ID:    "0xd989ab9e91e9af849666d507758f75ea2d4dcbe0a658284d8196ac527dbee181",
 	Coin:  coin.VET,
-	From:  "0x7b41d6451eaa012c551b6ed3af9f871a00602e88",
-	To:    "0xa4adafaef9ec07bc4dc6de146934c7119341ee25",
-	Fee:   "0",
-	Date:  1555688350,
+	From:  "0xb6b6c3ad63192cadd9064432242f3a52329302f3",
+	To:    "0xb853d6a965fbc047aaa9f04d774d53861d7ed653",
+	Fee:   "21000000000000000000",
+	Date:  1532802660,
 	Type:   "transfer",
 	Status: "completed",
-	Block: 2533108,
-	Sequence: 18236731166897477649,
+	Block: 248545,
+	Sequence: 1532802660,
 	Meta:  models.Transfer{
-		Value: "17521910000000000000000",
+		Value: "51770000000000000000000",
 	},
 }
 
-var expectedTokenTrx = models.Tx{
-	ID:     "0x52ae85810f8568c0dc13bc8afd6f144d7a8e2bb949b76a5d14ab529c2438f32f",
+var expectedVeThorTrx = models.Tx{
+	ID:     "0xd989ab9e91e9af849666d507758f75ea2d4dcbe0a658284d8196ac527dbee181",
 	Coin:   coin.VET,
-	From:   "0xb69ded9f0da15d240ee6803dacd7fcf68744e8ff",
-	To:     "0x70e0a09fa23019f83b42f018891611935c1459d0",
+	From:   "0xb6b6c3ad63192cadd9064432242f3a52329302f3",
+	To:     "0xb853d6a965fbc047aaa9f04d774d53861d7ed653",
 	Fee:    "0",
-	Date:   1555964860,
+	Date:   1532802660,
 	Type:   "transfer",
 	Status: "completed",
-	Sequence: 2560732,
-	Block:  2560732,
+	Sequence: 1532802660,
+	Block:  248545,
 	Meta:  models.Transfer{
-		Value: "29286000000000000000",
+		Value: "21000000000000000000",
 	},
 }
 func TestNormalize(t *testing.T) {
-	const address = "0x7b41d6451eAA012C551b6ED3af9F871a00602E88"
-	var tx Tx
-	var r  TxId
-	var cl Clause
+	const address = "0xb853d6a965fbc047aaa9f04d774d53861d7ed653"
+	const VeThorContract = "0x0000000000000000000000000000456e65726779" 
+
+	var tests = []struct {
+		Tx 		  string
+		Receipt   string
+		Output    string
+		Address   string
+		Token     string
+		Expected  models.Tx
+	}{
+		{transferTrx, transferReceipt, transferOutput, address, "", expectedTransferTrx},
+		{transferTrx, transferReceipt, transferOutput, address, VeThorContract, expectedVeThorTrx},
+	}
+
+	for _, test := range tests {
+		var tx      Tx
+		var receipt TxReceipt
+		var output  TxReceiptOutput
+
+
+		tErr := json.Unmarshal([]byte(test.Tx), &tx)
+		if tErr != nil {
+			t.Fatal(tErr)
+		}
+
+		rErr  := json.Unmarshal([]byte(test.Receipt), &receipt)
+		if rErr != nil {
+			t.Fatal(rErr)
+		}
+
+		olErr := json.Unmarshal([]byte(test.Output), &output)
+		if olErr != nil {
+			t.Fatal(olErr)
+		}
+
+		var readyTx models.Tx
+		normTx, ok := Normalize(&tx, &receipt, &output, test.Address, test.Token)
+		if !ok {
+			t.Fatal("VeChain: Can't normalize transaction", readyTx)
+		}
+		readyTx = normTx
 	
-	tErr := json.Unmarshal([]byte(transferTrx), &tx)
-	if tErr != nil {
-		t.Fatal(tErr)
-	}
-
-	rErr  := json.Unmarshal([]byte(transactionId), &r)
-	if rErr != nil {
-		t.Fatal(rErr)
-	}
-
-	clErr := json.Unmarshal([]byte(clause), &cl)
-	if clErr != nil {
-		t.Fatal(clErr)
-	}
-
-	var readyTx models.Tx
-	normTx, ok := Normalize(&tx, &r, &cl, address)
-	if !ok {
-		t.Fatal("VeChain: Can't normalize transaction", readyTx)
-	}
-	readyTx = normTx
-
-	actual, err := json.Marshal(&readyTx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected, err := json.Marshal(&finalTx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !bytes.Equal(actual, expected) {
-		println(string(actual))
-		println(string(expected))
-		t.Error("Transactions not equal")
-	}
-}
-
-func TestNormalizeToken(t *testing.T) {
-	var tx Tx
-	var receipt TxReceipt
-	var out TxReceiptOutput
+		actual, err := json.Marshal(&readyTx)
+		if err != nil {
+			t.Fatal(err)
+		}
 	
-	tErr := json.Unmarshal([]byte(tokenTrx), &tx)
-	if tErr != nil {
-		t.Fatal(tErr)
-	}
-
-	rErr  := json.Unmarshal([]byte(transactionReceipt), &receipt)
-	if rErr != nil {
-		t.Fatal(rErr)
-	}
-
-	clErr := json.Unmarshal([]byte(output), &out)
-	if clErr != nil {
-		t.Fatal(clErr)
-	}
-
-	var readyTx models.Tx
-	normTx, ok := NormalizeToken(&out, &receipt)
-	if !ok {
-		t.Fatal("VeChain: Can't normalize transaction", readyTx)
-	}
-	readyTx = normTx
-
-	actual, err := json.Marshal(&readyTx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected, err := json.Marshal(&expectedTokenTrx)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !bytes.Equal(actual, expected) {
-		println(string(actual))
-		println(string(expected))
-		t.Error("Transactions not equal")
+		expected, err := json.Marshal(&test.Expected)
+		if err != nil {
+			t.Fatal(err)
+		}
+	
+		if !bytes.Equal(actual, expected) {
+			println(string(actual))
+			println(string(expected))
+			t.Error("Transactions not equal")
+		}
 	}
 }
