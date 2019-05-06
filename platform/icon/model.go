c035df81a74a6cdb477e1d1ec4e64348da7f1d09
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-05-05 17:20:34 -0700

    Update icon linting

--- platform/icon/model.go
+++ platform/icon/model.go
@@ -1,22 +1,24 @@
 package icon
 
+// Response describes the ripple transaction response
 type Response struct {
-	Data        []Tx  `json:data`
-	ListSize    uint64 `json:listSize`
-	TotalSize   uint64 `json:totalSize`
-	Result      string `json:result`
-	Description string `json:description`
+	Data        []Tx   `json:"data"`
+	ListSize    uint64 `json:"listSize"`
+	TotalSize   uint64 `json:"totalSize"`
+	Result      string `json:"result"`
+	Description string `json:"description"`
 }
 
+// Tx describes the ripple transaction
 type Tx struct {
-	TxHash     string `json:txHash`
-	Height     uint64 `json:height`
-	CreateDate string `json:createDate`
-	FromAddr   string `json:fromAddr`
-	ToAddr     string `json:toAddr`
-	TxType     string `json:txType`
-	DataType   string `json:dataType`
-	Amount     string `json:amount`
-	Fee        string `json:fee`
-	State	   uint64 `json:status`	
+	TxHash     string `json:"txHash"`
+	Height     uint64 `json:"height"`
+	CreateDate string `json:"createDate"`
+	FromAddr   string `json:"fromAddr"`
+	ToAddr     string `json:"toAddr"`
+	TxType     string `json:"txType"`
+	DataType   string `json:"dataType"`
+	Amount     string `json:"amount"`
+	Fee        string `json:"fee"`
+	State      uint64 `json:"status"`
 }
