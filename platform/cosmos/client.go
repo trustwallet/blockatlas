b5f20ccbcdecd982126ce9b808426ee06c9cad0e
James Scaur <james@txbatch.com>
2019-05-06 20:41:37 +0200

    Split up model

--- platform/cosmos/client.go
+++ platform/cosmos/client.go
@@ -1,6 +1,7 @@
 package cosmos
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -10,13 +11,15 @@ import (
 	"github.com/trustwallet/blockatlas/models"
 )
 
+// Client - the HTTP client
 type Client struct {
 	HTTPClient *http.Client
 	BaseURL    string
 }
 
-func (c *Client) GetTxsOfAddress(address string) (*Tx, error) {
-	uri := fmt.Sprintf("%s/address/txList?%s",
+// GetAddressTransactions - get all ATOM transactions for a given address, via sender
+func (c *Client) GetAddressTransactions(address string) (txs []Tx, err error) {
+	uri := fmt.Sprintf("%s/txs?%s",
 		c.BaseURL,
 		url.Values{
 			"sender": {address},
@@ -26,19 +29,9 @@ func (c *Client) GetTxsOfAddress(address string) (*Tx, error) {
 
 	res, err := c.HTTPClient.Get(uri)
 	if err != nil {
-		logrus.WithError(err).Error("Cosmos: Failed to get transactions")
-		return nil, models.ErrSourceConn
+		logrus.WithError(err).Errorf("Cosmos: Failed to get transactions for address %s", address)
 	}
 
-	switch res.StatusCode {
-	case http.StatusBadRequest, http.StatusNotFound:
-		logrus.WithError(err).Error("Cosmos: Bad request or 404 error")
-		return nil, models.ErrSourceConn
-	case http.StatusOK:
-		break
-	default:
-		return nil, fmt.Errorf("%s", res.Status)
-	}
-
-	return res, nil
+	err = json.NewDecoder(res.Body).Decode(&txs)
+	return
 }
