0f73a626646150ab49b969bb46c82c2dc1f2e20f
Mykola <3277207+kolya182@users.noreply.github.com>
2019-05-17 13:33:59 -0700

    IOTEX: Remove unused structure fields

--- platform/iotex/model.go
+++ platform/iotex/model.go
@@ -1,14 +1,12 @@
 package iotex
 
 type Response struct {
-	Total      uint64        `json:"total"`
 	ActionInfo []*ActionInfo `json:"actionInfo"`
 }
 
 type ActionInfo struct {
 	Action    *Action `json:"action"`
 	ActHash   string  `json:"actHash"`
-	BlkHash   string  `json:"blkHash"`
 	BlkHeight string  `json:"blkHeight"`
 	Sender    string  `json:"sender"`
 	GasFee    string  `json:"gasFee"`
@@ -17,20 +15,14 @@ type ActionInfo struct {
 
 type Action struct {
 	Core         *ActionCore `json:"core"`
-	SenderPubKey []byte      `json:"senderPubKey"`
-	Signature    []byte      `json:"signature"`
 }
 
 type ActionCore struct {
-	Version  uint32    `json:"version"`
 	Nonce    string    `json:"nonce"`
-	GasLimit string    `json:"gasLimit"`
-	GasPrice string    `json:"gasPrice"`
 	Transfer *Transfer `json:"transfer"`
 }
 
 type Transfer struct {
 	Amount    string   `json:"amount"`
 	Recipient string   `json:"recipient"`
-	Payload   []byte   `json:"payload"`
 }
