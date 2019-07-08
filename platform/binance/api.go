542b47f824c8ddacadf69c824a5cb6ac100e4dba
Mykola <kolya182@gmail.com>
2019-07-07 18:12:15 -0700

    DEX: Support multisend transactions

--- platform/binance/api.go
+++ platform/binance/api.go
@@ -16,6 +16,7 @@ type Platform struct {
 
 func (p *Platform) Init() error {
 	p.client.BaseURL = viper.GetString("binance.api")
+	p.client.RPCBaseURL = viper.GetString("binance.rpc")
 	p.client.HTTPClient = http.DefaultClient
 	return nil
 }
@@ -44,7 +45,7 @@ func (p *Platform) GetBlockByNumber(num int64) (*blockatlas.Block, error) {
 		return nil, err
 	}
 	// TODO: Only returns BNB transactions for now
-	txs := NormalizeTxs(srcTxs.Txs, "")
+	txs := NormalizeTxs(srcTxs.Txs, "", "", p)
 	return &blockatlas.Block{
 		Number: num,
 		Txs:    txs,
@@ -56,42 +57,47 @@ func (p *Platform) GetTxsByAddress(address string) (blockatlas.TxPage, error) {
 	return p.GetTokenTxsByAddress(address, "")
 }
 
-func (p *Platform) GetTokenTxsByAddress(address string, token string) (blockatlas.TxPage, error) {
+func (p *Platform) GetTokenTxsByAddress(address, token string) (blockatlas.TxPage, error) {
 	srcTxs, err := p.client.GetTxsOfAddress(address, token)
 	if err != nil {
 		return nil, err
 	}
-	return NormalizeTxs(srcTxs.Txs, token), nil
+	return NormalizeTxs(srcTxs.Txs, token, address, p), nil
 }
 
 // NormalizeTx converts a Binance transaction into the generic model
-func NormalizeTx(srcTx *Tx, token string) (tx blockatlas.Tx, ok bool) {
+func NormalizeTx(srcTx *Tx, token, address string, p *Platform) (tx blockatlas.Tx, ok bool) {
 	value := util.DecimalExp(string(srcTx.Value), 8)
 	fee := util.DecimalExp(string(srcTx.Fee), 8)
+	hash := srcTx.Hash
+	asset := srcTx.Asset
 
 	tx = blockatlas.Tx{
-		ID:    srcTx.Hash,
+		ID:    hash,
 		Coin:  coin.BNB,
 		Date:  srcTx.Timestamp / 1000,
-		From:  srcTx.FromAddr,
-		To:    srcTx.ToAddr,
 		Fee:   blockatlas.Amount(fee),
 		Block: srcTx.BlockHeight,
 		Memo:  srcTx.Memo,
 	}
 
 	// Condition for native transfer (BNB)
-	if srcTx.Asset == "BNB" && srcTx.Type == TRANSFER && token == "" {
+	if asset == "BNB" && srcTx.Type == TRANSFER && token == "" {
+		tx.From = srcTx.FromAddr
+		tx.To = srcTx.ToAddr
 		tx.Meta = blockatlas.Transfer{
 			Value: blockatlas.Amount(value),
 		}
+
 		return tx, true
 	}
 
 	// Condition for native token transfer
-	if srcTx.Asset == token && srcTx.Type == TRANSFER {
+	if asset == token && srcTx.Type == TRANSFER && srcTx.FromAddr != "" {
+		tx.From = srcTx.FromAddr
+		tx.To = srcTx.ToAddr
 		tx.Meta = blockatlas.NativeTokenTransfer{
-			TokenID:  srcTx.Asset,
+			TokenID:  asset,
 			Symbol:   srcTx.MappedAsset,
 			Value:    blockatlas.Amount(value),
 			Decimals: 8,
@@ -102,17 +108,94 @@ func NormalizeTx(srcTx *Tx, token string) (tx blockatlas.Tx, ok bool) {
 		return tx, true
 	}
 
+	// Conditin where explorer does not return sender/recepient for multisend transaction
+	if (srcTx.FromAddr == "" || srcTx.ToAddr == "") && srcTx.Type == TRANSFER {
+		println("hash :", hash)
+		receipt, _ := p.client.GetTransactionReceipt(hash)
+		zeroMsgValue := receipt.TxReceipts.Value.Msg[0].MsgValue
+		zeroInput := zeroMsgValue.Inputs[0]
+		outputs := zeroMsgValue.Outputs
+		zeroOutputAdress := outputs[0].Address
+
+		// Condition for native transfer in multisend transaction
+		if zeroInput.Coins[0].Denom == "BNB" {
+			if zeroInput.Address == address {
+				tx.From = address
+				tx.To = zeroOutputAdress  // Pick 0 index as main receipient
+				tx.Meta = blockatlas.Transfer{
+					Value: blockatlas.Amount(zeroInput.Coins[0].Amount),
+				}
+				return tx, true
+			}
+
+			coin := getCoinOutput(outputs, address)
+			tx.To = address
+			tx.From = zeroOutputAdress
+			tx.Meta = blockatlas.Transfer{
+				Value: blockatlas.Amount(coin.Amount),
+			}
+			return tx, true
+		}
+
+		// Condition for token_transfer in multisend transaction
+		if zeroInput.Coins[0].Denom != "BNB" {
+			if zeroInput.Address == address {
+				tx.From = address
+				tx.To = zeroOutputAdress  // Pick 0 index as main receipient
+				tx.Meta = blockatlas.TokenTransfer{
+					Name: "", // TODO: Replace with actual name
+					Symbol: zeroInput.Coins[0].Denom,
+					TokenID: "", // TODO: Replace with actual tokenID
+					Decimals: 8,
+					From: address,
+					To: zeroOutputAdress,
+					Value: blockatlas.Amount(zeroInput.Coins[0].Amount),
+				}
+				return tx, true
+			}
+
+			coin := getCoinOutput(outputs, address)
+			tx.From = zeroOutputAdress
+			tx.To = address
+			tx.Meta = blockatlas.TokenTransfer{
+				Name: "", // TODO: Replace with actual name
+				Symbol: coin.Denom,
+				TokenID: "", // TODO: Replace with actual tokenID
+				Decimals: 8,
+				From: zeroOutputAdress,
+				To: address,
+				Value: blockatlas.Amount(coin.Amount),
+
+			}
+
+			return tx, true
+		}
+	}
+	
+
 	return tx, false
 }
 
 // NormalizeTxs converts multiple Binance transactions
-func NormalizeTxs(srcTxs []Tx, token string) (txs []blockatlas.Tx) {
+func NormalizeTxs(srcTxs []Tx, token, address string, p *Platform) (txs []blockatlas.Tx) {
 	for _, srcTx := range srcTxs {
-		tx, ok := NormalizeTx(&srcTx, token)
+		tx, ok := NormalizeTx(&srcTx, token, address, p)
 		if !ok || len(txs) >= blockatlas.TxPerPage {
 			continue
 		}
 		txs = append(txs, tx)
 	}
 	return
+}
+
+func getCoinOutput(outputs []Output, address string) Coin {
+	var coin Coin
+	for _, out := range outputs {
+		if out.Address == address {
+			coin = out.Coins[0]
+			continue
+		}
+	}
+
+	return coin
 }
