2855bed349e5a87eda6487de08bbc132b71838f9
MadCake <133312+madcake@users.noreply.github.com>
2019-07-10 15:47:33 +0700

    Update collectibles model

--- collectibles.go
+++ collectibles.go
@@ -1,26 +1,26 @@
 package blockatlas
 
 type Collection struct {
-	Name string `json:"name"`
-	Symbol string `json:"symbol"`
-	ImageUrl string `json:"image_url"`
-	Description string `json:"description"`
-	External_Link string `json:"external_link"`
-	Total string `json:"total"`
-	Category_Address string `json:"category_address"`
-	Address string `json:"address"`
-	Version string `json:"version"`
-	Coin int `json:"coin"`
-	Type string `json:"type"`
+	Name            string `json:"name"`
+	Symbol          string `json:"symbol"`
+	ImageUrl        string `json:"image_url"`
+	Description     string `json:"description"`
+	ExternalLink    string `json:"external_link"`
+	Total           string `json:"total"`
+	CategoryAddress string `json:"category_address"`
+	Address         string `json:"address"`
+	Version         string `json:"version"`
+	Coin            int    `json:"coin"`
+	Type            string `json:"type"`
 }
 
 type Collectible struct {
-	Token_ID string `json:"token_id"`
-	Contract_Address string `json:"contract_address"`
-	Category string `json:"category"`
-	Image_URL string `json:"image_url"`
-	External_Link string `json:"external_link"`
-	Type string `json:"type"`
-	Description string `json:"type"`
-	Coin int `json:"coin"`
+	TokenID         string `json:"token_id"`
+	ContractAddress string `json:"contract_address"`
+	Category        string `json:"category"`
+	ImageURL        string `json:"image_url"`
+	ExternalLink    string `json:"external_link"`
+	Type            string `json:"type"`
+	Description     string `json:"description"`
+	Coin            int    `json:"coin"`
 }
