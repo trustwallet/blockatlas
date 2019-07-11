28ec0488e500c06150160cf3e818b426ade17396
Maksim Rasputin <133312+madcake@users.noreply.github.com>
2019-07-11 09:13:52 +0700

    Add block fetching for stellar (#154)
    * Add block fetching for stellar
    
    * Fix logic in NormalizePayments
    
    * Change Sprintf

--- platform/stellar/client.go
+++ platform/stellar/client.go
@@ -8,14 +8,17 @@ import (
 )
 
 type Client struct {
-	HTTP       *http.Client
-	API        string
+	HTTP *http.Client
+	API  string
 }
 
 func (c *Client) GetTxsOfAddress(address string) (txs []Payment, err error) {
 	path := fmt.Sprintf("%s/accounts/%s/payments?order=desc&limit=25",
 		c.API, url.PathEscape(address))
+	return c.getTxs(path)
+}
 
+func (c *Client) getTxs(path string) (txs []Payment, err error) {
 	res, err := http.Get(path)
 	if err != nil {
 		return nil, err
@@ -30,4 +33,50 @@ func (c *Client) GetTxsOfAddress(address string) (txs []Payment, err error) {
 	}
 
 	return payments.Embedded.Records, nil
+}
+
+func (c *Client) CurrentBlockNumber() (num int64, err error) {
+	path := fmt.Sprintf("%s/ledgers?order=desc&limit=1", c.API)
+	res, err := http.Get(path)
+	if err != nil {
+		return num, err
+	}
+	defer res.Body.Close()
+	var ledgers LedgersPage
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&ledgers)
+	if err != nil {
+		return num, err
+	}
+
+	return ledgers.Embedded.Records[0].Sequence, nil
+}
+
+func (c *Client) GetBlockByNumber(num int64) (block *Block, err error) {
+	ledger, err := c.getLedger(num)
+	if err != nil {
+		return nil, err
+	}
+	path := fmt.Sprintf("%s/ledgers/%d/payments?limit=100&order=desc", c.API, num)
+	payments, err := c.getTxs(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Block{Ledger: *ledger, Payments: payments}, nil
+}
+
+func (c *Client) getLedger(num int64) (ledger *Ledger, err error) {
+	path := fmt.Sprintf("%s/ledgers/%d", c.API, num)
+	res, err := http.Get(path)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&ledger)
+	if err != nil {
+		return nil, err
+	}
+	return ledger, nil
 }
