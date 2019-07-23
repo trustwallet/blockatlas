8081c0e1d6ca5ea763abc763ef3ba541b628aa4c
MadCake <133312+madcake@users.noreply.github.com>
2019-07-24 00:49:54 +0700

    Clean code

--- observer/storage/redis/storage_test.go
+++ observer/storage/redis/storage_test.go
@@ -2,23 +2,14 @@ package redis
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/trustwallet/blockatlas/coin"
-
 	"testing"
 )
 
-const ethCoin = coin.ETH
-const addr1 = "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
-
 var webhook1Addr = "http://apple.com/push"
-var webhook1 = []string{webhook1Addr}
-
-const addr2 = "0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8"
 
 var webhook2Addr = "http://trustwallet.com/webhook"
-var webhook2 = []string{webhook2Addr}
 
-func TestRedisStorage_Add(t *testing.T) {
+func TestRedisStorage_WebhookAdd(t *testing.T) {
 	result := add([]string{webhook1Addr}, []string{webhook2Addr})
 
 	assert.Equal(t, result, []string{webhook1Addr, webhook2Addr})
