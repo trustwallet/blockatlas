2b5a00a14052bd19cc2063d725afd714b29f21ee
MadCake <133312+madcake@users.noreply.github.com>
2019-07-29 00:20:23 +0700

    Out set to separate file

--- tx.go
+++ tx.go
@@ -1,7 +1,5 @@
 package blockatlas
 
-import "sync"
-
 // Types of transaction metadata
 const (
 	TxTransfer            = "transfer"
@@ -147,39 +145,4 @@ func (t *Tx) GetAddresses() (addresses []string) {
 	default:
 		return addresses
 	}
-}
-
-type TxSet struct {
-	items map[Tx]bool
-	lock  sync.RWMutex
-}
-
-// Add adds a new element to the Set. Returns a pointer to the Set.
-func (s *TxSet) Add(t Tx) *TxSet {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	if s.items == nil {
-		s.items = make(map[Tx]bool)
-	}
-	_, ok := s.items[t]
-	if !ok {
-		s.items[t] = true
-	}
-	return s
-}
-
-func (s *TxSet) Txs() []Tx {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
-	items := []Tx{}
-	for i := range s.items {
-		items = append(items, i)
-	}
-	return items
-}
-
-func (s *TxSet) Size() int {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
-	return len(s.items)
 }
