49aee3184333913544f0006028b60a71cddbed9a
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-07-30 13:25:49 -0700

    Add generic client to simplify logic to fetch data (#220)
    * Add generic client to simplify logic to fetch data
    
    * Update client.go
    
    * Update client.go
    
    * Update client.go

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func Request(c *http.Client, base string, path string, params url.Values, result interface{}) error {
+
+	uri := fmt.Sprintf("%s/%s?%s",
+		base,
+		path,
+		params.Encode())
+
+	res, err := c.Get(uri)
+	if err != nil {
+		return err
+	}
+
+	err = json.NewDecoder(res.Body).Decode(result)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- platform/waves/client.go
+++ platform/waves/client.go
@@ -1,9 +1,10 @@
 package waves
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/trustwallet/blockatlas/client"
 	"net/http"
+	"net/url"
 )
 
 type Client struct {
@@ -12,64 +13,30 @@ type Client struct {
 }
 
 func (c *Client) GetTxs(address string, limit int) ([]Transaction, error) {
-	uri := fmt.Sprintf("%s/transactions/address/%s/limit/%d",
-		c.URL,
-		address,
-		limit)
-	req, _ := http.NewRequest("GET", uri, nil)
+	path := fmt.Sprintf("transactions/address/%s/limit/%d", address, limit)
 
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
+	txs := make([][]Transaction, 0)
+	err := client.Request(c.HTTPClient, c.URL, path, url.Values{}, &txs)
 
-	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("http %s", res.Status)
-	}
-
-	txsArrays := new([][]Transaction)
-	err = json.NewDecoder(res.Body).Decode(txsArrays)
-	if err != nil {
-		return nil, err
-	}
-	txsObj := *txsArrays
-
-	return txsObj[0], nil
-}
-
-func (c *Client) GetBlockByNumber(num int64) (*Block, error) {
-	uri := fmt.Sprintf("%s/blocks/at/%d", c.URL, num)
-
-	res, err := c.HTTPClient.Get(uri)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	stx := new(Block)
-	err = json.NewDecoder(res.Body).Decode(stx)
-	if err != nil {
-		return nil, err
-	}
-
-	return stx, nil
-}
-
-func (c *Client) GetCurrentBlock() (*CurrentBlock, error) {
-	uri := fmt.Sprintf("%s/blocks/height", c.URL)
-
-	res, err := c.HTTPClient.Get(uri)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	var currentBlock CurrentBlock
-	err = json.NewDecoder(res.Body).Decode(&currentBlock)
-	if err != nil {
-		return nil, err
+	if len(txs) > 0 {
+		return txs[0], err
 	} else {
-		return &currentBlock, nil
+		return []Transaction{}, err
 	}
+}
+
+func (c *Client) GetBlockByNumber(num int64) (block *Block, err error) {
+	path := fmt.Sprintf("blocks/at/%d", num)
+
+	err = client.Request(c.HTTPClient, c.URL, path, url.Values{}, &block)
+
+	return block, err
+}
+
+func (c *Client) GetCurrentBlock() (block *CurrentBlock, err error) {
+	path := "blocks/height"
+
+	err = client.Request(c.HTTPClient, c.URL, path, url.Values{}, &block)
+
+	return block, err
 }
