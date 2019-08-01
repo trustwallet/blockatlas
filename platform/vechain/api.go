e5b07edd7cc510c57c4925388987603e39e02cde
hewig <360470+hewigovens@users.noreply.github.com>
2019-08-02 00:01:30 +0800

    Fix vechain tx status (#228)
    * Fix vechain tx status
    
    * Fix codacy warnings

--- platform/vechain/api.go
+++ platform/vechain/api.go
@@ -151,6 +151,7 @@ func NormalizeTransfer(receipt *TransferReceipt, clause *Clause) (tx blockatlas.
 		Date:     int64(time),
 		Type:     blockatlas.TxTransfer,
 		Block:    block,
+		Status:   receipt.Receipt.Status(),
 		Sequence: block,
 		Meta: blockatlas.Transfer{
 			Value:    blockatlas.Amount(valueBase10),
@@ -184,6 +185,7 @@ func NormalizeTokenTransfer(t *TokenTransfer, receipt *TransferReceipt) (tx bloc
 		Date:     t.Timestamp,
 		Type:     blockatlas.TxNativeTokenTransfer,
 		Block:    block,
+		Status:   receipt.Receipt.Status(),
 		Sequence: block,
 		Meta: blockatlas.NativeTokenTransfer{
 			Name:     "VeThor Token",
