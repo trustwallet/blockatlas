b54ac19dea46db470275ef92f0c33d2eb180fa39
Artem Goryunov <725247+ArtemGoryunov@users.noreply.github.com>
2019-08-10 00:58:12 +0300

    Support Nebulas (NAS) transactions (#247)
    * Support Nebulas (NAS) transactions
    
    * Requested changes were made

---
+++ platform/nebulas/api_test.go
@@ -0,0 +1,71 @@
+package nebulas
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/trustwallet/blockatlas"
+	"github.com/trustwallet/blockatlas/coin"
+	"testing"
+)
+
+const transferSrc = `
+{
+  "hash": "96bd280d60447b7dbcdb3fa76a99856e0422a76304e9d01d0c87e1dfceb6d952",
+  "block": {
+    "height": 2848548
+  },
+  "from": {
+    "hash": "n1Yv9xJJcH4UjoJPVDGdUCL2CxK29asFuyV"
+  },
+  "to": {
+    "hash": "n1TFrmLUDTe5ggQaWJiXHSqNSRzKYdaV6hQ"
+  },
+  "value": "500000000000000000",
+  "nonce": 7,
+  "status": 1,
+  "timestamp": 1565213205000,
+  "type": "binary",
+  "currentTimestamp": 1565361175536,
+  "txFee": "400000000000000"
+}`
+
+
+var transferDst = blockatlas.Tx{
+	ID:     "96bd280d60447b7dbcdb3fa76a99856e0422a76304e9d01d0c87e1dfceb6d952",
+	Coin:   coin.NAS,
+	From:   "n1Yv9xJJcH4UjoJPVDGdUCL2CxK29asFuyV",
+	To:     "n1TFrmLUDTe5ggQaWJiXHSqNSRzKYdaV6hQ",
+	Fee:    "400000000000000",
+	Sequence: 7,
+	Date:   1565213205,
+	Block:  2848548,
+	Status: blockatlas.StatusCompleted,
+	Meta: blockatlas.Transfer{
+		Value: "500000000000000000",
+	},
+}
+
+func TestNormalize(t *testing.T) {
+	var srcTx Transaction
+	err := json.Unmarshal([]byte(transferSrc), &srcTx)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	resTx := NormalizeTx(srcTx)
+
+	resJSON, err := json.Marshal(&resTx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dstJSON, err := json.Marshal(&transferDst)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(resJSON, dstJSON) {
+		t.Error("tx don't equal")
+	}
+}
---
+++ platform/nebulas/client.go
@@ -0,0 +1,38 @@
+package nebulas
+
+import (
+	"fmt"
+	"github.com/trustwallet/blockatlas/client"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+const TxType = "binary"
+
+type Client struct {
+	HTTPClient *http.Client
+	URL        string
+}
+
+func (c *Client) GetTxs(address string, page int) ([]Transaction, error) {
+
+	path := fmt.Sprintf("tx")
+	var response Response
+	values := url.Values{
+		"a": {address},
+		"p": {strconv.Itoa(page)},
+	}
+	if err := client.Request(c.HTTPClient, c.URL, path, values, &response); err != nil {
+		return nil, err
+	}
+
+	var result []Transaction
+	for _, tx := range response.Data.TxnList {
+		if tx.Type == TxType {
+			result = append(result, tx)
+		}
+	}
+
+	return result, nil
+}
