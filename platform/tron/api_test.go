d40ccb1c4d0b0b2a8d3cffe0bfa9813520ac3cec
Mykola <kolya182@gmail.com>
2019-09-12 16:04:39 -0700

    Add token test

--- platform/tron/api_test.go
+++ platform/tron/api_test.go
@@ -1,7 +1,6 @@
 package tron
 
 import (
-	"bytes"
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"github.com/trustwallet/blockatlas"
@@ -30,6 +29,28 @@ const transferSrc = `
 }
 `
 
+const tokenTransferSrc = `
+{
+	"block_timestamp": 1564797900000,
+	"raw_data": {
+		"contract": [
+			{
+				"parameter": {
+					"value": {
+						"amount": 2776267,
+						"asset_name": "1002000",
+						"owner_address": "4182dd6b9966724ae2fdc79b416c7588da67ff1b35",
+						"to_address": "410583a68a3bcd86c25ab1bee482bac04a216b0261"
+					}
+				},
+				"type": "TransferAssetContract"
+			}
+		]
+	},
+	"txID": "24a10f7a503e78adc0d7e380b68005531b09e16b9e3f7b524e33f40985d287df"
+}
+`
+
 var transferDst = blockatlas.Tx{
 	ID:     "24a10f7a503e78adc0d7e380b68005531b09e16b9e3f7b524e33f40985d287df",
 	Coin:   coin.TRX,
@@ -44,14 +65,27 @@ var transferDst = blockatlas.Tx{
 	},
 }
 
-var tokenDst = blockatlas.Token{
-	Name:     "Test",
-	Symbol:   "TST",
-	Decimals: 8,
-	TokenID:  "1",
-	Coin:     195,
-	Type:     "TRC10",
+var tokenTransferDst = blockatlas.Tx{
+	ID:     "24a10f7a503e78adc0d7e380b68005531b09e16b9e3f7b524e33f40985d287df",
+	Coin:   coin.TRX,
+	From:   "TMuA6YqfCeX8EhbfYEg5y7S4DqzSJireY9",
+	To:     "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+	Fee:    "0", // TODO
+	Date:   1564797900,
+	Block:  0, // TODO
+	Status: blockatlas.StatusCompleted,
+	Meta: blockatlas.TokenTransfer{
+		Name: "BitTorrent",
+		Symbol: "BTT",
+		TokenID: "1002000",
+		Decimals: 6,
+		Value: "2776267",
+		From: "TMuA6YqfCeX8EhbfYEg5y7S4DqzSJireY9",
+		To: "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+	},
 }
+
+var assetInfo = AssetInfo{ Name: "BitTorrent", Symbol: "BTT", Decimals: 6, ID: "1002000" }
 
 type test struct {
 	name        string
@@ -67,6 +101,40 @@ func TestNormalize(t *testing.T) {
 	})
 }
 
+func TestNormalizeTokenTransfer(t *testing.T) {
+	testNormalizeTokenTransfer(t, &test{
+		name:        "token transfer",
+		apiResponse: tokenTransferSrc,
+		expected:    &tokenTransferDst,
+	})
+}
+
+func testNormalizeTokenTransfer(t *testing.T, _test *test) {
+	var srcTx Tx
+	err := json.Unmarshal([]byte(_test.apiResponse), &srcTx)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	res, err := NormalizeTokenTransfer(&srcTx, assetInfo)
+	if err != nil {
+		t.Errorf("%s: tx could not be normalized", _test.name)
+		return
+	}
+
+	actual, err := json.Marshal(&res)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := json.Marshal(_test.expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
 func testNormalize(t *testing.T, _test *test) {
 	var srcTx Tx
 	err := json.Unmarshal([]byte(_test.apiResponse), &srcTx)
@@ -80,28 +148,31 @@ func testNormalize(t *testing.T, _test *test) {
 		return
 	}
 
-	resJSON, err := json.Marshal(&res)
+	actual, err := json.Marshal(&res)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	dstJSON, err := json.Marshal(&transferDst)
+	expected, err := json.Marshal(&transferDst)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !bytes.Equal(resJSON, dstJSON) {
-		println(string(resJSON))
-		println(string(dstJSON))
-		t.Errorf("%s: tx don't equal", _test.name)
-	}
+	assert.Equal(t, expected, actual)
+}
+
+var tokenDst = blockatlas.Token{
+	Name:     "Test",
+	Symbol:   "TST",
+	Decimals: 8,
+	TokenID:  "1",
+	Coin:     195,
+	Type:     "TRC10",
 }
 
 func TestNormalizeToken(t *testing.T) {
 	asset := AssetInfo{Name: "Test", Symbol: "TST", ID: "1", Decimals: 8}
-
 	actual := NormalizeToken(asset)
-
 	assert.Equal(t, tokenDst, actual)
 }
 
@@ -113,7 +184,7 @@ func TestNormalizeValidator(t *testing.T) {
 		ID:     "TGzz8gjYiYRqpfmDwnLxfgPuLVNmpCswVp",
 		Status: true,
 		Reward: blockatlas.StakingReward{
-			Annual,
+			Annual: Annual,
 		},
 	}
 	assert.Equal(t, expected, actual)
