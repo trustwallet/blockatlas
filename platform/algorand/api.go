debb99e69c886a7f5d8cd0cafd59b8a853fba854
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-09-25 00:01:14 -0700

    Update api.go

--- platform/algorand/api.go
+++ platform/algorand/api.go
@@ -2,9 +2,7 @@ package algorand
 
 import (
 	"github.com/spf13/viper"
-	"github.com/trustwallet/blockatlas"
 	"github.com/trustwallet/blockatlas/coin"
-	"strconv"
 )
 
 type Platform struct {
@@ -37,48 +35,48 @@ func (p *Platform) Coin() coin.Coin {
 //	}, nil
 //}
 
-func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err error) {
-	return page, err
-
-	//txs, err := p.client.GetTxsOfAddress(address)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//return NormalizeTxs(txs), nil
-}
-
-func NormalizeTxs(txs []Transaction) []blockatlas.Tx {
-	result := make([]blockatlas.Tx, 0)
-
-	for _, tx := range txs {
-		if normalized, ok := Normalize(tx); ok {
-			result = append(result, normalized)
-		}
-	}
-
-	return result
-}
-
-func Normalize(tx Transaction) (result blockatlas.Tx, ok bool) {
-
-	if tx.Type != TransactionTypePay {
-		return result, false
-	}
-
-	return blockatlas.Tx{
-		ID:     tx.Hash,
-		Coin:   coin.ALGO,
-		From:   tx.From,
-		To:     tx.Payment.To,
-		Fee:    blockatlas.Amount(strconv.Itoa(int(tx.Fee))),
-		Date:   0,
-		Block:  uint64(tx.Round),
-		Status: blockatlas.StatusCompleted,
-		Type:   blockatlas.TxTransfer,
-		Meta: blockatlas.Transfer{
-			Value:    blockatlas.Amount(strconv.Itoa(int(tx.Payment.Amount))),
-			Symbol:   coin.Coins[coin.ALGO].Symbol,
-			Decimals: coin.Coins[coin.ALGO].Decimals,
-		},
-	}, true
-}
+//func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err error) {
+//	return page, err
+//
+//	//txs, err := p.client.GetTxsOfAddress(address)
+//	//if err != nil {
+//	//	return nil, err
+//	//}
+//	//return NormalizeTxs(txs), nil
+//}
+//
+//func NormalizeTxs(txs []Transaction) []blockatlas.Tx {
+//	result := make([]blockatlas.Tx, 0)
+//
+//	for _, tx := range txs {
+//		if normalized, ok := Normalize(tx); ok {
+//			result = append(result, normalized)
+//		}
+//	}
+//
+//	return result
+//}
+//
+//func Normalize(tx Transaction) (result blockatlas.Tx, ok bool) {
+//
+//	if tx.Type != TransactionTypePay {
+//		return result, false
+//	}
+//
+//	return blockatlas.Tx{
+//		ID:     tx.Hash,
+//		Coin:   coin.ALGO,
+//		From:   tx.From,
+//		To:     tx.Payment.To,
+//		Fee:    blockatlas.Amount(strconv.Itoa(int(tx.Fee))),
+//		Date:   0,
+//		Block:  uint64(tx.Round),
+//		Status: blockatlas.StatusCompleted,
+//		Type:   blockatlas.TxTransfer,
+//		Meta: blockatlas.Transfer{
+//			Value:    blockatlas.Amount(strconv.Itoa(int(tx.Payment.Amount))),
+//			Symbol:   coin.Coins[coin.ALGO].Symbol,
+//			Decimals: coin.Coins[coin.ALGO].Decimals,
+//		},
+//	}, true
+//}
