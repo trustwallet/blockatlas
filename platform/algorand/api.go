2c1df42b8f7908bee9a7d7a6012ed53c18864952
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-09-25 00:25:17 -0700

    Update api.go

--- platform/algorand/api.go
+++ platform/algorand/api.go
@@ -2,8 +2,8 @@ package algorand
 
 import (
 	//"github.com/spf13/viper"
-	"github.com/trustwallet/blockatlas"
 	"github.com/trustwallet/blockatlas/coin"
+	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 )
 
 type Platform struct {
@@ -38,7 +38,6 @@ func (p *Platform) Coin() coin.Coin {
 
 func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err error) {
 	return page, err
-
 	//txs, err := p.client.GetTxsOfAddress(address)
 	//if err != nil {
 	//	return nil, err
