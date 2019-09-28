021b5b348ee4fc52bf6d91139d5b020b269bce4f
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-09-27 18:50:49 -0700

    Add algorand transactions

--- platform/algorand/api.go
+++ platform/algorand/api.go
@@ -1,17 +1,19 @@
 package algorand
 
 import (
+	"github.com/spf13/viper"
 	//"github.com/spf13/viper"
 	"github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
+	"strconv"
 )
 
 type Platform struct {
-	//client Client
+	client Client
 }
 
 func (p *Platform) Init() error {
-	//p.client = InitClient(viper.GetString("algorand.api"))
+	p.client = InitClient(viper.GetString("algorand.api"))
 	return nil
 }
 
@@ -19,65 +21,62 @@ func (p *Platform) Coin() coin.Coin {
 	return coin.Coins[coin.ALGO]
 }
 
-//TODO: https://github.com/trustwallet/blockatlas/issues/373
-//func (p *Platform) CurrentBlockNumber() (int64, error) {
-//	return p.client.GetLatestBlock()
-//}
-//
-//func (p *Platform) GetBlockByNumber(num int64) (*blockatlas.Block, error) {
-//	txs, err := p.client.GetTxsInBlock(num)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return &blockatlas.Block{
-//		Number: num,
-//		Txs:    NormalizeTxs(txs),
-//	}, nil
-//}
-
-func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err error) {
-	return page, err
-	//txs, err := p.client.GetTxsOfAddress(address)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//return NormalizeTxs(txs), nil
+func (p *Platform) CurrentBlockNumber() (int64, error) {
+	return p.client.GetLatestBlock()
 }
 
-//
-//func NormalizeTxs(txs []Transaction) []blockatlas.Tx {
-//	result := make([]blockatlas.Tx, 0)
-//
-//	for _, tx := range txs {
-//		if normalized, ok := Normalize(tx); ok {
-//			result = append(result, normalized)
-//		}
-//	}
-//
-//	return result
-//}
-//
-//func Normalize(tx Transaction) (result blockatlas.Tx, ok bool) {
-//
-//	if tx.Type != TransactionTypePay {
-//		return result, false
-//	}
-//
-//	return blockatlas.Tx{
-//		ID:     tx.Hash,
-//		Coin:   coin.ALGO,
-//		From:   tx.From,
-//		To:     tx.Payment.To,
-//		Fee:    blockatlas.Amount(strconv.Itoa(int(tx.Fee))),
-//		Date:   0,
-//		Block:  uint64(tx.Round),
-//		Status: blockatlas.StatusCompleted,
-//		Type:   blockatlas.TxTransfer,
-//		Meta: blockatlas.Transfer{
-//			Value:    blockatlas.Amount(strconv.Itoa(int(tx.Payment.Amount))),
-//			Symbol:   coin.Coins[coin.ALGO].Symbol,
-//			Decimals: coin.Coins[coin.ALGO].Decimals,
-//		},
-//	}, true
-//}
+func (p *Platform) GetBlockByNumber(num int64) (*blockatlas.Block, error) {
+	txs, err := p.client.GetTxsInBlock(num)
+	if err != nil {
+		return nil, err
+	}
+
+	return &blockatlas.Block{
+		Number: num,
+		Txs:    NormalizeTxs(txs),
+	}, nil
+}
+
+func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err error) {
+	txs, err := p.client.GetTxsOfAddress(address)
+	if err != nil {
+		return nil, err
+	}
+	return NormalizeTxs(txs), nil
+}
+
+func NormalizeTxs(txs []Transaction) []blockatlas.Tx {
+	result := make([]blockatlas.Tx, 0)
+
+	for _, tx := range txs {
+		if normalized, ok := Normalize(tx); ok {
+			result = append(result, normalized)
+		}
+	}
+
+	return result
+}
+
+func Normalize(tx Transaction) (result blockatlas.Tx, ok bool) {
+
+	if tx.Type != TransactionTypePay {
+		return result, false
+	}
+
+	return blockatlas.Tx{
+		ID:     tx.Hash,
+		Coin:   coin.ALGO,
+		From:   tx.From,
+		To:     tx.Payment.To,
+		Fee:    blockatlas.Amount(strconv.Itoa(int(tx.Fee))),
+		Date:   int64(tx.Timestamp),
+		Block:  uint64(tx.Round),
+		Status: blockatlas.StatusCompleted,
+		Type:   blockatlas.TxTransfer,
+		Meta: blockatlas.Transfer{
+			Value:    blockatlas.Amount(strconv.Itoa(int(tx.Payment.Amount))),
+			Symbol:   coin.Coins[coin.ALGO].Symbol,
+			Decimals: coin.Coins[coin.ALGO].Decimals,
+		},
+	}, true
+}
--- platform/algorand/api_test.go
+++ platform/algorand/api_test.go
@@ -1,78 +1,76 @@
 package algorand
 
-//TODO: https://github.com/trustwallet/blockatlas/issues/373
-//
-//import (
-//	"encoding/json"
-//	"github.com/stretchr/testify/assert"
-//	"github.com/trustwallet/blockatlas"
-//	"github.com/trustwallet/blockatlas/coin"
-//	"testing"
-//)
-//
-//const (
-//	transfer = `
-//{
-//   "transactions":[
-//      {
-//         "type":"pay",
-//         "tx":"C2LK3CGBPIGERLPFUXE6INSBJGHOXU7YZMEGELWMVSBASFJYOOQQ",
-//         "from":"5TSQNIL54GB545B3WLC6OVH653SHAELMHU6MSVNGTUNMOEHAMWG7EC3AA4",
-//         "fee":1000,
-//         "first-round":2031300,
-//         "last-round":2031749,
-//         "noteb64":"6OZ0TFd0HPw=",
-//         "round":2031351,
-//         "poolerror":"",
-//         "payment":{
-//            "to":"4EZFQABCVQTHQCK3HQBIYGC4NV2VM42FZHEFTVH77ROG4ZGREC6Y7V5T2U",
-//            "close":"",
-//            "closeamount":0,
-//            "amount":1,
-//            "torewards":3237690,
-//            "closerewards":0
-//         },
-//         "fromrewards":0,
-//         "genesisID":"mainnet-v1.0",
-//         "genesishashb64":"wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=",
-//         "group":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
-//      }
-//   ]
-//}
-//`
-//)
-//
-//var expected = []*blockatlas.Tx{
-//	{
-//		ID:     "C2LK3CGBPIGERLPFUXE6INSBJGHOXU7YZMEGELWMVSBASFJYOOQQ",
-//		Coin:   coin.ALGO,
-//		From:   "5TSQNIL54GB545B3WLC6OVH653SHAELMHU6MSVNGTUNMOEHAMWG7EC3AA4",
-//		To:     "4EZFQABCVQTHQCK3HQBIYGC4NV2VM42FZHEFTVH77ROG4ZGREC6Y7V5T2U",
-//		Fee:    blockatlas.Amount("1000"),
-//		Date:   int64(0),
-//		Block:  2031351,
-//		Status: blockatlas.StatusCompleted,
-//		Type:   blockatlas.TxTransfer,
-//		Meta: blockatlas.Transfer{
-//			Value:    blockatlas.Amount("1"),
-//			Symbol:   "ALGO",
-//			Decimals: 6,
-//		},
-//	},
-//	nil,
-//	nil,
-//}
-//
-//func TestNormalize(t *testing.T) {
-//	a := assert.New(t)
-//
-//	var act TransactionsResponse
-//	a.NoError(json.Unmarshal([]byte(transfer), &act))
-//	a.Equal(1, len(act.Transactions))
-//
-//	for i, v := range act.Transactions {
-//		tx, ok := Normalize(v)
-//		assert.True(t, ok)
-//		assert.Equal(t, expected[i], &tx)
-//	}
-//}
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/trustwallet/blockatlas/coin"
+	"github.com/trustwallet/blockatlas/pkg/blockatlas"
+	"testing"
+)
+
+const (
+	transfer = `
+{
+  "transactions":[
+     {
+        "type":"pay",
+        "tx":"C2LK3CGBPIGERLPFUXE6INSBJGHOXU7YZMEGELWMVSBASFJYOOQQ",
+        "from":"5TSQNIL54GB545B3WLC6OVH653SHAELMHU6MSVNGTUNMOEHAMWG7EC3AA4",
+        "fee":1000,
+        "first-round":2031300,
+        "last-round":2031749,
+        "noteb64":"6OZ0TFd0HPw=",
+        "round":2031351,
+        "poolerror":"",
+        "payment":{
+           "to":"4EZFQABCVQTHQCK3HQBIYGC4NV2VM42FZHEFTVH77ROG4ZGREC6Y7V5T2U",
+           "close":"",
+           "closeamount":0,
+           "amount":1,
+           "torewards":3237690,
+           "closerewards":0
+        },
+        "fromrewards":0,
+        "genesisID":"mainnet-v1.0",
+        "genesishashb64":"wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=",
+        "group":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+     }
+  ]
+}
+`
+)
+
+var expected = []*blockatlas.Tx{
+	{
+		ID:     "C2LK3CGBPIGERLPFUXE6INSBJGHOXU7YZMEGELWMVSBASFJYOOQQ",
+		Coin:   coin.ALGO,
+		From:   "5TSQNIL54GB545B3WLC6OVH653SHAELMHU6MSVNGTUNMOEHAMWG7EC3AA4",
+		To:     "4EZFQABCVQTHQCK3HQBIYGC4NV2VM42FZHEFTVH77ROG4ZGREC6Y7V5T2U",
+		Fee:    blockatlas.Amount("1000"),
+		Date:   int64(0),
+		Block:  2031351,
+		Status: blockatlas.StatusCompleted,
+		Type:   blockatlas.TxTransfer,
+		Meta: blockatlas.Transfer{
+			Value:    blockatlas.Amount("1"),
+			Symbol:   "ALGO",
+			Decimals: 6,
+		},
+	},
+	nil,
+	nil,
+}
+
+func TestNormalize(t *testing.T) {
+	a := assert.New(t)
+
+	var act TransactionsResponse
+	a.NoError(json.Unmarshal([]byte(transfer), &act))
+	a.Equal(1, len(act.Transactions))
+
+	for i, v := range act.Transactions {
+		tx, ok := Normalize(v)
+		assert.True(t, ok)
+		assert.Equal(t, expected[i], &tx)
+	}
+}
