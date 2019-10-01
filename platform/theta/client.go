ba4b8166fa67b8a0319a3d6ed8334987010ebabd
Danilo Pantani <danpantani@gmail.com>
2019-10-01 08:24:34 -0300

    refactor tetha client

--- platform/theta/client.go
+++ platform/theta/client.go
@@ -1,41 +1,27 @@
 package theta
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/trustwallet/blockatlas/pkg/errors"
-	"io/ioutil"
-	"net/http"
+	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"net/url"
 )
 
-// Client is used to request data from the THETA blockchain over Theta Explorer
 type Client struct {
-	HTTPClient *http.Client
-	BaseURL    string
+	blockatlas.Request
 }
 
-func (c *Client) FetchAddressTransactions(address string) (txs []Tx, err error) {
+func (c *Client) FetchAddressTransactions(address string) ([]Tx, error) {
+	query := url.Values{
+		"type":        {"2"},
+		"pageNumber":  {"1"},
+		"limitNumber": {"100"},
+		"isEqualType": {"true"},
+	}
+	uri := fmt.Sprintf("/accounttx/%s", url.PathEscape(address))
 	var transfers AccountTxList
-
-	uri := fmt.Sprintf("%s/accounttx/%s?type=2&pageNumber=1&limitNumber=100&isEqualType=true",
-		c.BaseURL, url.PathEscape(address))
-
-	resp, err := c.HTTPClient.Get(uri)
+	err := c.Get(&transfers, uri, query)
 	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformRequest, errors.Params{"url": uri})
+		return nil, err
 	}
-	defer resp.Body.Close()
-
-	body, errBody := ioutil.ReadAll(resp.Body)
-	if errBody != nil {
-		return nil, errors.E(errBody, errors.TypePlatformUnmarshal, errors.Params{"url": uri})
-	}
-
-	errUnm := json.Unmarshal(body, &transfers)
-	if errUnm != nil {
-		return nil, errors.E(errUnm, errors.TypePlatformRequest, errors.Params{"url": uri})
-	}
-
 	return transfers.Body, nil
 }
