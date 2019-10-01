dfc7ba2aea13048625400d3d7ff359983e58dfe3
Danilo Pantani <danpantani@gmail.com>
2019-10-01 19:23:30 -0300

    refactor zilliqa client (#420)

--- platform/zilliqa/client.go
+++ platform/zilliqa/client.go
@@ -1,112 +1,16 @@
 package zilliqa
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"github.com/mitchellh/mapstructure"
-	"github.com/trustwallet/blockatlas/pkg/errors"
-	"github.com/ybbus/jsonrpc"
-	"io/ioutil"
-	"net/http"
-	"strconv"
+	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 )
 
 type Client struct {
-	HTTPClient *http.Client
-	RPCClient  jsonrpc.RPCClient
-	BaseURL    string
-	APIKey     string
+	blockatlas.Request
 }
 
-func (c *Client) newRequest(method, path string) (*http.Request, error) {
-	url := fmt.Sprintf("%s%s", c.BaseURL, path)
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformRequest, errors.Params{"path": path})
-	}
-	req.Header.Set("X-APIKEY", c.APIKey)
-	return req, nil
-}
-
-func (c *Client) GetBlockchainInfo() (*ChainInfo, error) {
-	var info *ChainInfo
-	err := c.RPCClient.CallFor(&info, "GetBlockchainInfo")
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformRequest)
-	}
-	return info, nil
-}
-
-func (c *Client) GetTxInBlock(number int64) ([]Tx, error) {
-	strNumber := strconv.FormatInt(number, 10)
-	res, err := c.RPCClient.Call("GetTransactionsForTxBlock", strNumber)
-	if err != nil {
-		return nil, err
-	}
-	var results [][]string
-	err = res.GetObject(&results)
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformRequest, errors.Params{"block": number})
-	}
-
-	var requests jsonrpc.RPCRequests
-	for _, ids := range results {
-		for _, id := range ids {
-			req := jsonrpc.NewRequest("GetTransaction", id)
-			requests = append(requests, req)
-		}
-	}
-
-	var txs []Tx
-
-	if len(requests) == 0 {
-		return txs, nil
-	}
-
-	responses, err := c.RPCClient.CallBatch(requests)
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformRequest, errors.Params{"block": number})
-	}
-
-	for _, result := range responses {
-		var txRPC TxRPC
-		if mapstructure.Decode(result.Result, &txRPC) != nil {
-			continue
-		}
-		txs = append(txs, txRPC.toTx())
-	}
-	return txs, nil
-}
-
-func (c *Client) GetTxsOfAddress(address string) ([]Tx, error) {
-	path := fmt.Sprintf("/addresses/%s/txs", address)
-	req, err := c.newRequest("GET", path)
-	if err != nil {
-		return nil, err
-	}
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformRequest, errors.Params{"url": path})
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformUnmarshal, errors.Params{"url": path})
-	}
-
-	if bytes.HasPrefix(body, []byte(`{"message":"Invalid API key specified"`)) {
-		return nil, errors.E("invalid Zilliqa API key", errors.TypePlatformUnmarshal,
-			errors.Params{"url": path, "body": string(body)})
-	}
-
-	txs := make([]Tx, 0)
-	err = json.Unmarshal(body, &txs)
-	if err != nil {
-		return nil, errors.E(err, errors.TypePlatformUnmarshal,
-			errors.Params{"url": path, "body": string(body)})
-	}
-
-	return txs, nil
+func (c *Client) GetTxsOfAddress(address string) (tx []Tx, err error) {
+	path := fmt.Sprintf("addresses/%s/txs", address)
+	err = c.Get(&tx, path, nil)
+	return
 }
---
+++ platform/zilliqa/rpc.go
@@ -0,0 +1,44 @@
+package zilliqa
+
+import (
+	"github.com/trustwallet/blockatlas/pkg/blockatlas"
+	"strconv"
+	"sync"
+)
+
+type RpcClient struct {
+	blockatlas.Request
+}
+
+func (c *RpcClient) GetBlockchainInfo() (info *ChainInfo, err error) {
+	err = c.RpcCall(&info, "GetBlockchainInfo", nil)
+	return
+}
+
+func (c *RpcClient) GetTx(hash string) (tx Tx, err error) {
+	err = c.RpcCall(&tx, "GetTransaction", []string{hash})
+	return
+}
+
+func (c *RpcClient) GetTxInBlock(number int64) (txs []Tx, err error) {
+	strNumber := strconv.FormatInt(number, 10)
+	var results [][]string
+	err = c.RpcCall(&results, "GetTransactionsForTxBlock", []string{strNumber})
+
+	var wg sync.WaitGroup
+	for _, ids := range results {
+		for _, id := range ids {
+			wg.Add(1)
+			go func(id string) {
+				defer wg.Done()
+				tx, errTx := c.GetTx(id)
+				if errTx != nil {
+					return
+				}
+				txs = append(txs, tx)
+			}(id)
+		}
+	}
+	wg.Wait()
+	return
+}
