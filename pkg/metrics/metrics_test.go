29e1cfed882e80b9670ac8e9d16bcb9ff05f005b
Danilo Pantani <danpantani@gmail.com>
2019-10-03 01:30:25 -0300

    improve regex to remove more params to have public metrics

--- pkg/metrics/metrics_test.go
+++ pkg/metrics/metrics_test.go
@@ -10,7 +10,7 @@ func Test_removeSensitiveInfo(t *testing.T) {
 	}{
 		{"Remove Nimiq address", "/v1/nimiq/NQ43 J7G6 K6T8 H5KJ 5CXN Q5JK 2GJ4 6DSB 7PUH", "/v1/nimiq/"},
 		{"Remove Tezos address", "/v1/tezos/tz1WCd2jm4uSt4vntk4vSuUWoZQGhLcDuR9q", "/v1/tezos/"},
-		{"Remove Tron info", "https://api.trongrid.io/v1/accounts/TPJYCz8ppZNyvw7pTwmjajcx4Kk1MmEUhD/transactions?limit=200&only_confirmed=true&token_id=1000011", "https://api.trongrid.io/v1/accounts//transactions?limit=200&only_confirmed=true&token_id="},
+		{"Remove Tron info", "https://api.trongrid.io/v1/accounts/TPJYCz8ppZNyvw7pTwmjajcx4Kk1MmEUhD/transactions?limit=200&only_confirmed=true&token_id=1000011", "https://api.trongrid.io/v1/accounts//transactions?limit&only_confirmed&token_id"},
 		{"Remove asset id", "https://api.trongrid.io/v1/assets/1000570?", "https://api.trongrid.io/v1/assets/?"},
 	}
 	for _, tt := range tests {
