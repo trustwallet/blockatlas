1d910d8db5d353c07caed5d64feb2b7641b8ed42
Danilo Pantani <danpantani@gmail.com>
2019-10-11 12:27:43 -0300

    add batch transactions for SQL (#446)
    * add sql batch transactions
    
    * change batch method signature

--- pkg/storage/sql/sql.go
+++ pkg/storage/sql/sql.go
@@ -7,47 +7,81 @@ import (
 )
 
 type sql struct {
-	client *gorm.DB
+	Client *gorm.DB
 }
 
+type Handler func(value interface{}) error
+
 func (db *sql) Get(value interface{}) error {
-	err := db.client.Last(value).Error
+	err := db.Client.Last(value).Error
 	if err != nil {
 		return errors.E(err, util.ErrNotFound)
 	}
 	return nil
 }
 
+func (db *sql) Find(out interface{}, where ...interface{}) error {
+	err := db.Client.Find(out, where...).Error
+	if err != nil {
+		return errors.E(err, util.ErrNotFound)
+	}
+	return nil
+}
+
+func (db *sql) CreateOrUpdate(value interface{}) error {
+	if db.Client.NewRecord(value) {
+		return db.Add(value)
+	}
+	return db.Update(value)
+}
+
 func (db *sql) Update(value interface{}) error {
-	if db.client.NewRecord(value) {
+	if db.Client.NewRecord(value) {
 		return util.ErrNotFound
 	}
-	err := db.client.Save(value).Error
+	err := db.Client.Save(value).Error
 	if err != nil {
 		return errors.E(err, util.ErrNotUpdated)
 	}
 	return nil
 }
 
+func (db *sql) UpdateMany(values ...interface{}) error {
+	return db.Batch(db.Update, values...)
+}
+
 func (db *sql) Add(value interface{}) error {
-	if !db.client.NewRecord(value) {
-		return util.ErrAlreadyExist
-	}
-	db.client.AutoMigrate(value)
-	err := db.client.Create(value).Error
+	err := db.Client.Create(value).Error
 	if err != nil {
 		return errors.E(err, util.ErrNotStored)
 	}
 	return nil
 }
 
+func (db *sql) AddMany(values ...interface{}) error {
+	return db.Batch(db.Add, values...)
+}
+
 func (db *sql) Delete(value interface{}) error {
-	if db.client.NewRecord(value) {
-		return util.ErrNotFound
-	}
-	err := db.client.Delete(value).Error
+	err := db.Client.Delete(value).Error
 	if err != nil {
 		return errors.E(err, util.ErrNotDeleted)
 	}
 	return nil
+}
+
+func (db *sql) DeleteMany(values ...interface{}) error {
+	return db.Batch(db.Delete, values...)
+}
+
+func (db *sql) Batch(handler Handler, values ...interface{}) error {
+	tx := db.Client.Begin()
+	for _, value := range values {
+		err := handler(value)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	return tx.Commit().Error
 }
