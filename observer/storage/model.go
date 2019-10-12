175503c615269c47fb62a1c9cf68851b2b06333a
Danilo Pantani <danpantani@gmail.com>
2019-10-11 22:12:38 -0300

    add created at field at database

--- observer/storage/model.go
+++ observer/storage/model.go
@@ -1,5 +1,7 @@
 package storage
 
+import "time"
+
 type Block struct {
 	ID          uint `gorm:"primary_key"`
 	Coin        uint `gorm:"primary_key"`
@@ -7,16 +9,18 @@ type Block struct {
 }
 
 type Xpub struct {
-	ID      uint `gorm:"auto_increment;not null"`
-	Coin    uint
-	Address string `gorm:"primary_key;type:varchar(150)"`
-	Xpub    string `gorm:"primary_key;type:varchar(150)"`
+	ID        uint `gorm:"auto_increment;not null"`
+	Coin      uint
+	Address   string `gorm:"primary_key;type:varchar(150)"`
+	Xpub      string `gorm:"primary_key;type:varchar(150)"`
+	CreatedAt time.Time
 }
 
 type Subscription struct {
-	ID      uint   `json:"-" gorm:"auto_increment;not null"`
-	Coin    uint   `json:"coin"`
-	Address string `json:"address" gorm:"primary_key;type:varchar(150)"`
-	Webhook string `json:"webhook" gorm:"primary_key;type:varchar(150)"`
-	Xpub    string `json:"-" sql:"-" gorm:"-"`
+	ID        uint   `json:"-" gorm:"auto_increment;not null"`
+	Coin      uint   `json:"coin"`
+	Address   string `json:"address" gorm:"primary_key;type:varchar(150)"`
+	Webhook   string `json:"webhook" gorm:"primary_key;type:varchar(150)"`
+	Xpub      string `json:"-" sql:"-" gorm:"-"`
+	CreatedAt time.Time
 }
