84ecf6dc9db40755134d84d159e0c7ce93de8a63
Danilo Pantani <danpantani@gmail.com>
2019-10-22 13:11:35 -0300

    change tezos api to TzStatus

--- platform/tezos/client.go
+++ platform/tezos/client.go
@@ -11,37 +11,21 @@ type Client struct {
 }
 
 func (c *Client) GetTxsOfAddress(address string) ([]Tx, error) {
-	var txs []Tx
-	path := fmt.Sprintf("v3/operations/%s", address)
-	err := c.Get(&txs, path, url.Values{"type": {"Transaction"}})
-	return txs, err
+	var account Op
+	path := fmt.Sprintf("account/%s/op", address)
+	err := c.Get(&account, path, url.Values{"limit": {"1000"}, "offset": {"0"}})
+	return account.Txs, err
 }
 
 func (c *Client) GetCurrentBlock() (int64, error) {
 	var head Head
-	err := c.Get(&head, "v3/head", nil)
-	return head.Level, err
-}
-
-func (c *Client) GetBlockHashByNumber(num int64) (string, error) {
-	var list []string
-	path := fmt.Sprintf("v3/block_hash_level/%d", num)
-	err := c.Get(&list, path, nil)
-
-	if err != nil && len(list) != 0 {
-		return "", err
-	} else {
-		return list[0], nil
-	}
+	err := c.Get(&head, "block/head", url.Values{"limit": {"1000"}, "offset": {"0"}})
+	return head.Height, err
 }
 
 func (c *Client) GetBlockByNumber(num int64) ([]Tx, error) {
-	var list []Tx
-	hash, err := c.GetBlockHashByNumber(num)
-	if err != nil {
-		return list, err
-	}
-	path := fmt.Sprintf("v3/operations/%s", hash)
-	err = c.Get(&list, path, url.Values{"type": {"Transaction"}})
-	return list, err
+	var block Op
+	path := fmt.Sprintf("block/%d/op", num)
+	err := c.Get(&block, path, url.Values{"limit": {"1000"}, "offset": {"0"}})
+	return block.Txs, err
 }
