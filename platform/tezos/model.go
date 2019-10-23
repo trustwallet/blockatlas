28920dbc91a8a4f8ca0fe506fe08ed16a5c52268
Danilo Pantani <danpantani@gmail.com>
2019-10-22 21:38:07 -0300

    change tezos api provider (#474)
    * change tezos api provider
    
    * change tezos api to TzStatus
    
    * change rpc host
    
    * fix unit tests

--- platform/tezos/model.go
+++ platform/tezos/model.go
@@ -1,50 +1,28 @@
 package tezos
 
-import (
-	"github.com/trustwallet/blockatlas/pkg/blockatlas"
-)
+type Op struct {
+	Txs []Tx `json:"ops"`
+}
 
 // Tx is a Tezos blockchain transaction
 type Tx struct {
-	Hash        string  `json:"hash"`
-	BlockHash   string  `json:"block_hash"`
-	NetworkHash string  `json:"network_hash"`
-	Type        Manager `json:"type"`
-}
-
-// Manager contains transfer operations
-type Manager struct {
-	Kind       string      `json:"kind"`
-	Src        Address     `json:"src"`
-	Operations []Operation `json:"operations"`
-}
-
-// Operation is a Tezos transfer operation
-type Operation struct {
-	Kind         string            `json:"kind"`
-	Src          Address           `json:"src"`
-	Dest         Address           `json:"destination"`
-	Amount       blockatlas.Amount `json:"amount"`
-	Failed       bool              `json:"failed"`
-	Internal     bool              `json:"internal"`
-	Burn         string            `json:"burn"`
-	Counter      int               `json:"counter"`
-	Fee          blockatlas.Amount `json:"fee"`
-	GasLimit     string            `json:"gas_limit"`
-	StorageLimit string            `json:"storage_limit"`
-	OpLevel      uint64            `json:"op_level"`
-	Timestamp    string            `json:"timestamp"`
+	Hash      string  `json:"hash"`
+	BlockHash string  `json:"block"`
+	Status    string  `json:"status"`
+	Success   bool    `json:"is_success"`
+	Time      string  `json:"time"`
+	Height    uint64  `json:"height"`
+	Type      string  `json:"type"`
+	Sender    string  `json:"sender"`
+	Volume    float64 `json:"volume"`
+	Receiver  string  `json:"receiver"`
+	Fee       int     `json:"gas_used"`
 }
 
 type Validator struct {
 	Address string `json:"pkh"`
 }
 
-// Address is a Tezos address object
-type Address struct {
-	Tz string `json:"tz"`
-}
-
 type Head struct {
-	Level int64 `json:"level"`
+	Height int64 `json:"height"`
 }
