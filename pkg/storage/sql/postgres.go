30fc135d0b8396ef969876464952fe5e2d0c955d
Danilo Pantani <danpantani@gmail.com>
2019-10-24 20:04:24 -0300

    fix max conn (#478)

--- pkg/storage/sql/postgres.go
+++ pkg/storage/sql/postgres.go
@@ -4,7 +4,6 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/trustwallet/blockatlas/pkg/errors"
-	"time"
 )
 
 type PgSql struct {
@@ -16,8 +15,6 @@ func (db *PgSql) Init(host string, conns int) error {
 	if err != nil {
 		return errors.E(err, "postgress connection failed")
 	}
-	client.DB().SetMaxIdleConns(conns * 2)
-	client.DB().SetConnMaxLifetime(time.Second * 5)
 	db.Client = client
 	return nil
 }
