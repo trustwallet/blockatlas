36ead1f663a99ea2aba2e1f73909e5c1a806898e
Danilo Pantani <danpantani@gmail.com>
2019-10-28 11:52:49 -0300

    enable cors

--- api/middleware.go
+++ api/middleware.go
@@ -30,3 +30,19 @@ func TokenAuthMiddleware(requiredToken string) gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
--- cmd/api.go
+++ cmd/api.go
@@ -38,6 +38,7 @@ func RunApi(bind string, c chan *gin.Engine) {
 
 	sg := sentrygin.New(sentrygin.Options{})
 	engine.Use(util.CheckReverseProxy, sg)
+	engine.Use(api.CORSMiddleware())
 
 	engine.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
