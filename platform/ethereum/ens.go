ee13d77314e88fcbaabfb6acba48dd4b1200bc88
hewig <360470+hewigovens@users.noreply.github.com>
2019-11-02 14:02:08 +0800

    Improve ens/zns coding style (#492)
    * Fix #491
    
    * return error
    
    * move code to Client
    
    * revert go.mod
    
    * return error
    
    * Fix subdomain query

--- platform/ethereum/ens.go
+++ platform/ethereum/ens.go
@@ -1,70 +1,63 @@
 package ethereum
 
 import (
-	"encoding/hex"
-	"net/http"
-
 	"github.com/ethereum/go-ethereum/ethclient"
 	cc "github.com/hewigovens/go-coincodec"
-	"github.com/spf13/viper"
 	CoinType "github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"github.com/trustwallet/blockatlas/pkg/errors"
-	"github.com/trustwallet/blockatlas/pkg/logger"
 	ens "github.com/wealdtech/go-ens/v3"
 )
 
-func (p *Platform) Lookup(coin uint64, name string) (*blockatlas.Resolved, error) {
-	client, err := ethclient.Dial(viper.GetString("ethereum.rpc"))
+func (p *Platform) Lookup(coin uint64, name string) (blockatlas.Resolved, error) {
+	client, err := ethclient.Dial(p.RpcURL)
 	result := blockatlas.Resolved{
 		Coin: coin,
 	}
 	if err != nil {
-		return nil, errors.E(http.StatusInternalServerError, "can't dial to ethereum rpc")
+		return result, errors.E(err, "can't dial to ethereum rpc")
 	}
 	defer client.Close()
+	// try to get multi coin address
 	ensName, err := ens.NewName(client, name)
 	if err != nil {
-		logger.Error("Query ens failed", err, logger.Params{
-			"name": name,
-			"coin": coin,
-		})
-		return &result, nil
+		if coin == CoinType.ETH {
+			// https://github.com/wealdtech/go-ens#management-of-subdomains
+			// subdomains have their own registrars they do not work with the Name interface.
+			return lookupLegacyETH(client, name)
+		}
+		return result, errors.E(err, "query ens failed")
 	}
 
 	address, err := ensName.Address(coin)
 	if err != nil {
 		if coin == CoinType.ETH {
-			// will remove this later
+			// user may not set multi coin address
 			return lookupLegacyETH(client, ensName.Name)
 		}
-		// return empty result for errors like: no unregistered
-		return &result, nil
+		return result, err
 	}
 
 	encoded, err := cc.ToString(address, uint32(coin))
 	result.Result = encoded
 	if err != nil {
-		logger.Error("Encode to address failed", err, logger.Params{
-			"address": hex.EncodeToString(address),
-			"coin":    coin,
-		})
+		return result, errors.E(err, "encode to address failed")
 	}
-	return &result, nil
+	return result, nil
 }
 
-func lookupLegacyETH(client *ethclient.Client, name string) (*blockatlas.Resolved, error) {
+func lookupLegacyETH(client *ethclient.Client, name string) (blockatlas.Resolved, error) {
 	result := blockatlas.Resolved{
 		Coin: CoinType.ETH,
 	}
 	resolver, err := ens.NewResolver(client, name)
 	if err != nil {
-		return &result, nil
+		return result, errors.E(err, "new ens resolver failed")
 	}
 	address, err := resolver.Address()
 	if err != nil {
-		return &result, nil
+		return result, errors.E(err, "query address failed")
 	}
 	result.Result = address.Hex()
-	return &result, nil
+	return result, nil
 }
--- platform/zilliqa/client.go
+++ platform/zilliqa/client.go
@@ -2,6 +2,7 @@ package zilliqa
 
 import (
 	"fmt"
+
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 )
 
@@ -12,5 +13,10 @@ type Client struct {
 func (c *Client) GetTxsOfAddress(address string) (tx []Tx, err error) {
 	path := fmt.Sprintf("addresses/%s/txs", address)
 	err = c.Get(&tx, path, nil)
+	return
+}
+
+func (c *Client) LookupName(name string) (response ZNSResponse, err error) {
+	err = c.Get(&response, "/"+name, nil)
 	return
 }
