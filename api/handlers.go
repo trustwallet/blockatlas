5e7e10f3e95a3e124ea7eae54061ddb20ef1cdfa
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-11-10 23:21:54 -0800

    Fixed collection_id param for generating correct collections url

--- api/handlers.go
+++ api/handlers.go
@@ -190,11 +190,11 @@ func makeCollectionsRoute(router gin.IRouter, api blockatlas.Platform) {
 // @Produce json
 // @Tags platform,collection
 // @Param coin path string true "the coin name" default(ethereum)
-// @Param address path string true "the query address" default(0x0875BCab22dE3d02402bc38aEe4104e1239374a7)
+// @Param owner path string true "the query address" default(0x0875BCab22dE3d02402bc38aEe4104e1239374a7)
 // @Param collection_id path string true "the query collection" default(0x06012c8cf97bead5deae237070f9587f8e7a266d)
 // @Success 200 {object} blockatlas.CollectionPage
 // @Failure 500 {object} api.ApiError
-// @Router /v2/{coin}/collections/{address}/collection/:collection_id [get]
+// @Router /v2/{coin}/collections/{owner}/collection/{collection_id} [get]
 func makeCollectionRoute(router gin.IRouter, api blockatlas.Platform) {
 	var collectionAPI blockatlas.CollectionAPI
 	collectionAPI, _ = api.(blockatlas.CollectionAPI)
