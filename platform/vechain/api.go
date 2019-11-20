dd27d371ea9ace391754fe159b7c786c11f357c2
Danilo Pantani <danpantani@gmail.com>
2019-11-20 17:47:16 -0300

    new vechain platform

--- platform/vechain/api.go
+++ platform/vechain/api.go
@@ -7,7 +7,7 @@ import (
 	"github.com/trustwallet/blockatlas/pkg/errors"
 	"github.com/trustwallet/blockatlas/pkg/logger"
 	"github.com/trustwallet/blockatlas/util"
-	"strings"
+	"strconv"
 	"sync"
 )
 
@@ -24,344 +24,149 @@ func (p *Platform) Coin() coin.Coin {
 	return coin.Coins[coin.VET]
 }
 
-const (
-	GasContract         = "0x0000000000000000000000000000456e65726779"
-	GasSymbol           = "VTHO"
-	GasName             = "VeThor Token"
-	VeThorTransferEvent = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-)
-
-// CurrentBlockNumber implementation of interface function which gets a current blockchain height
 func (p *Platform) CurrentBlockNumber() (int64, error) {
-	cbi, err := p.client.GetCurrentBlockInfo()
-	if err != nil {
-		return 0, err
-	}
-	return cbi.BestBlockNum, nil
+	return p.client.GetCurrentBlock()
 }
 
-// GetBlockByNumber implementation of interface function which gets a block for push notification
 func (p *Platform) GetBlockByNumber(num int64) (*blockatlas.Block, error) {
 	block, err := p.client.GetBlockByNumber(num)
 	if err != nil {
 		return nil, err
 	}
-	transactionsChan := p.getTransactions(block.Transactions)
-
-	txs := make([]blockatlas.Tx, 0)
-	for t := range transactionsChan {
-		ntxs := NormalizeBlockTransactions(t)
-		txs = append(txs, ntxs...)
+	cTxs := p.getTransactions(block.Transactions)
+	txs := make(blockatlas.TxPage, 0)
+	for t := range cTxs {
+		txs = append(txs, t...)
 	}
-
 	return &blockatlas.Block{
 		Number: num,
-		ID:     block.ID,
+		ID:     block.Id,
 		Txs:    txs,
 	}, nil
 }
 
-func (p *Platform) GetTxsByAddress(address string) (blockatlas.TxPage, error) {
-	return p.getTxsByAddress(address)
-}
-
-func (p *Platform) GetTokenTxsByAddress(address string, token string) (blockatlas.TxPage, error) {
-	if strings.ToLower(token) == GasContract {
-		return p.getThorTxsByAddress(address)
-	} else {
-		return nil, errors.E("vechain invalid token", errors.TypePlatformUnmarshal,
-			errors.Params{"token": token}).PushToSentry()
+func (p *Platform) getTransactions(ids []string) chan blockatlas.TxPage {
+	txChan := make(chan blockatlas.TxPage, len(ids))
+	var wg sync.WaitGroup
+	for _, id := range ids {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := p.getTransactionChannel(id, txChan)
+			if err != nil {
+				logger.Error(err)
+			}
+		}()
 	}
+	wg.Wait()
+	close(txChan)
+	return txChan
 }
 
-func (p *Platform) getThorTxsByAddress(address string) ([]blockatlas.Tx, error) {
-	sourceTxs, err := p.client.GetTokenTransfers(address)
+func (p *Platform) getTransactionChannel(id string, txChan chan blockatlas.TxPage) error {
+	srcTx, err := p.client.GetTransactionByID(id)
 	if err != nil {
-		return nil, err
+		return errors.E(err, "Failed to get tx", errors.TypePlatformUnmarshal,
+			errors.Params{"id": id}).PushToSentry()
 	}
-
-	var ids []string
-	for _, tx := range sourceTxs.TokenTransfers {
-		ids = append(ids, tx.TxID)
+	txs, err := NormalizeTransaction(srcTx)
+	if err != nil {
+		return errors.E(err, "Failed to NormalizeBlockTransactions tx", errors.TypePlatformUnmarshal,
+			errors.Params{"tx": srcTx}).PushToSentry()
 	}
-	receiptsChan := p.getTransactionReceipt(ids)
-
-	var txs []blockatlas.Tx
-	for _, t := range sourceTxs.TokenTransfers {
-		if !strings.EqualFold(t.ContractAddress, GasContract) {
-			continue
-		}
-
-		receipt := findTransferReceiptByTxID(receiptsChan, t.TxID)
-		if receipt == nil {
-			logger.Error("findTransferReceiptByTxID cannot find the receipt", logger.Params{
-				"transfer": t,
-			})
-			continue
-		}
-
-		tx, err := NormalizeTokenTransfer(&t, receipt)
-		if err != nil {
-			p := logger.Params{"receipt": receipt, "transfer": t}
-			err = errors.E(err, "invalid token", errors.TypePlatformUnmarshal, p).PushToSentry()
-			logger.Error(err, "getTxsByAddress clause error", p)
-			continue
-		}
-		txs = append(txs, tx)
-	}
-
-	return txs, nil
-}
-
-func (p *Platform) getTransactionReceipt(ids []string) chan *TransferReceipt {
-	receiptsChan := make(chan *TransferReceipt, len(ids))
-	sem := util.NewSemaphore(16)
-	var wg sync.WaitGroup
-	wg.Add(len(ids))
-	for _, id := range ids {
-		go func(id string) {
-			defer wg.Done()
-			sem.Acquire()
-			defer sem.Release()
-			receipt, err := p.client.GetTransactionReceipt(id)
-			if err != nil {
-				err = errors.E(err, "Failed to get tx receipt", errors.TypePlatformUnmarshal,
-					errors.Params{"id": id}).PushToSentry()
-				logger.Error(err, logger.Params{"id": id})
-				return
-			}
-			receiptsChan <- receipt
-		}(id)
-	}
-	wg.Wait()
-	close(receiptsChan)
-
-	return receiptsChan
-}
-
-func (p *Platform) getTransactions(ids []string) chan *NativeTransaction {
-	receiptsChan := make(chan *NativeTransaction, len(ids))
-
-	sem := util.NewSemaphore(16)
-	var wg sync.WaitGroup
-	wg.Add(len(ids))
-	for _, id := range ids {
-		go func(id string) {
-			defer wg.Done()
-			sem.Acquire()
-			defer sem.Release()
-			receipt, err := p.client.GetTransactionByID(id)
-			if err != nil {
-				err = errors.E(err, "Failed to get tx transaction", errors.TypePlatformUnmarshal,
-					errors.Params{"id": id}).PushToSentry()
-				logger.Error(err, logger.Params{"id": id})
-				return
-			}
-			receiptsChan <- receipt
-		}(id)
-	}
-
-	wg.Wait()
-	close(receiptsChan)
-
-	return receiptsChan
-}
-
-func findTransferReceiptByTxID(receiptsChan chan *TransferReceipt, txID string) *TransferReceipt {
-	for receipt := range receiptsChan {
-		if receipt.ID == txID {
-			return receipt
-		}
-	}
+	txChan <- txs
 	return nil
 }
 
-func (p *Platform) getTxsByAddress(address string) ([]blockatlas.Tx, error) {
-	sourceTxs, _ := p.client.GetTransactions(address)
-
-	var ids []string
-	for _, tx := range sourceTxs.Transactions {
-		ids = append(ids, tx.ID)
-	}
-	receiptsChan := p.getTransactionReceipt(ids)
-
-	var txs []blockatlas.Tx
-	for receipt := range receiptsChan {
-		for _, clause := range receipt.Clauses {
-			if !strings.EqualFold(receipt.Origin, address) && !strings.EqualFold(clause.To, address) {
-				continue
-			}
-			tx, err := NormalizeTransfer(receipt, &clause)
-			if err != nil {
-				p := logger.Params{"receipt": receipt, "clause": clause}
-				err = errors.E(err, errors.TypePlatformUnmarshal, p).PushToSentry()
-				logger.Error(err, "getTxsByAddress clause error", p)
-				continue
-			}
-			txs = append(txs, tx)
-		}
-	}
-
-	return txs, nil
-}
-
-func NormalizeTransfer(receipt *TransferReceipt, clause *Clause) (blockatlas.Tx, error) {
-	if receipt.Receipt == nil || clause == nil {
-		return blockatlas.Tx{}, errors.E("invalid parameters", errors.Params{"receipt": receipt, "clause": clause}).PushToSentry()
-	}
-	feeBase10, err := util.HexToDecimal(receipt.Receipt.Paid)
+func hexToInt(hex string) (int, error) {
+	nonceStr, err := util.HexToDecimal(hex)
 	if err != nil {
-		return blockatlas.Tx{}, err
+		return 0, err
 	}
-	valueBase10, err := util.HexToDecimal(clause.Value)
+	return strconv.Atoi(nonceStr)
+}
+
+func NormalizeTransaction(srcTx Tx) (blockatlas.TxPage, error) {
+	if srcTx.Clauses == nil || len(srcTx.Clauses) == 0 {
+		return blockatlas.TxPage{}, errors.E("NormalizeBlockTransaction: Clauses not found", errors.Params{"tx": srcTx}).PushToSentry()
+	}
+
+	nonce, err := hexToInt(srcTx.Nonce)
 	if err != nil {
-		return blockatlas.Tx{}, err
-	}
-	tx := blockatlas.Tx{
-		ID:       receipt.ID,
-		Coin:     coin.VET,
-		From:     receipt.Origin,
-		To:       clause.To,
-		Fee:      blockatlas.Amount(feeBase10),
-		Date:     int64(receipt.Timestamp),
-		Type:     blockatlas.TxTransfer,
-		Block:    receipt.Block,
-		Status:   ReceiptStatus(receipt.Receipt.Reverted),
-		Sequence: receipt.Block,
-		Meta: blockatlas.Transfer{
-			Value:    blockatlas.Amount(valueBase10),
-			Symbol:   coin.Coins[coin.VET].Symbol,
-			Decimals: 18,
-		},
-	}
-	return tx, nil
-}
-
-func NormalizeTokenTransfer(t *TokenTransfer, receipt *TransferReceipt) (blockatlas.Tx, error) {
-	feeBase10, err := util.HexToDecimal(receipt.Receipt.Paid)
-	if err != nil {
-		return blockatlas.Tx{}, err
-	}
-	valueBase10, err := util.HexToDecimal(t.Amount)
-	if err != nil {
-		return blockatlas.Tx{}, err
+		return blockatlas.TxPage{}, err
 	}
 
-	tx := blockatlas.Tx{
-		ID:       t.TxID,
-		Coin:     coin.VET,
-		From:     t.Origin,
-		To:       t.Receiver,
-		Fee:      blockatlas.Amount(feeBase10),
-		Date:     t.Timestamp,
-		Type:     blockatlas.TxNativeTokenTransfer,
-		Block:    t.Block,
-		Status:   ReceiptStatus(receipt.Receipt.Reverted),
-		Sequence: t.Block,
-		Meta: blockatlas.NativeTokenTransfer{
-			Name:     GasName,
-			Symbol:   GasSymbol,
-			TokenID:  GasContract,
-			Decimals: 18,
-			Value:    blockatlas.Amount(valueBase10),
-			From:     t.Origin,
-			To:       t.Receiver,
-		},
-	}
-	return tx, nil
-}
+	id := util.GetValidParameter(srcTx.Id, srcTx.Meta.TxId)
+	origin := util.GetValidParameter(srcTx.Origin, srcTx.Meta.TxOrigin)
+	fee := strconv.Itoa(srcTx.Gas)
 
-// NormalizeTransaction converts a VeChain VTHO token transaction into the generic model
-func NormalizeBlockTransactions(t *NativeTransaction) (txs []blockatlas.Tx) {
-	for _, output := range t.Receipt.Outputs {
-		vthoTransfers := normalizeVthoTransfers(t, output.Events)
-		txs = append(txs, vthoTransfers...)
-		transfers := normalizeTxTransfers(t, output.Transfers)
-		txs = append(txs, transfers...)
-	}
-	return txs
-}
-
-// normalizeVthoTransfers normalizes vtho transfer events in given transaction
-func normalizeVthoTransfers(t *NativeTransaction, events []Event) (txs []blockatlas.Tx) {
-	for _, event := range events {
-		if len(event.Topics) == 3 && event.Topics[0] == VeThorTransferEvent {
-			feeBase10, err := util.HexToDecimal(t.Receipt.Paid)
-			if err != nil {
-				continue
-			}
-
-			valueBase10, err := util.HexToDecimal(event.Data)
-			if err != nil {
-				continue
-			}
-			fromHex := event.Topics[1]
-			toHex := event.Topics[2]
-			from := util.Checksum(formatHexToAddress(fromHex))
-			to := util.Checksum(formatHexToAddress(toHex))
-
-			txs = append(txs, blockatlas.Tx{
-				ID:       t.ID,
-				Coin:     coin.VET,
-				From:     from,
-				To:       to,
-				Fee:      blockatlas.Amount(feeBase10),
-				Date:     t.Timestamp,
-				Type:     blockatlas.TxNativeTokenTransfer,
-				Block:    t.Block,
-				Status:   ReceiptStatus(t.Receipt.Reverted),
-				Sequence: t.Block,
-				Meta: blockatlas.NativeTokenTransfer{
-					Name:     GasName,
-					Symbol:   GasSymbol,
-					TokenID:  GasContract,
-					Decimals: 18,
-					Value:    blockatlas.Amount(valueBase10),
-					From:     from,
-					To:       to,
-				},
-			})
-		}
-	}
-	return
-}
-
-// normalizeTxTransfers normalizes transfers in given transaction
-func normalizeTxTransfers(t *NativeTransaction, transfers []Transfer) (txs []blockatlas.Tx) {
-	for _, transfer := range transfers {
-		feeBase10, err := util.HexToDecimal(t.Receipt.Paid)
+	txs := make(blockatlas.TxPage, 0)
+	for _, clause := range srcTx.Clauses {
+		value, err := util.HexToDecimal(clause.Value)
 		if err != nil {
-			continue
+			return blockatlas.TxPage{}, err
 		}
 
-		valueBase10, err := util.HexToDecimal(transfer.Amount)
-		if err != nil {
-			continue
-		}
 		txs = append(txs, blockatlas.Tx{
-			ID:       t.ID,
+			ID:       id,
 			Coin:     coin.VET,
-			From:     util.Checksum(transfer.Sender),
-			To:       util.Checksum(transfer.Recipient),
-			Fee:      blockatlas.Amount(feeBase10),
-			Date:     t.Timestamp,
+			From:     origin,
+			To:       clause.To,
+			Fee:      blockatlas.Amount(fee),
+			Date:     srcTx.Meta.BlockTimestamp,
 			Type:     blockatlas.TxTransfer,
-			Block:    t.Block,
-			Status:   ReceiptStatus(t.Receipt.Reverted),
-			Sequence: t.Block,
+			Block:    srcTx.Meta.BlockNumber,
+			Sequence: uint64(nonce),
+			Status:   blockatlas.StatusCompleted,
 			Meta: blockatlas.Transfer{
-				Value:    blockatlas.Amount(valueBase10),
+				Value:    blockatlas.Amount(value),
 				Symbol:   coin.Coins[coin.VET].Symbol,
 				Decimals: 18,
 			},
 		})
 	}
-	return
+	return txs, nil
 }
 
-func formatHexToAddress(hex string) string {
-	if len(hex) > 26 {
-		return "0x" + hex[26:]
+func (p *Platform) GetTxsByAddress(address string) (blockatlas.TxPage, error) {
+	srcTxs, err := p.client.GetTransactions(address)
+	if err != nil {
+		return nil, err
 	}
-	return hex
+
+	txs := make(blockatlas.TxPage, 0)
+	for _, t := range srcTxs {
+		tx, err := NormalizeLogTransaction(t)
+		if err != nil {
+			continue
+		}
+		txs = append(txs, tx)
+	}
+	return txs, nil
+}
+
+func NormalizeLogTransaction(srcTx LogTx) (blockatlas.Tx, error) {
+	value, err := util.HexToDecimal(srcTx.Amount)
+	if err != nil {
+		return blockatlas.Tx{}, err
+	}
+
+	id := util.GetValidParameter(srcTx.Id, srcTx.Meta.TxId)
+	tx := blockatlas.Tx{
+		ID:     id,
+		Coin:   coin.VET,
+		From:   srcTx.Sender,
+		To:     srcTx.Recipient,
+		Fee:    blockatlas.Amount("0"),
+		Date:   srcTx.Meta.BlockTimestamp,
+		Type:   blockatlas.TxTransfer,
+		Block:  srcTx.Meta.BlockNumber,
+		Status: blockatlas.StatusCompleted,
+		Meta: blockatlas.Transfer{
+			Value:    blockatlas.Amount(value),
+			Symbol:   coin.Coins[coin.VET].Symbol,
+			Decimals: 18,
+		},
+	}
+	return tx, nil
 }
--- platform/vechain/api_test.go
+++ platform/vechain/api_test.go
@@ -1,297 +1,125 @@
 package vechain
 
 import (
-	"bytes"
 	"encoding/json"
+	"github.com/stretchr/testify/assert"
 	"github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"testing"
 )
 
-const transferReceipt = `{
-    "block": 2620166,
-    "id": "0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
-    "nonce": "0x3657a2025b11f27f",
-    "origin": "0xB853D6a965fBC047aaA9f04d774d53861d7eD653",
-	"timestamp": 1556569300,
-	"receipt": {
-		"paid": "0x1236efcbcbb340000",
-		"reverted": false
-	}
+const transferSrc = `{
+  "id": "0xe75d6f28297d910faf31d7aaff9bc57faf14895ffc65da056a90b5a258c17784",
+  "chainTag": 74,
+  "blockRef": "0x000bae2bae707d76",
+  "expiration": 720,
+  "clauses": [
+    {
+      "to": "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+      "value": "0x2b5e3af16b1880000",
+      "data": "0x"
+    }
+  ],
+  "gasPriceCoef": 0,
+  "gas": 21000,
+  "origin": "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+  "delegator": null,
+  "nonce": "0x603ca6b1879375dc",
+  "dependsOn": null,
+  "size": 130,
+  "meta": {
+    "blockID": "0x000bae2cd3cdea4c79d0d7df3c16f2012b93877eba111803e50d3e0c5f17ed0d",
+    "blockNumber": 765484,
+    "blockTimestamp": 1537983090
+  }
 }`
 
-const transferFailedReceipt = `{
-    "block": 2620166,
-    "id": "0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
-    "nonce": "0x3657a2025b11f27f",
-    "origin": "0xB853D6a965fBC047aaA9f04d774d53861d7eD653",
-	"timestamp": 1556569300,
-	"receipt": {
-		"paid": "0x1236efcbcbb340000",
-		"reverted": false
-	}
-}`
-
-const transferClause = `{
-	"to": "0xdA623049A13df5c8a24f0d7713F4adD4ab136B1f",
-	"value": "0x29bde5885d7ac80000"
-}`
-
-const tokenTransfer = `
-{
-	"amount": "0x00000000000000000000000000000000000000000000000d8d726b7177a80000",
-	"block": 2620166,
-	"origin": "0xB853D6a965fBC047aaA9f04d774d53861d7eD653",
-	"receiver": "0x9f3742C2c2fE66c7FCa08d77d2262C22e3D56ac8",
-	"timestamp": 1556569300,
-	"txId": "0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517"
-}
-`
-
-const transaction = `
-{  
-   "block":2620166,
-   "blockRef":"0x003578d93e73a9ca",
-   "chainTag":74,
-   "clauses":[  
-      {  
-         "data":"0xa9059cbb0000000000000000000000009f3742c2c2fe66c7fca08d77d2262c22e3d56ac8000000000000000000000000000000000000000000000008ac7230489e800000",
-         "numValue":0,
-         "to":"0x0000000000000000000000000000456e65726779",
-         "txClauseIndex":0,
-         "txId":"0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
-         "value":"0x0"
-      }
-   ],
-   "expiration":720,
-   "gas":160000,
-   "gasPriceCoef":0,
-   "id":"0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
-   "meta":{  
-      "blockID":"0x003578db7b662faecc743f3a401515eef5baebe16c27e635f79bcfca3b8a39dc",
-      "blockNumber":3504347,
-      "blockTimestamp":1565458520
-   },
-   "nonce":"0x8e60abce86ae",
-   "numClauses":1,
-   "origin":"0xb853d6a965fbc047aaa9f04d774d53861d7ed653",
-   "receipt":{  
-      "gasPayer":"0xa760bdcbf6c2935d2f1591a38f23251619f802ad",
-      "gasUsed":36582,
-      "meta":{  
-         "blockID":"0x003578db7b662faecc743f3a401515eef5baebe16c27e635f79bcfca3b8a39dc",
-         "blockNumber":3504347,
-         "blockTimestamp":1565458520,
-         "txID":"0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
-         "txOrigin":"0xb853d6a965fbc047aaa9f04d774d53861d7ed653"
-      },
-      "outputs":[  
-         {  
-            "events":[  
-               {  
-                  "address":"0x0000000000000000000000000000456e65726779",
-                  "topics":[  
-                     "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-                     "0x000000000000000000000000b853d6a965fbc047aaa9f04d774d53861d7ed653",
-                     "0x0000000000000000000000009f3742c2c2fe66c7fca08d77d2262c22e3d56ac8"
-                  ],
-                  "data":"0x00000000000000000000000000000000000000000000000d8d726b7177a80000"
-               }
-            ],
-            "transfers":[  
-               {  
-                  "sender":"0xb853d6a965fbc047aaa9f04d774d53861d7ed653",
-                  "recipient":"0xda623049a13df5c8a24f0d7713f4add4ab136b1f",
-                  "amount":"0x29bde5885d7ac80000"
-               }
-            ]
-         }
-      ],
-      "paid":"0x1236efcbcbb340000",
-      "reverted":false,
-      "reward":"0x984d9c8dd8008000"
-   },
-   "reverted":0,
-   "size":191,
-   "timestamp":1556569300,
-   "totalValue":0
-}
-`
-
-var expectedTransferTrx = blockatlas.Tx{
-	ID:       "0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
+var expectedTransfer = blockatlas.Tx{
+	ID:       "0xe75d6f28297d910faf31d7aaff9bc57faf14895ffc65da056a90b5a258c17784",
 	Coin:     coin.VET,
-	From:     "0xB853D6a965fBC047aaA9f04d774d53861d7eD653",
-	To:       "0xdA623049A13df5c8a24f0d7713F4adD4ab136B1f",
-	Fee:      "21000000000000000000",
-	Date:     1556569300,
+	From:     "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+	To:       "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+	Date:     1537983090,
 	Type:     "transfer",
+	Fee:      blockatlas.Amount("21000"),
 	Status:   blockatlas.StatusCompleted,
-	Block:    2620166,
-	Sequence: 2620166,
+	Block:    765484,
+	Sequence: 6934600807657731548,
 	Meta: blockatlas.Transfer{
-		Value:    "770000000000000000000",
+		Value:    blockatlas.Amount("50000000000000000000"),
 		Decimals: 18,
 		Symbol:   "VET",
 	},
 }
 
-var expectedVeThorTrx = blockatlas.Tx{
-	ID:       "0x2b8776bd4679fa2afa28b55d66d4f6c7c77522fc878ce294d25e32475b704517",
+func TestNormalizeTransaction(t *testing.T) {
+	tests := []struct {
+		name   string
+		txData string
+		want   blockatlas.TxPage
+	}{
+		{"test normalize tx", transferSrc, blockatlas.TxPage{expectedTransfer}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var tx Tx
+			err := json.Unmarshal([]byte(tt.txData), &tx)
+			assert.Nil(t, err)
+			got, err := NormalizeTransaction(tx)
+			assert.Nil(t, err)
+			assert.Equal(t, len(got), 1, "tx could not be normalized")
+			assert.Equal(t, tt.want, got, "tx don't equal")
+		})
+	}
+}
+
+const transferLogSrc = `{
+  "sender": "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+  "recipient": "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+  "amount": "0x38d7ea4c68000",
+  "meta": {
+    "blockID": "0x000ab61020bcd82739b9956541ccfff3e8aa4f3891e0cbe7d073a0491fc1f87d",
+    "blockNumber": 701968,
+    "blockTimestamp": 1537342090,
+    "txID": "0xc89b224842f3b5edcf4e6950194148be7e83cc951ff4fdfa2490e4c0cf12c80b",
+    "txOrigin": "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+    "clauseIndex": 0
+  }
+}`
+
+var expectedTransferLog = blockatlas.Tx{
+	ID:       "0xc89b224842f3b5edcf4e6950194148be7e83cc951ff4fdfa2490e4c0cf12c80b",
 	Coin:     coin.VET,
-	From:     "0xB853D6a965fBC047aaA9f04d774d53861d7eD653",
-	To:       "0x9f3742C2c2fE66c7FCa08d77d2262C22e3D56ac8",
-	Fee:      "21000000000000000000",
-	Date:     1556569300,
-	Type:     blockatlas.TxNativeTokenTransfer,
+	From:     "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+	To:       "0x15f4d9bed894e2e426d65e3df1480c61fb131a57",
+	Date:     1537342090,
+	Type:     "transfer",
+	Fee:      blockatlas.Amount("0"),
 	Status:   blockatlas.StatusCompleted,
-	Sequence: 2620166,
-	Block:    2620166,
-	Meta: blockatlas.NativeTokenTransfer{
-		Name:     GasName,
-		Symbol:   GasSymbol,
-		TokenID:  GasContract,
+	Block:    701968,
+	Meta: blockatlas.Transfer{
+		Value:    blockatlas.Amount("1000000000000000"),
 		Decimals: 18,
-		Value:    "250000000000000000000",
-		From:     "0xB853D6a965fBC047aaA9f04d774d53861d7eD653",
-		To:       "0x9f3742C2c2fE66c7FCa08d77d2262C22e3D56ac8",
+		Symbol:   "VET",
 	},
 }
 
-func TestNormalizeTransfer(t *testing.T) {
-	var tests = []struct {
-		Receipt  string
-		Clause   string
-		Expected blockatlas.Tx
+func TestNormalizeLogTransaction(t *testing.T) {
+	tests := []struct {
+		name   string
+		txData string
+		want   blockatlas.Tx
 	}{
-		{transferReceipt, transferClause, expectedTransferTrx},
-		// {transferTrx, transferReceipt, transferOutput, address, VeThorContract, expectedVeThorTrx},
+		{"test normalize log tx", transferLogSrc, expectedTransferLog},
 	}
-
-	for _, test := range tests {
-		var receipt TransferReceipt
-		var clause Clause
-
-		// Unmarshal(*t, test.Receipt, &receipt)
-		rErr := json.Unmarshal([]byte(test.Receipt), &receipt)
-		if rErr != nil {
-			t.Fatal(rErr)
-		}
-
-		cErr := json.Unmarshal([]byte(test.Clause), &clause)
-		if cErr != nil {
-			t.Fatal(cErr)
-		}
-
-		var readyTx blockatlas.Tx
-		normTx, err := NormalizeTransfer(&receipt, &clause)
-		if err != nil {
-			t.Fatal("VeChain: Can't normalize transfer", readyTx)
-		}
-		readyTx = normTx
-
-		actual, err := json.Marshal(&readyTx)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		expected, err := json.Marshal(&test.Expected)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if !bytes.Equal(actual, expected) {
-			println(string(actual))
-			println(string(expected))
-			t.Error("Transactions not equal")
-		}
-	}
-}
-
-func TestNormalizeTokenTransfer(t *testing.T) {
-	var tests = []struct {
-		Receipt  string
-		Transfer string
-		Expected blockatlas.Tx
-	}{
-		{transferFailedReceipt, tokenTransfer, expectedVeThorTrx},
-	}
-
-	for _, test := range tests {
-		var receipt TransferReceipt
-		var tt TokenTransfer
-
-		// Unmarshal(*t, test.Receipt, &receipt)
-		rErr := json.Unmarshal([]byte(test.Receipt), &receipt)
-		if rErr != nil {
-			t.Fatal(rErr)
-		}
-
-		ttErr := json.Unmarshal([]byte(test.Transfer), &tt)
-		if ttErr != nil {
-			t.Fatal(ttErr)
-		}
-
-		var readyTx blockatlas.Tx
-		normTx, err := NormalizeTokenTransfer(&tt, &receipt)
-		if err != nil {
-			t.Fatal("VeChain: Can't normalize token transfer", readyTx)
-		}
-		readyTx = normTx
-
-		actual, err := json.Marshal(&readyTx)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		expected, err := json.Marshal(&test.Expected)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if !bytes.Equal(actual, expected) {
-			println(string(actual))
-			println(string(expected))
-			t.Error("Transactions not equal")
-		}
-	}
-}
-
-func TestNormalizeTransaction(t *testing.T) {
-	var tests = []struct {
-		Transaction         string
-		ExpectedTransaction []blockatlas.Tx
-	}{
-		{transaction, []blockatlas.Tx{expectedVeThorTrx, expectedTransferTrx}},
-	}
-
-	for _, test := range tests {
-		var transaction NativeTransaction
-
-		tErr := json.Unmarshal([]byte(test.Transaction), &transaction)
-		if tErr != nil {
-			t.Fatal(tErr)
-		}
-
-		var readyTxs []blockatlas.Tx
-
-		ntxs := NormalizeBlockTransactions(&transaction)
-		readyTxs = append(readyTxs, ntxs...)
-
-		actual, err := json.Marshal(&readyTxs)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		expectedTransactions, err := json.Marshal(&test.ExpectedTransaction)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if !bytes.Equal(actual, expectedTransactions) {
-			println(string(actual))
-			println(string(expectedTransactions))
-			t.Error("Transactions not equal")
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var tx LogTx
+			err := json.Unmarshal([]byte(tt.txData), &tx)
+			assert.Nil(t, err)
+			got, err := NormalizeLogTransaction(tx)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.want, got, "tx don't equal")
+		})
 	}
 }
