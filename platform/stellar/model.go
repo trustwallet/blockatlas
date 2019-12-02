6473842342ebecbf2375213f0c3080b7204cca3f
Mykola <kolya182@gmail.com>
2019-12-01 20:41:24 -0800

    Remove unsed struct firlds

--- platform/stellar/model.go
+++ platform/stellar/model.go
@@ -25,44 +25,17 @@ type Block struct {
 
 // Payment model returned by Horizon
 type Payment struct {
-	ID          string `json:"id"`
-	Type        string `json:"type"`
-	PagingToken string `json:"paging_token"`
-
-	Links struct {
-		Effects struct {
-			Href string `json:"href"`
-		} `json:"effects"`
-		Transaction struct {
-			Href string `json:"href"`
-		} `json:"transaction"`
-	} `json:"_links"`
-
-	SourceAccount string `json:"source_account"`
-	CreatedAt     string `json:"created_at"`
-
-	// create_account and account_merge field
-	Account string `json:"account"`
-
-	// create_account fields
+	ID              string `json:"id"`
+	Type            string `json:"type"`
+	SourceAccount   string `json:"source_account"`
+	CreatedAt       string `json:"created_at"`
+	Account         string `json:"account"`
 	Funder          string `json:"funder"`
 	StartingBalance string `json:"starting_balance"`
-
-	// account_merge fields
-	Into string `json:"into"`
-
-	// payment/path_payment fields
-	From        string `json:"from"`
-	To          string `json:"to"`
-	AssetType   string `json:"asset_type"`
-	AssetCode   string `json:"asset_code"`
-	AssetIssuer string `json:"asset_issuer"`
-	Amount      string `json:"amount"`
-
-	// transaction fields
+	Into            string `json:"into"`
+	From            string `json:"from"`
+	To              string `json:"to"`
+	AssetType       string `json:"asset_type"`
+	Amount          string `json:"amount"`
 	TransactionHash string `json:"transaction_hash"`
-	Memo            struct {
-		Type  string `json:"memo_type"`
-		Value string `json:"memo"`
-	}
 }
