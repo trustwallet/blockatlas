bbeb2a85e25d8e4c152683b600e7d01562c16278
Lorenzo Zanotto <contact@lorenzozanotto.com>
2019-12-12 10:30:55 +0100

    Remove unused Harmony model fields

--- platform/harmony/model.go
+++ platform/harmony/model.go
@@ -15,36 +15,13 @@ type Transaction struct {
 	Gas              string `json:"gas"`
 	GasPrice         string `json:"gasPrice"`
 	Hash             string `json:"hash"`
-	Input            string `json:"input"`
-	Nonce            string `json:"nonce"`
 	To               string `json:"to"`
-	TransactionIndex string `json:"transactionIndex"`
 	Value            string `json:"value"`
-	ShardID          int    `json:"shardID"`
-	ToShardID        int    `json:"toShardID"`
-	V                string `json:"v"`
-	R                string `json:"r"`
-	S                string `json:"s"`
 }
 
 type BlockInfo struct {
-	Difficulty       int           `json:"difficulty"`
-	ExtraData        string        `json:"extraData"`
-	GasLimit         string        `json:"gasLimit"`
-	GasUsed          string        `json:"gasUsed"`
 	Hash             string        `json:"hash"`
-	LogsBloom        string        `json:"logsBloom"`
-	Miner            string        `json:"miner"`
-	MixHash          string        `json:"mixHash"`
-	Nonce            int           `json:"nonce"`
 	Number           string        `json:"number"`
-	ParentHash       string        `json:"parentHash"`
-	ReceiptsRoot     string        `json:"receiptsRoot"`
-	Size             string        `json:"size"`
-	StateRoot        string        `json:"stateRoot"`
-	Timestamp        string        `json:"timestamp"`
 	Transactions     []Transaction `json:"transactions"`
-	TransactionsRoot string        `json:"transactionsRoot"`
-	Uncles           []interface{} `json:"uncles"`
 }
 
