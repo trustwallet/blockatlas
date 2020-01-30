0fd5d6658fcfcebde91fd15b5e2fa17d32cd0171
Danilo Pantani <danpantani@gmail.com>
2020-01-29 23:15:23 -0300

    fix zilliqa nonce

--- platform/zilliqa/model_test.go
+++ platform/zilliqa/model_test.go
@@ -47,3 +47,26 @@ func TestTxRPC_toTx(t *testing.T) {
 		t.Errorf("TxRPC.toTx() = %v, want %v", got, tx)
 	}
 }
+
+func TestTx_NonceValue(t *testing.T) {
+	tests := []struct {
+		name  string
+		nonce interface{}
+		want  uint64
+	}{
+		{"test int", 0, 0},
+		{"test float", 3.4, 3},
+		{"test string", "33", 33},
+		{"test error string", "test", 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := Tx{
+				Nonce: tt.nonce,
+			}
+			if got := tx.NonceValue(); got != tt.want {
+				t.Errorf("NonceValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
