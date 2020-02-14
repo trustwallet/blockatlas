a9e63a52d39f4e346aa4d5710f7fc47246e13f8c
Danilo Pantani <danpantani@gmail.com>
2020-02-13 22:57:18 -0300

    Split asset services methods

--- services/assets/stake.go
+++ services/assets/stake.go
@@ -12,17 +12,6 @@ const (
 	AssetsURL = "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/"
 )
 
-func GetCoinInfo(coinId int, token string) (info *blockatlas.CoinInfo, err error) {
-	c, ok := coin.Coins[uint(coinId)]
-	if !ok {
-		return info, errors.E("coin not found")
-	}
-	url := getCoinInfoUrl(c, token)
-	request := blockatlas.InitClient(url)
-	err = request.GetWithCache(&info, "info/info.json", nil, time.Hour*1)
-	return
-}
-
 func GetValidatorsMap(api blockatlas.StakeAPI) (blockatlas.ValidatorMap, error) {
 	validatorList, err := GetValidators(api)
 	if err != nil {
@@ -96,11 +85,4 @@ func calculateAnnual(annual float64, commission float64) float64 {
 
 func getImage(c coin.Coin, ID string) string {
 	return AssetsURL + c.Handle + "/validators/assets/" + ID + "/logo.png"
-}
-
-func getCoinInfoUrl(c coin.Coin, token string) string {
-	if len(token) == 0 {
-		return AssetsURL + c.Handle
-	}
-	return AssetsURL + c.Handle + "/assets/" + token
 }
