ff31205bd5a129277b9d9e48d6ce97e83359373e
Danilo Pantani <danpantani@gmail.com>
2020-02-19 01:07:07 -0300

    Add support for rpc batch call (#891)

---
+++ pkg/blockatlas/jsonrpc_test.go
@@ -0,0 +1,34 @@
+package blockatlas
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRpcRequests_fillDefaultValues(t *testing.T) {
+	tests := []struct {
+		name string
+		rs   RpcRequests
+		want RpcRequests
+	}{
+		{
+			"test 1",
+			RpcRequests{{Method: "method1", Params: "params1"}},
+			RpcRequests{{Method: "method1", Params: "params1", JsonRpc: JsonRpcVersion, Id: "method1"}},
+		}, {
+			"test 2",
+			RpcRequests{
+				{Method: "method1", Params: "params1"}, {Method: "method2", Params: "params2"}},
+			RpcRequests{
+				{Method: "method1", Params: "params1", JsonRpc: JsonRpcVersion, Id: "method1"},
+				{Method: "method2", Params: "params2", JsonRpc: JsonRpcVersion, Id: "method2"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.rs.fillDefaultValues()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
