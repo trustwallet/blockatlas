80e7e884644b514a2bfb64dfedc8331ebd2bd065
Danilo Pantani <danpantani@gmail.com>
2020-02-18 21:17:54 -0300

    Init platform inside the integration tests

--- platform/platform.go
+++ platform/platform.go
@@ -31,67 +31,67 @@ import (
 	"github.com/trustwallet/blockatlas/platform/zilliqa"
 )
 
-func getVar(name string) string {
+func GetVar(name string) string {
 	return viper.GetString(name)
 }
 
-func getApiVar(coinId uint) string {
+func GetApiVar(coinId uint) string {
 	varName := fmt.Sprintf("%s.api", coin.Coins[coinId].Handle)
-	return getVar(varName)
+	return GetVar(varName)
 }
 
-func getRpcVar(coinId uint) string {
+func GetRpcVar(coinId uint) string {
 	varName := fmt.Sprintf("%s.rpc", coin.Coins[coinId].Handle)
-	return getVar(varName)
+	return GetVar(varName)
 }
 
 func getPlatformMap() blockatlas.Platforms {
 	return blockatlas.Platforms{
-		coin.Fio().Handle:          fio.Init(getVar("fio.api")),
-		coin.Aion().Handle:         aion.Init(getVar("aion.api")),
-		coin.Icon().Handle:         icon.Init(getVar("icon.api")),
-		coin.Tron().Handle:         tron.Init(getVar("tron.api")),
-		coin.Nano().Handle:         nano.Init(getVar("nano.api")),
-		coin.Nimiq().Handle:        nimiq.Init(getVar("nimiq.api")),
-		coin.Iotex().Handle:        iotex.Init(getVar("iotex.api")),
-		coin.Theta().Handle:        theta.Init(getVar("theta.api")),
-		coin.Waves().Handle:        waves.Init(getVar("waves.api")),
-		coin.Ripple().Handle:       ripple.Init(getVar("ripple.api")),
-		coin.Harmony().Handle:      harmony.Init(getVar("harmony.api")),
-		coin.Vechain().Handle:      vechain.Init(getVar("vechain.api")),
-		coin.Nebulas().Handle:      nebulas.Init(getVar("nebulas.api")),
-		coin.Ontology().Handle:     ontology.Init(getVar("ontology.api")),
-		coin.Algorand().Handle:     algorand.Init(getVar("algorand.api")),
-		coin.Aeternity().Handle:    aeternity.Init(getVar("aeternity.api")),
-		coin.Tezos().Handle:        tezos.Init(getVar("tezos.api"), getVar("tezos.rpc")),
-		coin.Binance().Handle:      binance.Init(getVar("binance.api"), getVar("binance.dex")),
-		coin.Zilliqa().Handle:      zilliqa.Init(getVar("zilliqa.api"), getVar("zilliqa.key"), getVar("zilliqa.rpc"), getVar("zilliqa.lookup")),
-		coin.Kusama().Handle:       polkadot.Init(coin.KSM, getApiVar(coin.KSM)),
-		coin.Stellar().Handle:      stellar.Init(coin.XLM, getApiVar(coin.XLM)),
-		coin.Kin().Handle:          stellar.Init(coin.KIN, getApiVar(coin.KIN)),
-		coin.Cosmos().Handle:       cosmos.Init(coin.ATOM, getApiVar(coin.ATOM)),
-		coin.Kava().Handle:         cosmos.Init(coin.KAVA, getApiVar(coin.KAVA)),
-		coin.Bitcoin().Handle:      bitcoin.Init(coin.BTC, getApiVar(coin.BTC)),
-		coin.Litecoin().Handle:     bitcoin.Init(coin.LTC, getApiVar(coin.LTC)),
-		coin.Bitcoincash().Handle:  bitcoin.Init(coin.BCH, getApiVar(coin.BCH)),
-		coin.Zcash().Handle:        bitcoin.Init(coin.ZEC, getApiVar(coin.ZEC)),
-		coin.Zcoin().Handle:        bitcoin.Init(coin.XZC, getApiVar(coin.XZC)),
-		coin.Viacoin().Handle:      bitcoin.Init(coin.VIA, getApiVar(coin.VIA)),
-		coin.Ravencoin().Handle:    bitcoin.Init(coin.RVN, getApiVar(coin.RVN)),
-		coin.Groestlcoin().Handle:  bitcoin.Init(coin.GRS, getApiVar(coin.GRS)),
-		coin.Zelcash().Handle:      bitcoin.Init(coin.ZEL, getApiVar(coin.ZEL)),
-		coin.Decred().Handle:       bitcoin.Init(coin.DCR, getApiVar(coin.DCR)),
-		coin.Digibyte().Handle:     bitcoin.Init(coin.DGB, getApiVar(coin.DGB)),
-		coin.Dash().Handle:         bitcoin.Init(coin.DASH, getApiVar(coin.DASH)),
-		coin.Doge().Handle:         bitcoin.Init(coin.DOGE, getApiVar(coin.DOGE)),
-		coin.Qtum().Handle:         bitcoin.Init(coin.QTUM, getApiVar(coin.QTUM)),
-		coin.Gochain().Handle:      ethereum.Init(coin.GO, getApiVar(coin.GO), getRpcVar(coin.GO)),
-		coin.Thundertoken().Handle: ethereum.Init(coin.TT, getApiVar(coin.TT), getRpcVar(coin.TT)),
-		coin.Classic().Handle:      ethereum.Init(coin.ETC, getApiVar(coin.ETC), getRpcVar(coin.ETC)),
-		coin.Poa().Handle:          ethereum.Init(coin.POA, getApiVar(coin.POA), getRpcVar(coin.POA)),
-		coin.Callisto().Handle:     ethereum.Init(coin.CLO, getApiVar(coin.CLO), getRpcVar(coin.CLO)),
-		coin.Wanchain().Handle:     ethereum.Init(coin.WAN, getApiVar(coin.WAN), getRpcVar(coin.WAN)),
-		coin.Tomochain().Handle:    ethereum.Init(coin.TOMO, getApiVar(coin.TOMO), getRpcVar(coin.TOMO)),
-		coin.Ethereum().Handle:     ethereum.InitWitCollection(coin.ETH, getApiVar(coin.ETH), getRpcVar(coin.ETH), getVar("ethereum.collections_api"), getVar("ethereum.collections_api_key")),
+		coin.Fio().Handle:          fio.Init(GetVar("fio.api")),
+		coin.Aion().Handle:         aion.Init(GetVar("aion.api")),
+		coin.Icon().Handle:         icon.Init(GetVar("icon.api")),
+		coin.Tron().Handle:         tron.Init(GetVar("tron.api")),
+		coin.Nano().Handle:         nano.Init(GetVar("nano.api")),
+		coin.Nimiq().Handle:        nimiq.Init(GetVar("nimiq.api")),
+		coin.Iotex().Handle:        iotex.Init(GetVar("iotex.api")),
+		coin.Theta().Handle:        theta.Init(GetVar("theta.api")),
+		coin.Waves().Handle:        waves.Init(GetVar("waves.api")),
+		coin.Ripple().Handle:       ripple.Init(GetVar("ripple.api")),
+		coin.Harmony().Handle:      harmony.Init(GetVar("harmony.api")),
+		coin.Vechain().Handle:      vechain.Init(GetVar("vechain.api")),
+		coin.Nebulas().Handle:      nebulas.Init(GetVar("nebulas.api")),
+		coin.Ontology().Handle:     ontology.Init(GetVar("ontology.api")),
+		coin.Algorand().Handle:     algorand.Init(GetVar("algorand.api")),
+		coin.Aeternity().Handle:    aeternity.Init(GetVar("aeternity.api")),
+		coin.Tezos().Handle:        tezos.Init(GetVar("tezos.api"), GetVar("tezos.rpc")),
+		coin.Binance().Handle:      binance.Init(GetVar("binance.api"), GetVar("binance.dex")),
+		coin.Zilliqa().Handle:      zilliqa.Init(GetVar("zilliqa.api"), GetVar("zilliqa.key"), GetVar("zilliqa.rpc"), GetVar("zilliqa.lookup")),
+		coin.Kusama().Handle:       polkadot.Init(coin.KSM, GetApiVar(coin.KSM)),
+		coin.Stellar().Handle:      stellar.Init(coin.XLM, GetApiVar(coin.XLM)),
+		coin.Kin().Handle:          stellar.Init(coin.KIN, GetApiVar(coin.KIN)),
+		coin.Cosmos().Handle:       cosmos.Init(coin.ATOM, GetApiVar(coin.ATOM)),
+		coin.Kava().Handle:         cosmos.Init(coin.KAVA, GetApiVar(coin.KAVA)),
+		coin.Bitcoin().Handle:      bitcoin.Init(coin.BTC, GetApiVar(coin.BTC)),
+		coin.Litecoin().Handle:     bitcoin.Init(coin.LTC, GetApiVar(coin.LTC)),
+		coin.Bitcoincash().Handle:  bitcoin.Init(coin.BCH, GetApiVar(coin.BCH)),
+		coin.Zcash().Handle:        bitcoin.Init(coin.ZEC, GetApiVar(coin.ZEC)),
+		coin.Zcoin().Handle:        bitcoin.Init(coin.XZC, GetApiVar(coin.XZC)),
+		coin.Viacoin().Handle:      bitcoin.Init(coin.VIA, GetApiVar(coin.VIA)),
+		coin.Ravencoin().Handle:    bitcoin.Init(coin.RVN, GetApiVar(coin.RVN)),
+		coin.Groestlcoin().Handle:  bitcoin.Init(coin.GRS, GetApiVar(coin.GRS)),
+		coin.Zelcash().Handle:      bitcoin.Init(coin.ZEL, GetApiVar(coin.ZEL)),
+		coin.Decred().Handle:       bitcoin.Init(coin.DCR, GetApiVar(coin.DCR)),
+		coin.Digibyte().Handle:     bitcoin.Init(coin.DGB, GetApiVar(coin.DGB)),
+		coin.Dash().Handle:         bitcoin.Init(coin.DASH, GetApiVar(coin.DASH)),
+		coin.Doge().Handle:         bitcoin.Init(coin.DOGE, GetApiVar(coin.DOGE)),
+		coin.Qtum().Handle:         bitcoin.Init(coin.QTUM, GetApiVar(coin.QTUM)),
+		coin.Gochain().Handle:      ethereum.Init(coin.GO, GetApiVar(coin.GO), GetRpcVar(coin.GO)),
+		coin.Thundertoken().Handle: ethereum.Init(coin.TT, GetApiVar(coin.TT), GetRpcVar(coin.TT)),
+		coin.Classic().Handle:      ethereum.Init(coin.ETC, GetApiVar(coin.ETC), GetRpcVar(coin.ETC)),
+		coin.Poa().Handle:          ethereum.Init(coin.POA, GetApiVar(coin.POA), GetRpcVar(coin.POA)),
+		coin.Callisto().Handle:     ethereum.Init(coin.CLO, GetApiVar(coin.CLO), GetRpcVar(coin.CLO)),
+		coin.Wanchain().Handle:     ethereum.Init(coin.WAN, GetApiVar(coin.WAN), GetRpcVar(coin.WAN)),
+		coin.Tomochain().Handle:    ethereum.Init(coin.TOMO, GetApiVar(coin.TOMO), GetRpcVar(coin.TOMO)),
+		coin.Ethereum().Handle:     ethereum.InitWitCollection(coin.ETH, GetApiVar(coin.ETH), GetRpcVar(coin.ETH), GetVar("ethereum.collections_api"), GetVar("ethereum.collections_api_key")),
 	}
 }
