dd00bec0e4d71393ddfe16b41c40ec8977b29ff1
prazd <alexokda@list.ru>
2020-02-24 15:41:55 +0300

    Use only MQ in dispatcher(remove POST webhook method)

--- internal/init.go
+++ internal/init.go
@@ -7,8 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"github.com/trustwallet/blockatlas/config"
-	"github.com/trustwallet/blockatlas/mq"
-	"github.com/trustwallet/blockatlas/observer"
 	"github.com/trustwallet/blockatlas/pkg/logger"
 	"github.com/trustwallet/blockatlas/storage"
 	"path/filepath"
@@ -73,25 +71,6 @@ func InitAPIWithRedis(defaultPort, defaultConfigPath string) (string, string, *g
 	}
 
 	return port, confPath, &sg, cache
-}
-
-func InitRabbitMQ() observer.DispatchProtocol {
-	dispatchProtocolStr := viper.GetString("observer.dispatch_protocol")
-	dispatchProtocol := observer.DispatchProtocol(dispatchProtocolStr)
-
-	if dispatchProtocol != observer.AMQP && dispatchProtocol != observer.HTTP {
-		logger.Fatal("DispatchProtocol must be amqp (MQ) or http", logger.Params{"protocol": dispatchProtocol})
-	}
-
-	url := viper.GetString("observer.rabbitmq.uri")
-
-	if dispatchProtocol == "amqp" {
-		err := mq.Init(url)
-		if err != nil {
-			logger.Fatal("Failed to init Rabbit MQ", logger.Params{"url": url})
-		}
-	}
-	return dispatchProtocol
 }
 
 func LogVersionInfo() {
