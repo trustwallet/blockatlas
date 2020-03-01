94ce25c5274394139328cc6310a9f33456c41965
Nick Kozlov <Enormousrage@gmail.com>
2020-03-01 06:02:07 +0300

    Update: consumer logic, nginx, compose Add: update event, Ack (#943)

--- services/subscription/subscribtion.go
+++ services/subscription/subscribtion.go
@@ -11,6 +11,7 @@ import (
 const (
 	AddSubscription    blockatlas.SubscriptionOperation = "AddSubscription"
 	DeleteSubscription blockatlas.SubscriptionOperation = "DeleteSubscription"
+	UpdateSubscription blockatlas.SubscriptionOperation = "UpdateSubscription"
 )
 
 func Consume(delivery amqp.Delivery, storage storage.Addresses) {
@@ -19,20 +20,44 @@ func Consume(delivery amqp.Delivery, storage storage.Addresses) {
 	if err != nil {
 		logger.Fatal(err)
 	}
-	subscriptions := event.ParseSubscriptions()
+	newSubscriptions := event.ParseSubscriptions(event.NewSubscriptions)
+	oldSubscriptions := event.ParseSubscriptions(event.OldSubscriptions)
+	params := logger.Params{"operation": event.Operation, "guid": event.GUID, "new_subscriptions_len": len(newSubscriptions), "old_subscriptions_len": len(oldSubscriptions)}
 
 	switch event.Operation {
+	case UpdateSubscription:
+		err := storage.DeleteSubscriptions(oldSubscriptions)
+		if err != nil {
+			logger.Error(err, params)
+		}
+		err = storage.AddSubscriptions(newSubscriptions)
+		if err != nil {
+			logger.Error(err, params)
+		}
+		err = delivery.Ack(false)
+		if err != nil {
+			logger.Error(err, params)
+		}
+		logger.Info("Updated", params)
 	case AddSubscription:
-		err := storage.AddSubscriptions(subscriptions)
+		err = storage.AddSubscriptions(newSubscriptions)
 		if err != nil {
-			logger.Fatal(err, logger.Params{"operation": event.Operation, "guid": event.GUID})
+			logger.Error(err, params)
 		}
-		logger.Info("Success", logger.Params{"operation": event.Operation, "guid": event.GUID, "subscriptions_len": len(subscriptions)})
+		err = delivery.Ack(false)
+		if err != nil {
+			logger.Error(err, params)
+		}
+		logger.Info("Added", params)
 	case DeleteSubscription:
-		err := storage.DeleteSubscriptions(nil)
+		err := storage.DeleteSubscriptions(oldSubscriptions)
 		if err != nil {
-			logger.Fatal(err, logger.Params{"operation": event.Operation, "guid": event.GUID})
+			logger.Error(err, params)
 		}
-		logger.Info("Success", logger.Params{"operation": event.Operation, "guid": event.GUID, "subscriptions_len": len(subscriptions)})
+		err = delivery.Ack(false)
+		if err != nil {
+			logger.Error(err, params)
+		}
+		logger.Info("Deleted", params)
 	}
 }
