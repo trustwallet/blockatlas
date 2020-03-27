c2c8f14c6759b7e417e02f27eb7cb84d3c6d9aa3
Mykola <kolya182@gmail.com>
2020-03-27 16:48:08 -0700

    Simplify block trx fetching

--- platform/tezos/block.go
+++ platform/tezos/block.go
@@ -3,7 +3,6 @@ package tezos
 import (
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"github.com/trustwallet/blockatlas/pkg/logger"
-	"sync"
 )
 
 func (p *Platform) CurrentBlockNumber() (int64, error) {
@@ -12,25 +11,10 @@ func (p *Platform) CurrentBlockNumber() (int64, error) {
 
 func (p *Platform) GetBlockByNumber(num int64) (*blockatlas.Block, error) {
 	txTypes := []string{TxTypeTransaction, TxTypeDelegation}
-	var wg sync.WaitGroup
-	out := make(chan []Transaction, len(txTypes))
-	wg.Add(len(txTypes))
-	for _, t := range txTypes {
-		go func(txType string, num int64, wg *sync.WaitGroup) {
-			defer wg.Done()
-			txs, err := p.client.GetBlockByNumber(num, txType)
-			if err != nil {
-				logger.Error("GetAddrTxs", err, logger.Params{"txType": txType, "num": num})
-				return
-			}
-			out <- txs
-		}(t, num, &wg)
-	}
-	wg.Wait()
-	close(out)
-	srcTxs := make([]Transaction, 0)
-	for r := range out {
-		srcTxs = append(srcTxs, r...)
+	srcTxs, err := p.client.GetBlockByNumber(num, txTypes)
+	if err != nil {
+		logger.Error("GetAddrTxs", err, logger.Params{"txType": txTypes, "num": num})
+		return nil, err
 	}
 	txs := NormalizeTxs(srcTxs, "")
 	return &blockatlas.Block{
