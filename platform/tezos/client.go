888269d1087a64f597a1390099fedd955334760c
Mykola <kolya182@gmail.com>
2020-03-27 00:16:53 -0700

    Adopt client

--- platform/tezos/client.go
+++ platform/tezos/client.go
@@ -1,27 +1,38 @@
 package tezos
 
 import (
-	"net/url"
-	"strconv"
-
+	"fmt"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
+	"net/url"
 )
 
 type Client struct {
 	blockatlas.Request
 }
 
-func (c *Client) GetTxsOfAddress(address string, txType TxType) (txs []Transaction, err error) {
-	err = c.Get(&txs, "v1/"+string(txType), url.Values{"n": {"50"}, "p": {"0"}, "account": {address}})
+func (c *Client) GetTxsOfAddress(address string, txType TxType) (txs ExplorerAccount, err error) {
+	var path = fmt.Sprintf("/account/%s/op", address)
+	err = c.Get(&txs, path, url.Values{
+		"order": {"desc"},
+		"type":  {string(txType)},
+		"limit": {"25"},
+	})
 	return
 }
 
-func (c *Client) GetCurrentBlock() (height int64, err error) {
-	err = c.Get(&height, "v1/blocks_num", nil)
-	return
+// Get last indexed block by explorer
+func (c *Client) GetCurrentBlock() (int64, error) {
+	var status Status
+	err := c.Get(&status, "/status", nil)
+	return status.Indexed, err
 }
 
-func (c *Client) GetBlockByNumber(num int64, txType TxType) (txs []Transaction, err error) {
-	err = c.Get(&txs, "v1/"+string(txType), url.Values{"n": {"50"}, "p": {"0"}, "block": {strconv.Itoa(int(num))}})
-	return
+func (c *Client) GetBlockByNumber(num int64, txType TxType) ([]Transaction, error) {
+	var blockOps ExplorerAccount
+	var path = fmt.Sprintf("/account/%d/op", num)
+	err := c.Get(&blockOps, path, url.Values{
+		"limit": {"500"}, // TODO Remove once fixed https://github.com/blockwatch-cc/tzindex/issues/17
+		"type":  {string(txType)},
+	})
+	return blockOps.Transactions, err
 }
