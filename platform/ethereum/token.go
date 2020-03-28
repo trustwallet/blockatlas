6d09675639a1d2df6392c0510ca314e1181f0c64
Nick Kozlov <Enormousrage@gmail.com>
2020-03-28 04:27:10 +0300

    Fix issue with hardcoded token type for any eth like platform (#983)

--- platform/ethereum/token.go
+++ platform/ethereum/token.go
@@ -1,6 +1,7 @@
 package ethereum
 
 import (
+	"github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 )
 
@@ -14,13 +15,33 @@ func (p *Platform) GetTokenListByAddress(address string) (blockatlas.TokenPage, 
 
 // NormalizeToken converts a Ethereum token into the generic model
 func NormalizeToken(srcToken *Contract, coinIndex uint) (t blockatlas.Token, ok bool) {
+	var tokenType blockatlas.TokenType
+	switch coinIndex {
+	case coin.Ethereum().ID:
+		tokenType = blockatlas.TokenTypeERC20
+	case coin.Classic().ID:
+		tokenType = blockatlas.TokenTypeETC20
+	case coin.Poa().ID:
+		tokenType = blockatlas.TokenTypePOA20
+	case coin.Callisto().ID:
+		tokenType = blockatlas.TokenTypeCLO20
+	case coin.Wanchain().ID:
+		tokenType = blockatlas.TokenTypeWAN20
+	case coin.Thundertoken().ID:
+		tokenType = blockatlas.TokenTypeTT20
+	case coin.Gochain().ID:
+		tokenType = blockatlas.TokenTypeGO20
+	default:
+		tokenType = "unknown"
+	}
+
 	t = blockatlas.Token{
 		Name:     srcToken.Name,
 		Symbol:   srcToken.Symbol,
 		TokenID:  srcToken.Address,
 		Coin:     coinIndex,
 		Decimals: srcToken.Decimals,
-		Type:     blockatlas.TokenTypeERC20,
+		Type:     tokenType,
 	}
 
 	return t, true
