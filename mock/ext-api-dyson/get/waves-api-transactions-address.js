0308c0e35742772563b2545d032d8ef6260ba723
Nick Kozlov <enormousrage@gmail.com>
2020-03-29 23:24:49 +0300

    Fix Waves api mock attempt 2

--- mock/ext-api-dyson/get/waves-api-transactions-address.js
+++ mock/ext-api-dyson/get/waves-api-transactions-address.js
@@ -12,11 +12,11 @@ module.exports = {
                 [
                     {
                         senderPublicKey: "2UstBx1nMYQ2mPeaJi6tv9oUFCUHLvU1G7nS8Leazsbw",
-                        amount: Number(369133368000),
+                        amount: parseFloat("369133368000"),
                         signature: "5Hw5SW7C1EK8hE2YKawFCAWcJoB1Z5NSZFkA65bS3PcDY9w2fi7etPCJDamK2WNb14RWa3BykdT5yFd64SxodjeQ",
-                        fee: Number(100000),
-                        type: Number(4),
-                        version: Number(1),
+                        fee: parseFloat("100000"),
+                        type: parseFloat("4"),
+                        version: parseFloat("1"),
                         attachment: "",
                         sender: "3PJ4q4sqriJs2y7Z45wmbLrbmV9MDecbPxD",
                         feeAssetId: null,
@@ -27,16 +27,16 @@ module.exports = {
                         recipient: "3PLrCnhKyX5iFbGDxbqqMvea5VAqxMcinPW",
                         feeAsset: null,
                         id: "23JGFzBh65fzZArK6KSeRqjjBG5WnQshJJkUv53hCE1E",
-                        timestamp: Number(1582527770493),
-                        height: Number(1943922)
+                        timestamp: parseFloat("1582527770493"),
+                        height: parseFloat("1943922")
                     },
                     {
                         senderPublicKey: "3uT3a9ceebFf6vEa5DmedD5pK6xa1GY7PnvhWPMVyqxC",
-                        amount: Number(369133468000),
+                        amount: parseFloat("369133468000"),
                         signature: "3Nrz47KpD3U39Nf7Los23MDoukqXCrCJun1BqnUgjpy9iZLfXc163dTrz4wVvURC2yiULsNYDYA2pxTPGWpBotc5",
-                        fee: Number(100000),
-                        type: Number(4),
-                        version: Number(1),
+                        fee: parseFloat("100000"),
+                        type: parseFloat("4"),
+                        version: parseFloat("1"),
                         attachment: "Paribu",
                         sender: "3PHYYqBA6ZfBsoGsrXP8r7ZptLXYdGDt1Cm",
                         feeAssetId: null,
@@ -47,8 +47,8 @@ module.exports = {
                         recipient: "3PJ4q4sqriJs2y7Z45wmbLrbmV9MDecbPxD",
                         feeAsset: null,
                         id: "1456haw7zSTKDmVSbY1njrYdskX7xKbv241c5WJjWhCu",
-                        timestamp: Number(1582527244669),
-                        height: Number(1943911)
+                        timestamp: parseFloat("1582527244669"),
+                        height: parseFloat("1943911")
                     }
                 ]
             ]
