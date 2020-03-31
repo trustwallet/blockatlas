e078561cfffd0252b1eb1bcc67fc5ccc452b442b
mykola.eth <3277207+kolya182@users.noreply.github.com>
2020-03-30 21:11:07 -0700

    Fix fee decimal (#990)

--- platform/tezos/transaction_test.go
+++ platform/tezos/transaction_test.go
@@ -26,7 +26,7 @@ var (
 		Coin:      1729,
 		From:      addr1,
 		To:        addr1,
-		Fee:       "0.0015",
+		Fee:       "1500",
 		Date:      1582894746,
 		Block:     843988,
 		Status:    "completed",
