fd8a0b0fe3a92deb48084e8e44eda382ed372ec1
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-03-31 11:05:22 +0200

    FIO history, use actor as input to API.

---
+++ platform/fio/actor.go
@@ -0,0 +1,105 @@
+package fio
+
+import (
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/trustwallet/blockatlas/pkg/errors"
+)
+
+func actorFromPublicKeyOrActor(addressOrActor string) string {
+	len := len(addressOrActor)
+	if len >= 51 && len <= 55 {
+		// assume public key string
+		pkBytes, err := bytesFromPublicKeyString(addressOrActor)
+		if err != nil {
+			return ""
+		}
+		return actorFromPublicKeyBytes(pkBytes)
+	}
+	if len <= 13 {
+		// assume actor
+		return addressOrActor
+	}
+	return ""
+}
+
+func actorFromPublicKey(address string) string {
+	pkBytes, err := bytesFromPublicKeyString(address)
+	if err != nil {
+		return ""
+	}
+	return actorFromPublicKeyBytes(pkBytes)
+}
+
+func actorFromPublicKeyBytes(pkBytes []byte) string {
+	shortenedKey := shortenKey(pkBytes)
+	name13 := getName(shortenedKey)
+	// trim to 12 characters
+	return name13[:12]
+}
+
+func bytesFromPublicKeyString(address string) ([]byte, error) {
+	if address[:3] != "FIO" {
+		return nil, errors.E("Invalid FIO public key prefix")
+	}
+	array := base58.Decode(address[3:])
+	if len(array) != 37 {
+		return nil, errors.E("Invalid FIO public key length")
+	}
+	return array, nil
+}
+
+func mask12(len int) byte {
+	if len == 12 {
+		return 0x0f
+	}
+	return 0x1f
+}
+
+func mask0(len int) byte {
+	if len == 0 {
+		return 0x0f
+	}
+	return 0x1f
+}
+
+func shortenKey(addrKey []byte) uint64 {
+	var (
+		res uint64 = 0
+		i   int    = 1 // Ignore key head
+		len int    = 0
+	)
+	for len <= 12 {
+		//assert(i < 33)
+		trimmedChar := uint64(addrKey[i] & mask12(len))
+		if trimmedChar == 0 {
+			i++
+			continue
+		} // Skip a zero and move to next
+		var shuffle byte = 0
+		if len < 12 {
+			shuffle = byte(5*(12-len) - 1)
+		}
+		res = res | (trimmedChar << shuffle)
+		len++
+		i++
+	}
+	return res
+}
+
+func getName(shortKey uint64) string {
+	var (
+		charmap string = ".12345abcdefghijklmnopqrstuvwxyz"
+		str     [13]byte
+		tmp     uint64 = shortKey
+	)
+	for i := 0; i <= 12; i++ {
+		c := charmap[tmp&uint64(mask0(i))]
+		str[12-i] = c
+		if i == 0 {
+			tmp = tmp >> 4
+		} else {
+			tmp = tmp >> 5
+		}
+	}
+	return string(str[:])
+}
--- platform/fio/transaction.go
+++ platform/fio/transaction.go
@@ -1,16 +1,17 @@
 package fio
 
 import (
+	"encoding/json"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"github.com/trustwallet/blockatlas/pkg/errors"
-	"encoding/json"
 	"strconv"
 	"strings"
 	"time"
 )
 
 func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err error) {
-	account := address // TODO this should be actor
+	// take actor from address
+	account := actorFromPublicKey(address)
 	actions, err := p.client.getTransactions(account)
 	if err != nil {
 		return nil, err
@@ -30,26 +31,26 @@ func (p *Platform) GetTxsByAddress(address string) (page blockatlas.TxPage, err 
 func (p *Platform) Normalize(action *Action, account string) (blockatlas.Tx, error) {
 	var (
 		to, from string
-		amount blockatlas.Amount
+		amount   blockatlas.Amount
 	)
 
 	// Action Act.Name == "trnsfiopubky" not handled
 	if action.ActionTrace.Act.Account == "fio.token" &&
-	   action.ActionTrace.Act.Name == "transfer" {
+		action.ActionTrace.Act.Name == "transfer" {
 		//if action.ActionTrace.Act.Name == "transfer" {
-			// convert to action-specific data
-			var actionData ActionData
-			dataJson, _ := json.Marshal(action.ActionTrace.Act.Data)
-			dataErr := json.Unmarshal(dataJson, &actionData)
-			if dataErr != nil {
-				return blockatlas.Tx{}, errors.E("Unparseable Data")
-			}
-			from = actionData.From
-			to = actionData.To
-			amountNum, err := strconv.ParseFloat(strings.Split(actionData.Quantity, " ")[0], 64)
-			if err == nil {
-				amount = blockatlas.Amount(strconv.Itoa(int(amountNum * 1000000000)))
-			}
+		// convert to action-specific data
+		var actionData ActionData
+		dataJson, _ := json.Marshal(action.ActionTrace.Act.Data)
+		dataErr := json.Unmarshal(dataJson, &actionData)
+		if dataErr != nil {
+			return blockatlas.Tx{}, errors.E("Unparseable Data")
+		}
+		from = actionData.From
+		to = actionData.To
+		amountNum, err := strconv.ParseFloat(strings.Split(actionData.Quantity, " ")[0], 64)
+		if err == nil {
+			amount = blockatlas.Amount(strconv.Itoa(int(amountNum * 1000000000)))
+		}
 		//}
 		//if action.ActionTrace.Act.Name == "trnsfiopubky" {
 		//	to = actionData.PayeePublicKey
@@ -70,8 +71,8 @@ func (p *Platform) Normalize(action *Action, account string) (blockatlas.Tx, err
 				Symbol:   p.Coin().Symbol,
 				Decimals: p.Coin().Decimals,
 			},
-			Memo:   actionData.Memo,
-			Type:   blockatlas.TxTransfer,
+			Memo: actionData.Memo,
+			Type: blockatlas.TxTransfer,
 		}
 		tx.Direction = tx.GetTransactionDirection(account)
 		return tx, nil
