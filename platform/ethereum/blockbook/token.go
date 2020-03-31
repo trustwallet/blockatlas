111e34a95a287ed4ae381e0aced7bf09acc2995b
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-03-31 16:24:33 +0800

    Fix txs and models

--- platform/ethereum/blockbook/token.go
+++ platform/ethereum/blockbook/token.go
@@ -12,22 +12,17 @@ func (c *Client) GetTokenList(address string, coinIndex uint) (blockatlas.TokenP
 	return NormalizeTokens(account.Tokens, coinIndex), nil
 }
 
-// NormalizeTxs converts multiple Ethereum tokens
 func NormalizeTokens(srcTokens []Token, coinIndex uint) []blockatlas.Token {
 	tokenPage := make([]blockatlas.Token, 0)
 	for _, srcToken := range srcTokens {
-		token, ok := NormalizeToken(&srcToken, coinIndex)
-		if !ok {
-			continue
-		}
+		token := NormalizeToken(&srcToken, coinIndex)
 		tokenPage = append(tokenPage, token)
 	}
 	return tokenPage
 }
 
-// NormalizeToken converts a Blockbook Ethereum token into the generic model
-func NormalizeToken(srcToken *Token, coinIndex uint) (t blockatlas.Token, ok bool) {
-	t = blockatlas.Token{
+func NormalizeToken(srcToken *Token, coinIndex uint) blockatlas.Token {
+	return blockatlas.Token{
 		Name:     srcToken.Name,
 		Symbol:   srcToken.Symbol,
 		TokenID:  srcToken.Contract,
@@ -35,6 +30,4 @@ func NormalizeToken(srcToken *Token, coinIndex uint) (t blockatlas.Token, ok boo
 		Decimals: srcToken.Decimals,
 		Type:     blockatlas.TokenTypeERC20,
 	}
-
-	return t, true
 }
