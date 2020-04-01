f007d022dbd9968c78c56a3a2deef77d5f60e3be
Adam R <13562139+catenocrypt@users.noreply.github.com>
2020-04-01 17:43:17 +0200

    FIO transaction history (#984)
    * FIO mock script: more generic.
    
    * Update FIO API endpoint.
    
    * FIO transaction history.
    
    * Fill all fields.
    
    * Minor review finding.
    
    * Formatting.
    
    * FIO history, use actor as input to API.
    
    * Also accept actors, not only public keys.
    
    * FIO history: add tests (mocked).
    
    * Minor, remove console logs.
    
    * Additional test.
    
    * Small cleanup
    
    * Review comments, code cleanup.
    
    * Review comments, error handling.
    
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>
    Co-authored-by: Nick Kozlov <enormousrage@gmail.com>

--- platform/fio/model.go
+++ platform/fio/model.go
@@ -1,5 +1,45 @@
 package fio
 
+// ActionData (from get_actions)
+type ActionData struct {
+	From     string `json:"from"`
+	To       string `json:"to"`
+	Quantity string `json:"quantity"`
+	Memo     string `json:"memo"`
+}
+
+// ActionAct (from get_actions)
+type ActionAct struct {
+	Account string      `json:"account"`
+	Name    string      `json:"name"`
+	Data    interface{} `json:"data"` // Structure of data is action-specific
+}
+
+// ActionTrace
+type ActionTrace struct {
+	Act   ActionAct `json:"act"`
+	TrxID string    `json:"trx_id"`
+}
+
+// Action (from get_actions)
+type Action struct {
+	BlockNum    uint64      `json:"block_num"`
+	BlockTime   string      `json:"block_time"`
+	ActionTrace ActionTrace `json:"action_trace"`
+}
+
+// GetActionsRequest request struct for get_actions
+type GetActionsRequest struct {
+	AccountName string `json:"account_name"`
+	// pos, offset
+	Sort string `json:"sort"` // desc
+}
+
+// GetActionsResponse request struct for get_actions
+type GetActionsResponse struct {
+	Actions []Action `json:"actions"`
+}
+
 // GetPubAddressRequest request struct for get_pub_address
 type GetPubAddressRequest struct {
 	FioAddress string `json:"fio_address"`
@@ -10,6 +50,5 @@ type GetPubAddressRequest struct {
 // GetPubAddressResponse response struct for get_pub_address
 type GetPubAddressResponse struct {
 	PublicAddress string `json:"public_address"`
-	BlockNum      int    `json:"block_num"`
 	Message       string `json:"message"`
 }
