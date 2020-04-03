8850c21b6cde7cbca81c6de5e0da18a93d58b1cb
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-04-03 11:49:38 +0200

    FIO API: switch to fio.greymass.com.

--- mock/ext-api-dyson/post/fio-api-history.js
+++ mock/ext-api-dyson/post/fio-api-history.js
@@ -1,13 +1,13 @@
 /// FIO RPC API Mock, history API
-/// curl -H "Content-Type: application/json" -d '{"account_name": "ezsmbcy2opod"}' https://fio.eosphere.io/v1/history/get_actions
+/// curl -H "Content-Type: application/json" -d '{"account_name": "ezsmbcy2opod"}' https://fio.greymass.com/v1/history/get_actions
 /// curl -H "Content-Type: application/json" -d '{"account_name": "ezsmbcy2opod"}' http://localhost:3000/fio-api/v1/history/get_actions
 /// curl "http://localhost:8420/v1/fio/FIO7Q3XfQ2ocGP1zYst6Sfx5qrsiZ865Cu8So2atrub9JN94so7gt"
 
 module.exports = {
     path: '/fio-api/v1/history/:action',
     template: function(params, query, body) {
-        //console.log(params);
-        //console.log(body);
+        console.log(params);
+        console.log(body);
         if (params.action === 'get_actions') {
             switch (body.account_name) {
                 case 'ezsmbcy2opod': // FIO7Q3XfQ2ocGP1zYst6Sfx5qrsiZ865Cu8So2atrub9JN94so7gt
