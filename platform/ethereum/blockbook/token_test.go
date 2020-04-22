0c9eeb0fde7829d909c535aee565a5d1c0672118
Mykola <kolya182@gmail.com>
2020-04-21 21:02:06 -0700

    flatten test and add test zero balance token

--- platform/ethereum/blockbook/token_test.go
+++ platform/ethereum/blockbook/token_test.go
@@ -9,39 +9,27 @@ import (
 
 func TestNormalizeToken(t *testing.T) {
 	type args struct {
-		srcToken  *Token
+		srcToken  Token
 		coinIndex uint
 	}
 	tests := []struct {
 		name string
 		args args
-		want blockatlas.Token
+		want []blockatlas.Token
 	}{
 		{
-			name: "Test Normalize Token",
-			args: args{
-				srcToken: &Token{
-					Type:     "ERC20",
-					Name:     "USD//C",
-					Contract: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-					Symbol:   "USDC",
-					Decimals: 6,
-				},
-				coinIndex: 60,
-			},
-			want: blockatlas.Token{
-				Name:     "USD//C",
-				Symbol:   "USDC",
-				Decimals: 6,
-				TokenID:  "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-				Type:     "ERC20",
-				Coin:     60,
-			},
+			name: "Should normalize and return token with balance",
+			args: args{srcToken: Token{Balance: "100", Type: "ERC20", Name: "USD//C", Contract: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", Symbol: "USDC", Decimals: 6}, coinIndex: 60},
+			want: []blockatlas.Token{{Type: "ERC20", Name: "USD//C", TokenID: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", Symbol: "USDC", Decimals: 6, Coin: 60}},
+		},		{
+			name: "Should not return token with zero balance",
+			args: args{srcToken: Token{Balance: "0", Type: "ERC20", Name: "USD//C", Contract: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", Symbol: "USDC", Decimals: 6}, coinIndex: 60},
+			want: []blockatlas.Token{},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NormalizeToken(tt.args.srcToken, tt.args.coinIndex); !reflect.DeepEqual(got, tt.want) {
+			if got := NormalizeTokens([]Token{tt.args.srcToken}, tt.args.coinIndex); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NormalizeToken() = %v, want %v", got, tt.want)
 			}
 		})
