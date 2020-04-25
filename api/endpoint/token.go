1a7a4a4f3fb8b4d3fb462aad8abc8a601f8eadcd
Alexey Prazdnikov <alexey.praznikov@binance.com>
2020-04-25 05:08:16 +0300

    Rename param

--- api/endpoint/token.go
+++ api/endpoint/token.go
@@ -44,7 +44,7 @@ func GetTokensByAddress(c *gin.Context, tokenAPI blockatlas.TokenAPI) {
 // @Param data body string true "Payload" default({"60": ["0xb3624367b1ab37daef42e1a3a2ced012359659b0"]})
 // @Success 200 {object} blockatlas.ResultsResponse
 // @Router /v3/tokens [post]
-func GetTokens(c *gin.Context, tokens map[uint]blockatlas.TokenAPI) {
+func GetTokens(c *gin.Context, tokensAPIs map[uint]blockatlas.TokenAPI) {
 	var query map[string][]string
 	if err := c.Bind(&query); err != nil {
 		c.JSON(http.StatusInternalServerError, model.CreateErrorResponse(model.InternalFail, err))
@@ -56,7 +56,7 @@ func GetTokens(c *gin.Context, tokens map[uint]blockatlas.TokenAPI) {
 		if err != nil {
 			continue
 		}
-		tokenAPI, ok := tokens[uint(coinNum)]
+		tokenAPI, ok := tokensAPIs[uint(coinNum)]
 		if !ok {
 			continue
 		}
