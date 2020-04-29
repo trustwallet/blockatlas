d15963d6ddedfc9d0b03989e0d7002c0c04a06af
Mykola <kolya182@gmail.com>
2020-04-29 14:21:16 -0700

    fix binance mock

--- mock/ext-api-dyson/get/binance-api-txs.js
+++ mock/ext-api-dyson/get/binance-api-txs.js
@@ -1,7 +1,7 @@
 /// Binance chain block explorer API Mock, tx
 /// Returns:
 /// - Multi-transaction transaction for a specific address
-///   see http://localhost:3000/binance-api/v1/transactions?address=bnb1jeu6gscugy6l2wyatxthkh2hmer4hzevgcmf0q&limit=25&startTime=1578585526000&txAsset=BNB&txType=TRANSFER
+///   see http://localhost:3000/binance-rpc/v1/transactions?address=bnb1jeu6gscugy6l2wyatxthkh2hmer4hzevgcmf0q&limit=25&startTime=1578585526000&txAsset=BNB&txType=TRANSFER
 ///   see https://dex.binance.org/api/v1/transactions?address=bnb1jeu6gscugy6l2wyatxthkh2hmer4hzevgcmf0q&limit=25&startTime=1578585526000&txAsset=BNB&txType=TRANSFER
 ///   see http://localhost:8420/v1/binance/bnb1jeu6gscugy6l2wyatxthkh2hmer4hzevgcmf0q
 /// - empty response for other txHash'es
@@ -31,27 +31,5 @@ module.exports = {
                 "sequence": 68
             }], "total": 1
         }`)
-        // tx: [{
-        //     txHash: "DDA939C475755B2A00123CDA37A5EC442F502EA2983014D04F647C3CB9FB57A0",
-        //     blockHeight: parseFloat('75110907'),
-        //     txType: "TRANSFER",
-        //     timeStamp: "2020-03-17T13:14:30.939Z",
-        //     fromAddr: "bnb166fvwtfra2atta5mm8mygt2fyltktqctgpedcg",
-        //     toAddr: "bnb1mnf0g6zhy2rpy63w8ryc6yp77s0fmqfyzjtkvd",
-        //     value: parseFloat('0.00037500'),
-        //     txAsset: "BNB",
-        //     txFee: parseFloat('0.00060'),
-        //     proposalId: null,
-        //     txAge: parseFloat('1997110'),
-        //     orderId: null,
-        //     code: parseFloat('0'),
-        //     data: null,
-        //     confirmBlocks: parseFloat('0'),
-        //     memo: "",
-        //     source: parseFloat('2'),
-        //     sequence: parseFloat('946')
-        // }
-        // ],
-        // total: 1
     }
 };
