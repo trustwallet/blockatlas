4a4daec9da916144afd977f1ea8dd01fb0d1ca3b
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-05-13 18:32:19 +0200

    More mock update scripts, almost all get scripts.

--- mock/ext-api-dyson/get/ethclassic-api-commands1.js
+++ mock/ext-api-dyson/get/ethclassic-api-commands1.js
@@ -15,73 +15,21 @@ module.exports = {
         switch (params.command1) {
             case 'transactions':
                 if (query.address === '0x7d2d0e153026fb428b885d86de50768d4cfeac37') {
-                    return JSON.parse(`
-                        {
-                            "docs": [
-                                {
-                                    "operations": [],
-                                    "contract": null,
-                                    "_id": "0x8439b360732a43b4bbebe4f00b9f8b2dbe14c6bd52d70598ab7fe6c8503383fe",
-                                    "blockNumber": 9833329,
-                                    "time": 1582237362,
-                                    "nonce": 7071,
-                                    "from": "0x2d4BeCA01fDE8963b3A2DBf8a838e0F2EBaB0FF9",
-                                    "to": "0x7D2D0E153026fB428B885D86De50768D4cFeaC37",
-                                    "value": "18529160000000000",
-                                    "gas": "21000",
-                                    "gasPrice": "1000000000",
-                                    "gasUsed": "21000",
-                                    "input": "0x",
-                                    "error": "",
-                                    "id": "0x8439b360732a43b4bbebe4f00b9f8b2dbe14c6bd52d70598ab7fe6c8503383fe",
-                                    "timeStamp": "1582237362"
-                                },
-                                {
-                                    "operations": [],
-                                    "contract": null,
-                                    "_id": "0x524fb5fedace7bee35c165ac7f641d689122cb21390db7f7384507473a5ba058",
-                                    "blockNumber": 9804290,
-                                    "time": 1581854371,
-                                    "nonce": 6918,
-                                    "from": "0x2d4BeCA01fDE8963b3A2DBf8a838e0F2EBaB0FF9",
-                                    "to": "0x7D2D0E153026fB428B885D86De50768D4cFeaC37",
-                                    "value": "16208400000000000",
-                                    "gas": "21000",
-                                    "gasPrice": "1000000000",
-                                    "gasUsed": "21000",
-                                    "input": "0x",
-                                    "error": "",
-                                    "id": "0x524fb5fedace7bee35c165ac7f641d689122cb21390db7f7384507473a5ba058",
-                                    "timeStamp": "1581854371"
-                                }
-                            ],
-                            "total": 2
-                        }
-                    `);
+                    var fn = "../../ext-api-data/get/" +
+                        "mock%2Fethclassic-api%2Ftransactions%3Faddress%3D0x7d2d0e153026fb428b885d86de50768d4cfeac37.json";
+                    var json = require(fn);
+                    return json;
                 }
+                break;
 
             case 'tokens':
                 if (query.address === '0xa12105efa0663147bddee178f6a741ac15676b79') {
-                    return JSON.parse(`
-                        {
-                            "total": 2,
-                            "docs": [
-                                {
-                                    "address": "0xCA68fE57A0E9987F940Ebcc65fe5F96E7bC30128",
-                                    "name": "Litecoin Classic Token",
-                                    "decimals": 8,
-                                    "symbol": "LCT"
-                                },
-                                {
-                                    "address": "0x2B682bd9d5c31E67a95cbdF0292017C02E51923C",
-                                    "name": "Ether Klown",
-                                    "decimals": 6,
-                                    "symbol": "KLOWN2"
-                                }
-                            ]
-                        }
-                    `);
+                    var fn = "../../ext-api-data/get/" +
+                        "mock%2Fethclassic-api%2Ftokens%3Faddress%3D0xa12105efa0663147bddee178f6a741ac15676b79.json";
+                    var json = require(fn);
+                    return json;
                 }
+                break;
         }
 
         return {error: "Not implemented"};
--- mock/ext-api-dyson/get/tron-api-v1-assets.js
+++ mock/ext-api-dyson/get/tron-api-v1-assets.js
@@ -11,89 +11,22 @@ module.exports = {
         //console.log(query)
         switch (params.arg2) {
             case '1002000':
-                    return JSON.parse(`
-                        {
-                            "success": true,
-                            "meta": {
-                                "at": 1586265512148,
-                                "page_size": 1
-                            },
-                            "data": [
-                                {
-                                    "id": "1002000",
-                                    "abbr": "BTT",
-                                    "description": "Official Token of BitTorrent Protocol",
-                                    "name": "BitTorrent",
-                                    "num": 1,
-                                    "precision": 6,
-                                    "total_supply": "990000000000000000",
-                                    "trx_num": 1,
-                                    "url": "www.bittorrent.com",
-                                    "owner_address": "4137fa1a56eb8c503624701d776d95f6dae1d9f0d6",
-                                    "start_time": 1548000000000,
-                                    "end_time": 1548000001000
-                                }
-                            ]
-                        }
-                    `);
+                var fn = "../../ext-api-data/get/" +
+                    "mock%2Ftron-api%2Fv1%2Fassets%2F1002000.json";
+                var json = require(fn);
+                return json;
                     
             case '1002798':
-                return JSON.parse(`
-                    {
-                        "success": true,
-                        "meta": {
-                            "at": 1586265317378,
-                            "page_size": 1
-                        },
-                        "data": [
-                            {
-                                "id": "1002798",
-                                "abbr": "EPICAL",
-                                "description": "The token of the game Builder III",
-                                "name": "EPICAL",
-                                "num": 1000000000,
-                                "precision": 6,
-                                "total_supply": "10000000000000000",
-                                "trx_num": 1000000000,
-                                "url": "https://builder3.fun",
-                                "vote_score": 0,
-                                "owner_address": "41133b084f5225a9112f4e8527db26b381a32babd0",
-                                "start_time": 1574966426578,
-                                "end_time": 1574966486578
-                            }
-                        ]
-                    }
-                `);
+                var fn = "../../ext-api-data/get/" +
+                    "mock%2Ftron-api%2Fv1%2Fassets%2F1002798.json";
+                var json = require(fn);
+                return json;
 
-
-
-        case '1002814':
-                return JSON.parse(`
-                    {
-                        "success": true,
-                        "meta": {
-                            "at": 1586265542289,
-                            "page_size": 1
-                        },
-                        "data": [
-                            {
-                                "id": "1002814",
-                                "abbr": "AX",
-                                "description": "The token of the game TrainX",
-                                "name": "AX",
-                                "num": 1000000000,
-                                "precision": 6,
-                                "total_supply": "10000000000000000",
-                                "trx_num": 1000000000,
-                                "url": "https://trainx.fun",
-                                "vote_score": 0,
-                                "owner_address": "418e267ead411aaaf671be100a7afe587d4eab0d71",
-                                "start_time": 1576483805985,
-                                "end_time": 1576483865985
-                            }
-                        ]
-                    }
-                `);
+            case '1002814':
+                var fn = "../../ext-api-data/get/" +
+                    "mock%2Ftron-api%2Fv1%2Fassets%2F1002814.json";
+                var json = require(fn);
+                return json;
         }
 
         return {error: "Not implemented"};
