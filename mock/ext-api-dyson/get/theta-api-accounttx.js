d89c54e6b8ee1fed30d2dbecf48c059718fada4a
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-05-13 15:32:46 +0200

    More scripts updated.

--- mock/ext-api-dyson/get/theta-api-accounttx.js
+++ mock/ext-api-dyson/get/theta-api-accounttx.js
@@ -8,85 +8,10 @@ module.exports = {
     template: function(params, query, body) {
         //console.log(params)
         if (params.address === '0xac0eeb6ee3e32e2c74e14ac74155063e4f4f981f') {
-            return JSON.parse(`
-                {
-                    "type": "account_tx_list",
-                    "body": [
-                        {
-                            "_id": "0x85aaf834470dd858dc2f60c7bd535ddf08c3fcfef9456f43676462950a355b78",
-                            "block_height": "4160784",
-                            "data": {
-                                "fee": {
-                                    "thetawei": "0",
-                                    "tfuelwei": "2000000000000"
-                                },
-                                "inputs": [
-                                    {
-                                        "address": "0xac0eeb6ee3e32e2c74e14ac74155063e4f4f981f",
-                                        "coins": {
-                                            "thetawei": "1000000000000000",
-                                            "tfuelwei": "2000000000000"
-                                        },
-                                        "sequence": "52",
-                                        "signature": "0x60ae320a7a0f7f6e72ed5080830d2156f145a191197092df9eb88e7b8c7637ad01894b128b81d33b67a6516e3a6c7dc064576eb40b9516b4dc127e9845ede8c300"
-                                    }
-                                ],
-                                "outputs": [
-                                    {
-                                        "address": "0x082a2aef39b6473c55ba7e28c122ed3aea0de381",
-                                        "coins": {
-                                            "thetawei": "1000000000000000",
-                                            "tfuelwei": "0"
-                                        }
-                                    }
-                                ]
-                            },
-                            "hash": "0x85aaf834470dd858dc2f60c7bd535ddf08c3fcfef9456f43676462950a355b78",
-                            "number": 31836666,
-                            "status": "finalized",
-                            "timestamp": "1579009947",
-                            "type": 2
-                        },
-                        {
-                            "_id": "0x43804bbc7ff254f5eb3ff0a9bc1feed5788cbbc3eb5b535c52de4c47c8963dfd",
-                            "block_height": "4160736",
-                            "data": {
-                                "fee": {
-                                    "thetawei": "0",
-                                    "tfuelwei": "2000000000000"
-                                },
-                                "inputs": [
-                                    {
-                                        "address": "0xac0eeb6ee3e32e2c74e14ac74155063e4f4f981f",
-                                        "coins": {
-                                            "thetawei": "1000000000000000",
-                                            "tfuelwei": "2000000000000"
-                                        },
-                                        "sequence": "51",
-                                        "signature": "0x23cd4f14859ef6a9cd0bd820591101cd53c74d3421e01b42ca0671501afdd6f73dcf2ec8e35ceba773319ba552c0b170daa3460bf6125992e846f9d0cf839a9700"
-                                    }
-                                ],
-                                "outputs": [
-                                    {
-                                        "address": "0x082a2aef39b6473c55ba7e28c122ed3aea0de381",
-                                        "coins": {
-                                            "thetawei": "1000000000000000",
-                                            "tfuelwei": "0"
-                                        }
-                                    }
-                                ]
-                            },
-                            "hash": "0x43804bbc7ff254f5eb3ff0a9bc1feed5788cbbc3eb5b535c52de4c47c8963dfd",
-                            "number": 31836430,
-                            "status": "finalized",
-                            "timestamp": "1579009642",
-                            "type": 2
-                        }
-                    ],
-                    "totalPageNumber": 1,
-                    "currentPageNumber": 1
-                }
-            `);
+            var fn = "../../ext-api-data/get/" +
+                "mock%2Ftheta-api%2Faccounttx%2F0xac0eeb6ee3e32e2c74e14ac74155063e4f4f981f%3FisEqualType%3Dtrue%26limitNumber%3D100%26pageNumber%3D1%26type%3D2.json";
+            var json = require(fn);
+            return json;
         }
         
         return {error: "Not implemented"};
--- mock/ext-api-dyson/get/viacoin-api-address.js
+++ mock/ext-api-dyson/get/viacoin-api-address.js
@@ -9,80 +9,10 @@ module.exports = {
     template: function(params, query, body) {
         switch (params.address) {
             case 'VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A':
-                return JSON.parse(`
-                {
-                    "page": 1,
-                    "totalPages": 785695,
-                    "itemsOnPage": 2,
-                    "address": "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A",
-                    "balance": "120312392935",
-                    "totalReceived": "18370512600313",
-                    "totalSent": "18250200207378",
-                    "unconfirmedBalance": "0",
-                    "unconfirmedTxs": 0,
-                    "txs": 1571390,
-                    "transactions": [
-                        {
-                            "txid": "1b311427cd4749cbebd5c1fc70896b9e92a58d6935ba75bb40ed4a9e22e4a6cb",
-                            "version": 1,
-                            "vin": [
-                                {
-                                    "sequence": 4294967295,
-                                    "n": 0,
-                                    "coinbase": "03a02873045ea2915d"
-                                }
-                            ],
-                            "vout": [
-                                {
-                                    "value": "976562",
-                                    "n": 0,
-                                    "hex": "76a91424cc424c1e5e977175d2b20012554d39024bd68f88ac",
-                                    "addresses": [
-                                        "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A"
-                                    ]
-                                }
-                            ],
-                            "blockHash": "c590a4a7357d308917c6d3d8a8bd75b27968db346b965f2cd6e2be639e792f24",
-                            "blockHeight": 7547040,
-                            "confirmations": 3,
-                            "blockTime": 1587712349,
-                            "value": "976562",
-                            "valueIn": "0",
-                            "fees": "0",
-                            "hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0903a02873045ea2915dffffffff01b2e60e00000000001976a91424cc424c1e5e977175d2b20012554d39024bd68f88ac00000000"
-                        },
-                        {
-                            "txid": "e7393042e4b7755ba320a80ee734b0f0bd496a46fa3c9e9982c1a8176b6815b4",
-                            "version": 1,
-                            "vin": [
-                                {
-                                    "sequence": 4294967295,
-                                    "n": 0,
-                                    "coinbase": "039e2873045ea29101"
-                                }
-                            ],
-                            "vout": [
-                                {
-                                    "value": "976562",
-                                    "n": 0,
-                                    "hex": "76a91424cc424c1e5e977175d2b20012554d39024bd68f88ac",
-                                    "addresses": [
-                                        "VdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A"
-                                    ]
-                                }
-                            ],
-                            "blockHash": "87f245b6882d8d1a0a2122c5dd5e00a8a6ea0e811d8bc53cde5ba00a1a7b63ce",
-                            "blockHeight": 7547038,
-                            "confirmations": 5,
-                            "blockTime": 1587712257,
-                            "value": "976562",
-                            "valueIn": "0",
-                            "fees": "0",
-                            "hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff09039e2873045ea29101ffffffff01b2e60e00000000001976a91424cc424c1e5e977175d2b20012554d39024bd68f88ac00000000"
-                        }
-                    ]
-                }                
-                `);
+                var fn = "../../ext-api-data/get/" +
+                    "mock%2Fviacoin-api%2Fv2%2Faddress%2FVdMPvn7vUTSzbYjiMDs1jku9wAh1Ri2Y1A%3Fdetails%3Dtxs.json";
+                var json = require(fn);
+                return json;
         }
         return {error: "Not implemented"};
     }
