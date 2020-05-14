033efb7c12972d3d4df424d6236ec2788cfc8bdb
mykola.eth <3277207+kolya182@users.noreply.github.com>
2020-05-14 12:10:35 -0700

    Checksum address according to Wanchain EIP55 spec (#1097)
    * Checksum address acording to Wanchain speck
    
    * Fix wrong ref passed to func
    
    * Add return to better indicate logic
    
    * Add ToEIP55ByCoinID and test
    
    * Fix test
    
    * remove unsued test param
    
    * remove redundant test

--- pkg/address/address.go
+++ pkg/address/address.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"github.com/mr-tron/base58"
+	"github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/pkg/errors"
 	"github.com/trustwallet/blockatlas/pkg/logger"
 	"golang.org/x/crypto/sha3"
@@ -58,4 +59,42 @@ func HexToAddress(hexAddr string) (b58 string, err error) {
 	bytes = append(bytes, checksum[:4]...)
 	b58 = base58.EncodeAlphabet(bytes, base58.BTCAlphabet)
 	return
+}
+
+// Returns an EIP55 Wanchain compliant hex string representation of the address.
+// See https://wandevs.org/docs/difference-between-wanchain-and-ethereum/
+// https://github.com/wanchain/go-wanchain/blob/b238c203ca7dc6a578d57c0c473bec0fcf2431bd/common/types.go#L173
+func EIP55ChecksumWanchain(address string) string {
+	v := []byte(Remove0x(strings.ToLower(address)))
+	sha := sha3.NewLegacyKeccak256()
+	_, err := sha.Write(v)
+	if err != nil {
+		logger.Error(err)
+	}
+	hash := sha.Sum(nil)
+
+	result := v
+	for i := 0; i < len(result); i++ {
+		hashByte := hash[i/2]
+		if i%2 == 0 {
+			hashByte = hashByte >> 4
+		} else {
+			hashByte &= 0xf
+		}
+		if result[i] > '9' && hashByte <= 7 {
+			result[i] -= 32
+		}
+	}
+	return "0x" + string(result)
+}
+
+func ToEIP55ByCoinID(str string, coinID uint) string {
+	switch coinID {
+	case coin.ETH, coin.POA, coin.ETC, coin.TOMO, coin.CLO, coin.TT, coin.GO:
+		return EIP55Checksum(str)
+	case coin.WAN:
+		return EIP55ChecksumWanchain(str)
+	default:
+		return str
+	}
 }
