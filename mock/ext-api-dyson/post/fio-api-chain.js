e76a6bedd5fedcb541d2cb194ffbf506235369a3
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-05-14 15:36:44 +0200

    FIO lookup responses from files.

--- mock/ext-api-dyson/post/fio-api-chain.js
+++ mock/ext-api-dyson/post/fio-api-chain.js
@@ -4,7 +4,7 @@
 /// - public address not found message for other input
 /// See:
 /// curl -H "Content-Type: application/json" -d '{"fio_address":"trust@trust","token_code":"BTC","chain_code":"BTC"}' "http://localhost:3347/mock/fio-api/v1/chain/get_pub_address"
-/// curl -H "Content-Type: application/json" -d '{"fio_address":"trust@trust","token_code":"BTC","chain_code":"BTC"}' "http://testnet.fioprotocol.io/v1/chain/get_pub_address"
+/// curl -H "Content-Type: application/json" -d '{"fio_address":"trust@trust","token_code":"BTC","chain_code":"BTC"}' "https://fio.greymass.com/v1/chain/get_pub_address"
 /// curl "http://localhost:8437/v2/ns/lookup?name=trust@trust&coins=60"
 
 module.exports = {
@@ -14,22 +14,26 @@ module.exports = {
             switch (body.fio_address) {
                 case 'trust@trust':
                     switch (body.token_code) {
-                        case 'BTC':
-                            return {public_address: 'bc1qvy4074rggkdr2pzw5vpnn62eg0smzlxwp70d7v'};
                         case 'ETH':
-                            return {public_address: '0xce5cB6c92Da37bbBa91Bd40D4C9D4D724A3a8F51'};
-                        case 'BNB':
-                            return {public_address: 'bnb1ts3dg54apwlvr9hupv2n0j6e46q54znnusjk9s'};
+                            var fn = "../../ext-api-data/post/" +
+                                "mock%2Ffio-api%2Fv1%2Fchain%2Fget_pub_address.json";
+                            var json = require(fn);
+                            return json;
                     }
                     break;
         
                 case 'trust@trustwallet':
-                    return {public_address: '0xce5cB6c92Da37bbBa91Bd40D4C9D4D724A3a0001'};
+                    var fn = "../../ext-api-data/post/" +
+                        "mock%2Ffio-api%2Fv1%2Fchain%2Fget_pub_address.0001.json";
+                    var json = require(fn);
+                    return json;
         
                 case 'name@somefiodomain':
-                    return {public_address: '0xce5cB6c92Da37bbBa91Bd40D4C9D4D724A3a0002'};
+                    var fn = "../../ext-api-data/post/" +
+                        "mock%2Ffio-api%2Fv1%2Fchain%2Fget_pub_address.0002.json";
+                    var json = require(fn);
+                    return json;
             }
-            return {message: 'Public address not found'};
         }
         return {error: "Not implemented"};
     }
