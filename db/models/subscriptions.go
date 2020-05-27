52bdf85d93f24a20c4065d7bc8fdf04e15bcf6e2
Nick Kozlov <enormousrage@gmail.com>
2020-05-27 21:06:12 +0300

    Change DB tables

--- db/models/subscriptions.go
+++ db/models/subscriptions.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Subscription struct {
-	CreatedAt time.Time
+	CreatedAt time.Time  `gorm:"default:CURRENT_TIMESTAMP"`
 	DeletedAt *time.Time `sql:"index"`
-	Coin      uint       `gorm:"primary_key; column:coin; auto_increment:false"`
-	Address   string     `gorm:"primary_key; column:address; type:varchar(128)"`
+	Coin      uint       `gorm:"primary_key; column:coin; auto_increment:false";sql:"index"`
+	Address   string     `gorm:"primary_key; column:address; type:varchar(128)";sql:"index"`
 }
