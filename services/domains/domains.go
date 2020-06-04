5ff1949eb023370f9283c0d8580f0f651812b8f3
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-06-04 14:28:48 +0200

    Lint fix.

--- services/domains/domains.go
+++ services/domains/domains.go
@@ -10,21 +10,19 @@ func HandleLookup(name string, coins []uint64) ([]blockatlas.Resolved, error) {
 	// Visit all providers, try lookup with all matching ones
 	// There must be at least one provider (normally ==1)
 	ret := []blockatlas.Resolved{}
-	matchingCount := 0
+	providerCount := 0 // to count number of providers visited, may be different than number of results
 	for _, api := range platform.NamingAPIs {
 		if !api.Match(name) {
 			continue
 		}
+		providerCount++
 		result, err := api.Lookup(coins, name)
 		if err != nil {
 			return nil, errors.E(err, "name format not recognized", errors.Params{"name": name, "coins": coins})
 		}
-		for _, r := range result {
-			ret = append(ret, r)
-		}
-		matchingCount++
+		ret = append(ret, result...)
 	}
-	if matchingCount == 0 {
+	if providerCount == 0 {
 		return nil, errors.E("platform not found", errors.Params{"name": name, "coins": coins})
 	}
 	return ret, nil
