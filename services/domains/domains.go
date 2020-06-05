d9b5e612a95e2579740c6cae35b22036f78ca327
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-06-05 15:24:24 +0200

    Add unit test to services/domain.

--- services/domains/domains.go
+++ services/domains/domains.go
@@ -8,20 +8,26 @@ import (
 
 func HandleLookup(name string, coins []uint64) ([]blockatlas.Resolved, error) {
 	addresses := make([]blockatlas.Resolved, 0)
-	providerCount := 0 // to count number of providers visited, may be different than number of results
-	for _, api := range platform.NamingAPIs {
-		if !api.CanHandle(name) {
-			continue
-		}
-		providerCount++
+	apis := findHandlerApis(name, platform.NamingAPIs)
+	if len(apis) == 0 {
+		return nil, errors.E("platform not found", errors.Params{"name": name, "coins": coins})
+	}
+	for _, api := range apis {
 		provAddresses, err := api.Lookup(coins, name)
 		if err != nil {
 			return nil, errors.E(err, "name format not recognized", errors.Params{"name": name, "coins": coins})
 		}
 		addresses = append(addresses, provAddresses...)
 	}
-	if providerCount == 0 {
-		return nil, errors.E("platform not found", errors.Params{"name": name, "coins": coins})
-	}
 	return addresses, nil
+}
+
+func findHandlerApis(name string, allApis map[uint]blockatlas.NamingServiceAPI) []blockatlas.NamingServiceAPI {
+	apis := []blockatlas.NamingServiceAPI{}
+	for _, api := range allApis {
+		if api.CanHandle(name) {
+			apis = append(apis, api)
+		}
+	}
+	return apis
 }
