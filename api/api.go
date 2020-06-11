a446fa1c748fe9d9d015879ca8c3109757517a5e
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2020-06-11 13:45:38 +0300

    Remove swagger auth

--- api/api.go
+++ api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 	_ "github.com/trustwallet/blockatlas/docs"
@@ -25,8 +24,5 @@ func SetupPlatformAPI(router gin.IRouter) {
 }
 
 func SetupSwaggerAPI(router gin.IRouter) {
-	admin := router.Group("/admin", gin.BasicAuth(gin.Accounts{
-		viper.GetString("gin.login"): viper.GetString("gin.pass"),
-	}))
-	admin.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
