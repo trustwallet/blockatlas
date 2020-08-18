44940a6ca3220c0aa769dc58feab3f0df5683f21
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2020-08-18 18:33:50 +0300

    Add tests

--- tests/integration/db_test/tokenassociations_test.go
+++ tests/integration/db_test/tokenassociations_test.go
@@ -34,21 +34,82 @@ func Test_AddNewAssociationForAddress(t *testing.T) {
 	})
 
 	assert.Equal(t, assetIDsFromDB, assets)
+}
 
-	//var subscriptions []models.Subscription
-	//
-	//for i := 0; i < 100; i++ {
-	//	subscriptions = append(subscriptions, models.Subscription{
-	//		Coin:    uint(i),
-	//		Address: "testAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddr",
-	//	})
-	//}
-	//
-	//assert.Nil(t, database.AddSubscriptions(subscriptions, context.Background()))
-	//for i := 0; i < 100; i++ {
-	//	s, err := database.GetSubscriptions(uint(i), []string{"testAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddrtestAddr"}, context.Background())
-	//	assert.Nil(t, err)
-	//	assert.NotNil(t, s)
-	//}
+func Test_UpdateAssociationsForExistingAddresses(t *testing.T) {
+	setup.CleanupPgContainer(database.Gorm)
+	assets := []string{"f"}
 
+	err := database.AddAssociationsForAddress("A", assets, context.Background())
+	assert.Nil(t, err)
+
+	err = database.AddAssociationsForAddress("B", assets, context.Background())
+	assert.Nil(t, err)
+
+	assetsForA := []string{"aa", "bbb", "cccc"}
+	assetsForB := []string{"as", "bbb", "cccc"}
+
+	updateMap := make(map[string][]string)
+	updateMap["A"] = assetsForA
+	updateMap["B"] = assetsForB
+
+	err = database.UpdateAssociationsForExistingAddresses(updateMap, context.Background())
+	assert.Nil(t, err)
+
+	associationsA, err := database.GetAssociationsByAddresses([]string{"A"}, context.Background())
+	assert.Nil(t, err)
+
+	var assetIDsFromDBA []string
+	for _, a := range associationsA {
+		assetIDsFromDBA = append(assetIDsFromDBA, a.Asset.AssetID)
+	}
+	assetsA := []string{"aa", "bbb", "cccc", "f"}
+
+	sort.Slice(assetsA, func(i, j int) bool {
+		return len(assetsA[i]) > len(assetsA[j])
+	})
+
+	sort.Slice(assetIDsFromDBA, func(i, j int) bool {
+		return len(assetIDsFromDBA[i]) > len(assetIDsFromDBA[j])
+	})
+
+	assert.Equal(t, assetIDsFromDBA, assetsA)
+
+	associationsB, err := database.GetAssociationsByAddresses([]string{"B"}, context.Background())
+	assert.Nil(t, err)
+
+	var assetIDsFromDBB []string
+	for _, a := range associationsB {
+		assetIDsFromDBB = append(assetIDsFromDBB, a.Asset.AssetID)
+	}
+	assetsB := []string{"as", "bbb", "cccc", "f"}
+
+	sort.Slice(assetsB, func(i, j int) bool {
+		return len(assetsA[i]) > len(assetsA[j])
+	})
+
+	sort.Slice(assetIDsFromDBB, func(i, j int) bool {
+		return len(assetIDsFromDBB[i]) > len(assetIDsFromDBB[j])
+	})
+
+	assert.Equal(t, assetIDsFromDBB, assetsB)
+
+	associationsAB, err := database.GetAssociationsByAddresses([]string{"A", "B"}, context.Background())
+	assert.Nil(t, err)
+
+	var assetIDsFromDBAB []string
+	for _, a := range associationsAB {
+		assetIDsFromDBAB = append(assetIDsFromDBAB, a.Asset.AssetID)
+	}
+	assetsAB := []string{"aa", "as", "bbb", "cccc", "f"}
+
+	sort.Slice(assetsAB, func(i, j int) bool {
+		return len(assetsAB[i]) > len(assetsAB[j])
+	})
+
+	sort.Slice(assetIDsFromDBAB, func(i, j int) bool {
+		return len(assetIDsFromDBAB[i]) > len(assetIDsFromDBAB[j])
+	})
+
+	assert.Equal(t, assetIDsFromDBAB, assetsAB)
 }
