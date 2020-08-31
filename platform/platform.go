b573ace185121f4cd763c982ae646d79fcb81b98
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-08-31 18:03:32 +0800

    [Smart Chain] Add config (#1206)
    * Add smart chain config
    
    * update bsc sample config

--- platform/platform.go
+++ platform/platform.go
@@ -2,6 +2,7 @@ package platform
 
 import (
 	"fmt"
+
 	"github.com/trustwallet/blockatlas/platform/kava"
 
 	"github.com/spf13/viper"
@@ -107,6 +108,7 @@ func getAllHandlers() blockatlas.Platforms {
 		coin.Callisto().Handle:     ethereum.Init(coin.CLO, GetApiVar(coin.CLO), GetRpcVar(coin.CLO)),
 		coin.Wanchain().Handle:     ethereum.Init(coin.WAN, GetApiVar(coin.WAN), GetRpcVar(coin.WAN)),
 		coin.Tomochain().Handle:    ethereum.Init(coin.TOMO, GetApiVar(coin.TOMO), GetRpcVar(coin.TOMO)),
+		coin.Bsc().Handle:          ethereum.InitWithBlockbook(coin.BSC, GetApiVar(coin.BSC), GetRpcVar(coin.BSC)),
 		coin.Ethereum().Handle:     ethereum.InitWitCollection(coin.ETH, GetApiVar(coin.ETH), GetRpcVar(coin.ETH), GetVar("ethereum.blockbook_api"), GetVar("ethereum.collections_api"), GetVar("ethereum.collections_api_key")),
 		coin.Near().Handle:         near.Init(GetApiVar(coin.NEAR)),
 		coin.Elrond().Handle:       elrond.Init(coin.ERD, GetApiVar(coin.ERD)),
