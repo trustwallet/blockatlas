2ee0337aa57c76a054db6205a3257601805721eb
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2020-09-03 02:06:21 +0300

    Fixes to models

--- db/models/subscriptions.go
+++ db/models/subscriptions.go
@@ -6,12 +6,12 @@ type (
 	NotificationSubscription struct {
 		DeletedAt *time.Time `sql:"index"`
 		Address   Address    `gorm:"ForeignKey:AddressID; not null"`
-		AddressID uint       `gorm:"unique_index"`
+		AddressID uint       `gorm:"primary_key; auto_increment:false"`
 	}
 
 	AssetSubscription struct {
 		DeletedAt *time.Time `sql:"index"`
 		Address   Address    `gorm:"ForeignKey:AddressID; not null"`
-		AddressID uint       `gorm:"unique_index"`
+		AddressID uint       `gorm:"primary_key; auto_increment:false"`
 	}
 )
