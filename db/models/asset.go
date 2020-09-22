61b411c1472167a7049a5066ebb537a9e7e7c729
jeyldii <re3fo@mail.ru>
2020-09-23 01:52:25 +0300

    Fixes type serial in Asset.

--- db/models/asset.go
+++ db/models/asset.go
@@ -1,6 +1,6 @@
 package models
 
 type Asset struct {
-	ID    uint   `gorm:"primary_key; type:int4;"`
+	ID    uint   `gorm:"primary_key; type:serial;"`
 	Asset string `gorm:"type:varchar(128); uniqueIndex"`
 }
