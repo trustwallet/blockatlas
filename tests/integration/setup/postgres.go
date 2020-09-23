168424671f2d3bdcd0cc3b07a396e1113a9de1b8
jeyldii <re3fo@mail.ru>
2020-09-23 03:32:45 +0300

    Turn off logs in db integrations tests.

--- tests/integration/setup/postgres.go
+++ tests/integration/setup/postgres.go
@@ -42,7 +42,7 @@ func runPgContainerAndInitConnection() (*db.Instance, error) {
 		err    error
 	)
 	err = pool.Retry(func() error {
-		dbConn, err = db.New(uri, uri, true)
+		dbConn, err = db.New(uri, uri, false)
 		return err
 	})
 	if err != nil {
