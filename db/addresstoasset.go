3069ca882b74846c2b5d0861ab59d507fadaeecf
jeyldii <re3fo@mail.ru>
2020-09-24 23:37:30 +0300

    Adds logs

--- db/addresstoasset.go
+++ db/addresstoasset.go
@@ -151,10 +151,11 @@ func (i *Instance) UpdateAssociationsForExistingAddresses(associations map[strin
 
 		uniqueAssets := getUniqueStrings(assets)
 		uniqueAssetsModel := make([]models.Asset, 0, len(uniqueAssets))
+		logger.Info("uniqueAssetsModel")
 		for _, l := range uniqueAssets {
+			logger.Info("uniqueAssets", logger.Params{"asset": l})
 			uniqueAssetsModel = append(uniqueAssetsModel, models.Asset{Asset: l})
 		}
-		logger.Info("----------------------------------------", logger.Params{"uniqueAssetsModel": uniqueAssetsModel}, uniqueAssetsModel, "----------------------------------------")
 		if err := db.Clauses(clause.OnConflict{DoNothing: true}).Create(&uniqueAssetsModel).Error; err != nil {
 			return err
 		}
