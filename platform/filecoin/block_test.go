abf107892bc0f4068c886ce72d8553760e4ff284
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2020-11-17 15:38:43 +0300

    Add mock test for filecoin

--- platform/filecoin/block_test.go
+++ platform/filecoin/block_test.go
@@ -1,13 +1,31 @@
 package filecoin
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/trustwallet/golibs/mock"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestPlatform_CurrentBlockNumber(t *testing.T) {
-	p := Init("https://api.filscan.io:8700/rpc/v1")
+	chainHead, err := mock.JsonFromFilePathToString("mocks/ChainHead.json")
+	assert.Nil(t, err)
+
+	data := make(map[string]func(http.ResponseWriter, *http.Request))
+	data["/"] = func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if _, err := fmt.Fprint(w, chainHead); err != nil {
+			panic(err)
+		}
+	}
+
+	server := httptest.NewServer(mock.CreateMockedAPI(data))
+	defer server.Close()
+
+	p := Init(server.URL)
 	block, err := p.CurrentBlockNumber()
 	assert.Nil(t, err)
-	assert.NotNil(t, block)
+	assert.Equal(t, int64(243590), block)
 }
