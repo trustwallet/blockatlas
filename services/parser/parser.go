e1456253fef67ca8847fd75f7dd724411586cabf
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-01-04 20:33:11 -0800

    Update parser.go

--- services/parser/parser.go
+++ services/parser/parser.go
@@ -66,6 +66,7 @@ func parse(params Params) {
 		log.WithFields(log.Fields{
 			"operation": "fetch GetBlocksIntervalToFetch",
 			"coin":      params.Api.Coin().Handle,
+			"tags":      log.Fields{"coin": params.Api.Coin().Handle},
 		}).Error(err)
 		time.Sleep(params.ParsingBlocksInterval)
 		return
