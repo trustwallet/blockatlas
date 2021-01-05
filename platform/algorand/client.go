2a94256731bff6255d6be32b62a4aa256480af64
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-01-05 00:40:42 -0800

    Fix normalize test

--- platform/algorand/client.go
+++ platform/algorand/client.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
-	"github.com/trustwallet/golibs/numbers"
 )
 
 type Client struct {
@@ -41,30 +40,21 @@ func (c *Client) GetTxsInBlock(number int64) ([]Transaction, error) {
 	return resp.Transactions, err
 }
 
+//deprecated, no longer need to support staking
 func (c *Client) GetAccount(address string) (account *Account, err error) {
-	path := fmt.Sprintf("v1/account/%s", address)
+	path := fmt.Sprintf("v2/accounts/%s", address)
 	err = c.Get(&account, path, nil)
 	return
 }
 
 func (c *Client) GetTxsOfAddress(address string) ([]Transaction, error) {
 	var response TransactionsResponse
-	path := fmt.Sprintf("v1/account/%s/transactions", address)
+	path := fmt.Sprintf("v2/accounts/%s/transactions", address)
 
 	err := c.Get(&response, path, nil)
 	if err != nil {
 		return nil, blockatlas.ErrSourceConn
 	}
-	results := make([]Transaction, 0)
 
-	//FIXME. Currently fetching the last 6 transactions and get 6 blocks for each to retrieve timestamp.
-	//Algorand team promised to provide endpoint soon that will contain timestamp value inside TransactionsResponse response
-	//Get latest 6 transactions, which is enough until new endpoint fixes it.
-	txs := response.Transactions[:numbers.Min(6, len(response.Transactions))]
-
-	for _, t := range txs {
-		results = append(results, t)
-	}
-
-	return results, err
+	return response.Transactions, err
 }
