c6819182ffe59eaaae6ff52725548d5d592cbb89
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-01-11 00:09:00 -0800

    Update subscription.go

--- db/subscription.go
+++ db/subscription.go
@@ -15,7 +15,7 @@ func (i *Instance) CreateSubscriptions(addresses []blockatlas.Subscription) erro
 		result = append(result, models.Subscription{Address: address.AddressID()})
 	}
 
-	return i.Gorm.Clauses(clause.OnConflict{DoNothing: true}).Create(&result).Error
+	return i.Gorm.Clauses(clause.OnConflict{DoNothing: true}).CreateInBatches(&result, 10).Error
 }
 
 func (i *Instance) GetSubscriptions(addresses []string) ([]models.Subscription, error) {
