a85abde7b97e0e47b393e76603005eced356db6a
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-01-23 16:44:41 -0800

    Add ability to disable parsing for specific coin (#1382)
    * Add ability to disable parsing for specific coin
    
    * Fix TestDb_SetBlock tests

--- db/tracker.go
+++ db/tracker.go
@@ -1,51 +1,20 @@
 package db
 
 import (
-	"sync"
-
 	"github.com/trustwallet/blockatlas/db/models"
 	"gorm.io/gorm/clause"
 )
 
-var memoryCache heightBlockMap
-
-func init() {
-	memoryCache.m = make(map[string]int64)
-}
-
-type heightBlockMap struct {
-	m map[string]int64
-	sync.RWMutex
-}
-
-func (hbm *heightBlockMap) SetHeight(coin string, b int64) {
-	hbm.Lock()
-	defer hbm.Unlock()
-	hbm.m[coin] = b
-}
-
-func (hbm *heightBlockMap) GetHeight(coin string) (int64, bool) {
-	hbm.RLock()
-	defer hbm.RUnlock()
-	b, ok := hbm.m[coin]
-	return b, ok
-}
-
-func (i *Instance) GetLastParsedBlockNumber(coin string) (int64, error) {
-	height, ok := memoryCache.GetHeight(coin)
-	if ok {
-		return height, nil
-	}
+func (i *Instance) GetLastParsedBlockNumber(coin string) (models.Tracker, error) {
 	var tracker models.Tracker
 	if err := i.Gorm.
 		Find(&tracker, "coin = ?", coin).Error; err != nil {
-		return 0, nil
+		return tracker, err
 	}
-	return tracker.Height, nil
+	return tracker, nil
 }
 
 func (i *Instance) SetLastParsedBlockNumber(coin string, num int64) error {
-	memoryCache.SetHeight(coin, num)
 	tracker := models.Tracker{
 		Coin:   coin,
 		Height: num,
