08180ec8845096286bb32f888c925e5bef2af4a4
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-02-21 11:43:04 +0900

    ignore internal GetTransaction error

--- platform/bitcoin/blockbook/block.go
+++ platform/bitcoin/blockbook/block.go
@@ -1,10 +1,22 @@
 package blockbook
 
-import "github.com/trustwallet/golibs/types"
+import (
+	"strings"
+
+	"github.com/trustwallet/golibs/types"
+)
+
+const (
+	transactionError = "Internal server error: GetTransaction 0x"
+)
 
 func (c *Client) GetBlockByNumber(num int64, coinIndex uint) (*types.Block, error) {
 	block, err := c.GetAllTransactionsByBlockNumber(num)
 	if err != nil {
+		err2, ok := err.(*ClientError)
+		if ok && strings.HasPrefix(err2.Error(), transactionError) {
+			return &types.Block{Number: num, Txs: types.Txs{}}, nil
+		}
 		return nil, err
 	}
 	txs := make(types.Txs, 0)
